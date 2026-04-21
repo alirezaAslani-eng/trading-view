8d93aad52c6fbb156dd0a30bd809de3442aa934d
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-21 16:52:50 +0330

    fix(mui hydration error)
    Need dependencies `@emotion/cache` & `@mui/material-nextjs`

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/context/app/ThemeProvider";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { theme } from "@/packages/mui/theme";
 import { CssBaseline } from "@mui/material";
 
@@ -6,10 +7,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {children}
-        </ThemeProvider>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
