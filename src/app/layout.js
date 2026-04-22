37b1a2c85fbf207e53518a7533d2274161d9186f
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-22 15:51:53 +0330

    fix(load font)
    Almost all pages are using these three font familes

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,15 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { theme } from "@/packages/mui/theme";
 import { CssBaseline } from "@mui/material";
 import IranYekanMedium from "@/constant/app/nextjsFont/IranYekan-Medium";
+import IranYekanDemibold from "@/constant/app/nextjsFont/IranYekan-Demibold";
+import IranYekanRegular from "@/constant/app/nextjsFont/IranYekan-Regular";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={IranYekanMedium.variable}>
+      <body
+        className={`${IranYekanMedium.variable} ${IranYekanDemibold.variable} ${IranYekanRegular.variable}`}
+      >
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
