99d4fb14cf44433a179dde96d23e7c2666b5c0e9
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-22 14:01:46 +0330

    feat(loade font as global)
    The common font for most of pages is medium size so it's better to load globaly.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,12 @@ import { ThemeProvider } from "@/context/app/ThemeProvider";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { theme } from "@/packages/mui/theme";
 import { CssBaseline } from "@mui/material";
+import IranYekanMedium from "@/constant/app/nextjsFont/IranYekan-Medium";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body>
+      <body className={IranYekanMedium.variable}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
