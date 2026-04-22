998be474425ae2c65981ef496cde78ebf18b50ed
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-22 11:09:58 +0330

    feat(add green colors)

---
+++ src/packages/mui/theme/shades/green.js
@@ -0,0 +1,6 @@
+const green = {
+  400: "#0FC485",
+  500: "#229E6C",
+};
+
+export default green;
--- src/packages/mui/theme/shades/index.js
+++ src/packages/mui/theme/shades/index.js
@@ -1,1 +1,2 @@
 export { default as nuetral } from "./nuetral";
+export { default as green } from "./green";
