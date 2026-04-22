57ca5909df4b65a2070a8cb213289633f755ea22
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-22 14:32:03 +0330

    feat(configured mui typography variants)

--- src/packages/mui/theme/typography.js
+++ src/packages/mui/theme/typography.js
@@ -3,12 +3,94 @@ import { TypographyVariantsOptions } from "@mui/material/styles";
  * @type {TypographyVariantsOptions}
  */
 const typography = {
-  // fontFamily: "", // * < -- defult font-family
-  // xs: { fontSize: "12px" },
-  // base: { fontSize: "16px" },
-  // sm: { fontSize: "14px" },
-  // lg: { fontSize: "18px" },
-  // xl: { fontSize: "20px" },
+  fontFamily: "var(--iranyekan-medium)", // * < -- defult font-family
+  // * -------------- Heading --------------
+  h1: {
+    fontSize: "36px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-demibold)",
+  },
+  h2: {
+    fontSize: "32px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-demibold)",
+  },
+  h3: {
+    fontSize: "29px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-demibold)",
+  },
+  h4: {
+    fontSize: "26px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-demibold)",
+  },
+  h5: {
+    fontSize: "23px",
+    lineHeight: 1.7,
+    fontFamily: "var(--iranyekan-demibold)",
+  },
+  h6: {
+    fontSize: "20px",
+    lineHeight: 1.7,
+    fontFamily: "var(--iranyekan-demibold)",
+  },
+  h7: {
+    fontSize: "18px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-demibold)",
+  },
+  // * -------------- Body --------------
+
+  body1: {
+    fontSize: "18px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-regular)",
+  },
+  body2: {
+    fontSize: "16px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-regular)",
+  },
+  body3: {
+    fontSize: "14px",
+    lineHeight: 1.2,
+    fontFamily: "var(--iranyekan-regular)",
+  },
+  // * -------------- Button --------------
+
+  button1: {
+    fontSize: "18px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-medium)",
+  },
+  button2: {
+    fontSize: "16px",
+    lineHeight: 1.2,
+    fontFamily: "var(--iranyekan-medium)",
+  },
+  button3: {
+    fontSize: "14px",
+    lineHeight: 1.2,
+    fontFamily: "var(--iranyekan-medium)",
+  },
+  button4: {
+    fontSize: "13px",
+    lineHeight: 1.5,
+    fontFamily: "var(--iranyekan-medium)",
+  },
+
+  // * -------------- Caption --------------
+  caption1: {
+    fontSize: "13px",
+    lineHeight: 1.2,
+    fontFamily: "var(--iranyekan-regular)",
+  },
+  caption2: {
+    fontSize: "11px",
+    lineHeight: 1.2,
+    fontFamily: "var(--iranyekan-regular)",
+  },
 };
 
 export default typography;
