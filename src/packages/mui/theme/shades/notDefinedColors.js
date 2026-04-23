d56662fb18576d8bbf2a4bb2fc47d0b8d753cc29
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-23 09:39:42 +0330

    feat(add not defined color)

--- src/packages/mui/theme/shades/notDefinedColors.js
+++ src/packages/mui/theme/shades/notDefinedColors.js
@@ -2,6 +2,10 @@ const notDefinedColors = {
   "#595B5F": "#595B5F",
   "#363636": "#363636",
   "#57A8FF": "#57A8FF",
+  "#282828": "#282828",
+  "#3A3A3A": "#3A3A3A",
+  "#434343": "#434343",
+  "#B7B7B7": "#B7B7B7",
 };
 
 export default notDefinedColors;
