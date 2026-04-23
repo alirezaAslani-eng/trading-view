b53c956d2d8aac4761a617ddaadb16e8d2150b45
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-23 23:23:59 +0330

    feat(add style to component)
    added default margin

--- src/components/ui/Checkbox/CheckBox.jsx
+++ src/components/ui/Checkbox/CheckBox.jsx
@@ -2,6 +2,7 @@
 import { Box, Checkbox, FormLabel, Typography } from "@mui/material";
 import React from "react";
 import useRndomID from "@/hooks/app/useRndomID";
+import { identifySxProp } from "@/packages/mui/theme/helpers";
 /**
  *
  * @param {import("@mui/material").CheckboxProps & {label:import("react").ReactNode}} props
@@ -10,7 +11,14 @@ function CheckBox({ label, ...props }) {
   const randomLabelID = useRndomID();
   return (
     <Box sx={{ display: "flex", alignItems: "center" }}>
-      <Checkbox id={randomLabelID} />
+      <Checkbox
+        id={randomLabelID}
+        {...props}
+        sx={(tm) => ({
+          ml: "8px",
+          ...identifySxProp(tm, props.sx),
+        })}
+      />
       <FormLabel htmlFor={randomLabelID}>{label}</FormLabel>
     </Box>
   );
