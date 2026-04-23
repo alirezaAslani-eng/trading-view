7a396180046dc7ab0953a67b5ddbd1a1a57272a7
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-23 18:06:48 +0330

    feat(add page) added signup page

---
+++ src/app/(NoLayout)/signup/page.js
@@ -0,0 +1,30 @@
+import SignupForm from "@/components/template/Form/SignupForm";
+import AuthFormLayout from "@/components/template/Layout/AuthFormLayout/AuthFormLayout";
+import AuthFormLayoutHeading from "@/components/template/Layout/AuthFormLayout/AuthFormLayoutHeading";
+import AuthFormLayoutToggleButoon from "@/components/template/Layout/AuthFormLayout/AuthFormLayoutToggleButoon";
+import { Box } from "@mui/material";
+import React from "react";
+
+function page() {
+  return (
+    <Box
+      sx={{
+        height: "100svh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AuthFormLayout>
+        <AuthFormLayoutHeading
+          title="به آیرونکس خوش آمدید"
+          subTitle="جهت عضویت و ورود به پلتفرم، شماره تماس خود را وارد کنید"
+        />
+        <AuthFormLayoutToggleButoon />
+        <SignupForm />
+      </AuthFormLayout>
+    </Box>
+  );
+}
+
+export default page;
