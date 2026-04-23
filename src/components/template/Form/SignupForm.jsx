b16408050c8668f826655ab2be515416b35717ed
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-23 18:03:39 +0330

    feat(add component)
    added SignupForm compnent, this component let users to signup

---
+++ src/components/template/Form/SignupForm.jsx
@@ -0,0 +1,50 @@
+"use client";
+import { Box, Button, FormLabel, Stack, Typography } from "@mui/material";
+import useRndomID from "@/hooks/app/useRndomID";
+import { identifySxProp } from "@/packages/mui/theme/helpers";
+import PhoneNumberField from "@/components/ui/Input/PhoneNumberField";
+import CheckBox from "@/components/ui/Checkbox/CheckBox";
+
+/**
+ * @param {{sx:import("@mui/material").BoxProps}} p1
+ */
+function SignupForm({ sx }) {
+  const phoneLabelID = useRndomID();
+  return (
+    <Box
+      sx={(tm) => ({ mt: "56px", width: "100%", ...identifySxProp(tm, sx) })}
+      component={"form"}
+    >
+      <Stack direction={"row"} sx={{ gap: "38px", alignSelf: "start" }}>
+        <CheckBox
+          label={<Typography variant="button2">{"حساب حقیقی"}</Typography>}
+        />
+        <CheckBox
+          label={<Typography variant="button2">{"حساب حقوقی"}</Typography>}
+        />
+      </Stack>
+
+      <Box sx={{ width: "100%", mt: "32px", px: "calc(50px - 18px)" }}>
+        <FormLabel htmlFor={phoneLabelID} variant="button2">
+          {"شماره موبایل"}
+        </FormLabel>
+        <Box sx={{ mt: "10px" }}>
+          <PhoneNumberField />
+        </Box>
+      </Box>
+
+      <Button
+        fullWidth
+        sx={{ mt: "32px" }}
+        size={"large"}
+        variant="contained"
+        color="primary"
+        type="submit"
+      >
+        {"تایید و دریافت کد"}
+      </Button>
+    </Box>
+  );
+}
+
+export default SignupForm;
