39296a90e8624fe7026fb067d87ab28aeb9e80c3
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-24 13:34:57 +0330

    feat(add component)
    This component will be responsible for requesting OTP and handling timer state

---
+++ src/components/template/Button/SendAuthOTP.jsx
@@ -0,0 +1,30 @@
+import { identifySxProp } from "@/packages/mui/theme/helpers";
+import { Typography } from "@mui/material";
+import React from "react";
+
+/**
+ * @param {import('@mui/material').TypographyProps} props
+ */
+function SendAuthOTP(props) {
+  return (
+    <>
+      <Typography
+        color="primary"
+        variant="body3"
+        {...props}
+        sx={(tm) => ({
+          display: "flex",
+          alignItems: "center",
+          gap: "6px",
+          cursor: "pointer",
+          userSelect: "none",
+          ...identifySxProp(tm, props.sx),
+        })}
+      >
+        {true ? props.children : "timer"}
+      </Typography>
+    </>
+  );
+}
+
+export default SendAuthOTP;
