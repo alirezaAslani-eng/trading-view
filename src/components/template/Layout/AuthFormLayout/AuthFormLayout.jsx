34621fceeb20abbb7b8050ed2acf7af84ee9b94a
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-24 18:08:24 +0330

    feat(add style)
    added border and backdropFilter to the layout component of Authentication forms

--- src/components/template/Layout/AuthFormLayout/AuthFormLayout.jsx
+++ src/components/template/Layout/AuthFormLayout/AuthFormLayout.jsx
@@ -2,7 +2,7 @@
 import { identifySxProp } from "@/packages/mui/theme/helpers";
 import { notDefinedColors } from "@/packages/mui/theme/shades";
 import { alpha, Stack } from "@mui/material";
-
+const blur = "blur(40px)";
 /**
  * @param {{children:import("react").ReactNode,sx:import("@mui/material").StackProps["sx"]}} param0
  * @returns
@@ -12,10 +12,13 @@ function AuthFormLayout({ children, sx }) {
     <Stack
       sx={(tm) => ({
         alignItems: "center",
-        borderRadius: "24px",
         bgcolor: alpha(notDefinedColors["#363636"], 0.18),
         p: "56px 18px 68px 18px ",
         width: "min(503px, 100%)",
+        backdropFilter: blur,
+        border: "2px solid",
+        borderRadius: "24px",
+        borderColor: alpha(tm.palette.border.white,0.05),
         ...identifySxProp(tm, sx),
       })}
     >
