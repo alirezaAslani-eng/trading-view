903369815ecef741872a2aaf364996609332c71e
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-24 13:35:39 +0330

    fix(style)

--- src/components/ui/Link/NextLink.jsx
+++ src/components/ui/Link/NextLink.jsx
@@ -2,5 +2,9 @@
 import { styled } from "@mui/material";
 import Link from "next/link";
 
-const NextLink = styled(Link)({ display: "block" });
+const NextLink = styled(Link)({
+  display: "block",
+  textDecoration: "none",
+  color: "inherit",
+});
 export default NextLink;
