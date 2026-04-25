5b0ff597cc2710fec305628151ba3912257474e2
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-25 14:37:28 +0330

    feat(add component)
    A reusable component that will be used for selecte inputs and dropdown components

---
+++ src/components/ui/DropdownButton/DropdownButton.jsx
@@ -0,0 +1,69 @@
+"use client";
+import { notDefinedColors } from "@/packages/mui/theme/shades";
+import { Box, styled } from "@mui/material";
+
+const StyledDropdownButton = styled(Box, {
+  shouldForwardProp: (prop) => {
+    return (
+      !!(prop !== "variant") ||
+      !!(prop !== "color") ||
+      !!(prop !== "active") ||
+      !!(prop !== "size")
+    );
+  },
+})(
+  ({
+    theme,
+    // * --- Custom props ---
+    variant = "text",
+    color = "nuteral",
+    size = "large",
+    active = false,
+  }) => ({
+    display: "flex",
+    alignItems: "center",
+    ...dropdownButtonTheme({ active, color, theme, variant }),
+    ...dropdownButtonSizes(theme, size),
+  }),
+);
+
+function dropdownButtonTheme({ theme, variant, color, active }) {
+  // * ---- Neuteral Theme ----
+  if (color === "nuteral") {
+    if (variant === "text") {
+      // * --- active state ----
+      if (active) {
+        return {
+          backgroundColor: notDefinedColors["#2F2F31"],
+          color: theme.palette.text.heading,
+        };
+      }
+      return {
+        backgroundColor: "transparent",
+        color: theme.palette.text.heading,
+      };
+    }
+  }
+}
+
+function dropdownButtonSizes(theme, size) {
+  if (size === "large") {
+    return {
+      padding: "0px 15px",
+      height: "42px",
+      borderRadius: "12px",
+      fontSize: theme.typography.button3?.fontSize,
+      fontFamily: theme.typography.button3?.fontFamily,
+      lineHeight: theme.typography.button3?.lineHeight,
+    };
+  }
+}
+
+/**
+ * @param {React.ComponentProps<typeof StyledDropdownButton> & {variant:string,color:string,size:string,active:boolean}} props
+ */
+function DropdownButton(props) {
+  return <StyledDropdownButton {...props} />;
+}
+
+export default DropdownButton;
