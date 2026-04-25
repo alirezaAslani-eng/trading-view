2ab29cd4da18ee94518100b5eb52b5d11757313f
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-25 14:33:15 +0330

    feat(component)
    Made Link component works like NavLink by adding the prop `activeSx`

--- src/components/ui/Link/NextLink.jsx
+++ src/components/ui/Link/NextLink.jsx
@@ -1,10 +1,33 @@
 "use client";
+import { identifySxProp } from "@/packages/mui/theme/helpers";
 import { styled } from "@mui/material";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-const NextLink = styled(Link)({
+const StyledNextLink = styled(Link)({
   display: "block",
   textDecoration: "none",
   color: "inherit",
 });
+
+/**
+ * @param {{activeSx:import("@mui/material").SxProps<import("@mui/material").Theme>} & React.ComponentProps<typeof StyledNextLink>} props
+ */
+function NextLink({ activeSx, ...props }) {
+  const pathname = usePathname();
+
+  return (
+    <StyledNextLink
+      {...props}
+      sx={(tm) => ({
+        ...identifySxProp(tm, props.sx),
+        ...(pathname === props.href && identifySxProp(tm, activeSx)),
+      })}
+    >
+      {props.children}
+    </StyledNextLink>
+  );
+}
+
 export default NextLink;
