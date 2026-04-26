3ef30d9a7c06cea6f0a5461ae7afb12af609a66a
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-26 23:06:15 +0330

    feat(add container)
    initialized container component for panel's pages but in the future it will have breakpoints based on the design

--- src/app/(PanelLayout)/assets/page.jsx
+++ src/app/(PanelLayout)/assets/page.jsx
@@ -1,12 +1,15 @@
 import PageHeader from "@/components/common/Appbar/PageHeader";
+import PanelContainer from "@/components/ui/Layout/PanelContainer";
 import { Box } from "@mui/material";
 import React from "react";
 
 function page() {
   return (
-    <Box sx={{ px: "28px" }}>
-      <PageHeader title="دارایی‌ها" subtitle="نمای کلی سرمایه و سفارشات" />
-    </Box>
+    <main>
+      <PanelContainer>
+        <PageHeader title="دارایی‌ها" subtitle="نمای کلی سرمایه و سفارشات" />
+      </PanelContainer>
+    </main>
   );
 }
 
