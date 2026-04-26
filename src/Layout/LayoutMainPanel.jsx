c6b59fd3edbd47fbc3c00c33ba7280f67264100b
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-26 14:11:27 +0330

    feat(add layout component)
    developed the layout of panel's pages

---
+++ src/Layout/LayoutMainPanel.jsx
@@ -0,0 +1,34 @@
+import { Box } from "@mui/material";
+import React from "react";
+import SidebarPanel from "@/components/template/Sidebar/SidebarPanel";
+
+function LayoutMainPanel() {
+  return (
+    <>
+      <Box sx={{ display: "flex", height: "2000px" }}>
+        
+        {/* // * --- sidebar --- */}
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Box
+            sx={{
+              position: "sticky",
+              top: "0px",
+              p: "20px 20px 20px 0px",
+              width: "264px",
+              height: "100svh",
+            }}
+          >
+            <SidebarPanel />
+          </Box>
+        </Box>
+
+        {/* // * --- main content --- */}
+        <Box component={"main"} sx={{ flex: 1, minWidth: "0px" }}>
+          {children}
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default LayoutMainPanel;
---
+++ src/app/(PanelLayout)/layout.jsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import LayoutMainPanel from "@/Layout/LayoutMainPanel";
+
+function layout({ children }) {
+  return <LayoutMainPanel>{children}</LayoutMainPanel>;
+}
+
+export default layout;
