942eebc374c5002daefe6c5e76a175ac5f27722e
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-26 11:24:54 +0330

    feat(add component)
    This component has modularized at the moment but in the future the structure might changes

---
+++ src/components/template/Button/SwitchTheme.jsx
@@ -0,0 +1,34 @@
+"use client";
+import FakeIcon from "@/components/ui/Icon/FakeIcon";
+import { Box, Switch, Typography } from "@mui/material";
+
+function SwitchTheme() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        mt: "32px",
+      }}
+    >
+      <Typography
+        variant="button3"
+        sx={{
+          color: "text.disabled",
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <FakeIcon />
+        {"حالت تیره"}
+      </Typography>
+
+      <Switch />
+    </Box>
+  );
+}
+
+
+export default SwitchTheme;
