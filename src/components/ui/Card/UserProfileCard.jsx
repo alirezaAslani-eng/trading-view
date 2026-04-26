211165008fe9bf75f73c359df3b2884b417748c0
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-26 11:34:14 +0330

    feat(add component)
    A component that presents user's name, profile and email.
    The structure of component might chages in the future.

---
+++ src/components/ui/Card/UserProfileCard.jsx
@@ -0,0 +1,56 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { lineClamp } from "@/packages/mui/theme/helpers";
+import ProfileCircle from "@/components/ui/Profile/ProfileCircle";
+import FakeIcon from "@/components/ui/Icon/FakeIcon";
+
+function UserProfileCard() {
+  return (
+    <Box
+      sx={{
+        border: "1px solid",
+        borderColor: "border.default",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        pr: "8px",
+        pl: "4px",
+        height: "52px",
+        borderRadius: "12px",
+        mt: "144px",
+        gap:"30px"
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "8px",
+        }}
+      >
+        {/* // * ---------- Profile ---------- */}
+        <ProfileCircle src="/images/person.png" />
+
+        {/* // * ---------- Info ---------- */}
+        <Stack sx={{ width: "100px", gap: "6px" }}>
+          {/* // * ---------- Name ---------- */}
+          <Typography
+            variant="body3"
+            sx={{ color: "text.heading", ...lineClamp(1) }}
+          >
+            {"علی زمانی نژاد"}
+          </Typography>
+          {/* // * ---------- Email ---------- */}
+          <Typography
+            variant="caption2"
+            sx={{ color: "text.disabled", ...lineClamp(1) }}
+          >
+            {"Alizamani23@gmail.com ."}
+          </Typography>
+        </Stack>
+      </Box>
+      <FakeIcon />
+    </Box>
+  );
+}
+
+export default UserProfileCard;
