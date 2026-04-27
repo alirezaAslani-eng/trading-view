f4c22a1ee65e6e5e537e75c019d153b4351819b8
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-27 15:25:29 +0330

    feat(add icon)

---
+++ src/components/ui/Icon/ReceiveIcon.jsx
@@ -0,0 +1,37 @@
+import { SvgIcon } from "@mui/material";
+import React from "react";
+
+/**
+ * @param {import('@mui/material').SvgIconProps} props
+ */
+function ReceiveIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M7.04321 9.51944L9.99993 12.4762L12.9567 9.51944"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9.99976 12.4762V4.59164"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M4.08643 14.4474C7.92031 15.7287 12.0794 15.7287 15.9133 14.4474"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
+
+export default ReceiveIcon;
---
+++ src/components/ui/Icon/index.js
@@ -0,0 +1,2 @@
+export { default as ReceiveIcon } from "./ReceiveIcon";
+export { default as SendIcon } from "./SendIcon";
