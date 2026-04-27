a2f5775d7c5e8262f56e2eff0910e0adc7099974
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-27 15:40:17 +0330

    feat(add componenet to a page)

--- src/app/(PanelLayout)/assets/page.jsx
+++ src/app/(PanelLayout)/assets/page.jsx
@@ -1,15 +1,22 @@
 import PageHeader from "@/components/common/Appbar/PageHeader";
+import TotalAssetCard from "@/components/template/Card/TotalAssetCard";
 import PanelContainer from "@/components/ui/Layout/PanelContainer";
+import PanelPaper from "@/components/ui/Paper/PanelPaper";
 import { Box } from "@mui/material";
 import React from "react";
 
 function page() {
   return (
-    <main>
-      <PanelContainer>
-        <PageHeader title="دارایی‌ها" subtitle="نمای کلی سرمایه و سفارشات" />
-      </PanelContainer>
-    </main>
+    <PanelContainer>
+      <PageHeader title="دارایی‌ها" subtitle="نمای کلی سرمایه و سفارشات" />
+      <main>
+        <Box sx={{ mt: "41px" }}>
+          <PanelPaper sx={{ p: "20px 16px", width: "698px" }}>
+            <TotalAssetCard />
+          </PanelPaper>
+        </Box>
+      </main>
+    </PanelContainer>
   );
 }
 
