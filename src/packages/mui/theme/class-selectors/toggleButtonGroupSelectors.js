4b853463403312eab8a3130bfef430495c5e4eec
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-27 11:42:12 +0330

    feat(add mui class selectors)
    storage these selectors to make theme reuseable and ease to maintain

---
+++ src/packages/mui/theme/class-selectors/toggleButtonGroupSelectors.js
@@ -0,0 +1,9 @@
+const activeToggleButtonSelector = "& .Mui-selected";
+const inactiveToggleButtonSelector = "& button:not(.Mui-selected)";
+const allToggleButtonsSelector = "& button";
+
+export {
+  activeToggleButtonSelector,
+  allToggleButtonsSelector,
+  inactiveToggleButtonSelector,
+};
