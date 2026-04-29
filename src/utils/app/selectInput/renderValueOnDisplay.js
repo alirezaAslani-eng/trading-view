341c6ebe70ba112c499809185c29929b9bddc590
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-29 14:56:19 +0330

    fix(select input utility)
    used Children.toArray is more safe than enumrate children prop directly

--- src/utils/app/selectInput/renderValueOnDisplay.js
+++ src/utils/app/selectInput/renderValueOnDisplay.js
@@ -1,14 +1,17 @@
+"use client"
 import React from "react";
 
 /**
  * @returns {import("react").ReactNode}
  */
 function renderValueOnDisplay({ selectedValue, children }) {
-  
-  const selectedItem = React.Children.toArray(children?.props?.children).find((child) => {
+  const menu = React.Children.toArray(children)?.[0];
+  const menuitems = React.Children.toArray(menu?.props?.children);
+
+  const selectedItem = menuitems.find((child) => {
     return child?.props?.value === selectedValue;
   });
-  return selectedItem?.props?.children;
+  return selectedItem?.props?.children ?? null;
 }
 
 export default renderValueOnDisplay;
