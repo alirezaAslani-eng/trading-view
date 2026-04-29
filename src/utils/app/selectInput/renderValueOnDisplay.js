1283075ad69a65929a96477dbb6a4035a1c663ef
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-29 13:14:00 +0330

    fix(utility)
    the first children is SelectMenu component so we need its chilren which are the SelectItems

--- src/utils/app/selectInput/renderValueOnDisplay.js
+++ src/utils/app/selectInput/renderValueOnDisplay.js
@@ -4,7 +4,8 @@ import React from "react";
  * @returns {import("react").ReactNode}
  */
 function renderValueOnDisplay({ selectedValue, children }) {
-  const selectedItem = React.Children.toArray(children).find((child) => {
+  
+  const selectedItem = React.Children.toArray(children?.props?.children).find((child) => {
     return child?.props?.value === selectedValue;
   });
   return selectedItem?.props?.children;
