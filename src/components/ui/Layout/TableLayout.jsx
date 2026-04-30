16ae1d582b68d27ef01e874cec62176645f3fe91
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-30 22:07:12 +0330

    feat(add a common layout for tables)
    This layout includes a heading section and a title.

---
+++ src/components/ui/Layout/TableLayout.jsx
@@ -0,0 +1,26 @@
+"use client";
+import PanelPaper from "@/components/ui/Paper/PanelPaper";
+import { Box, styled, Typography } from "@mui/material";
+
+const TableLayout = styled(PanelPaper)({
+  display: "flex",
+  flexDirection: "column",
+  gap: "40px",
+  padding: "20px 16px",
+  width:"100%"
+});
+
+const TableLayoutHeading = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+const TableLayoutTitle = styled(Typography)(({ theme }) => {
+  return {
+    fontSize: theme.typography.button1.fontSize,
+    fontFamily: theme.typography.button1.fontFamily,
+    color: theme.palette.text.heading,
+  };
+});
+export { TableLayout, TableLayoutHeading, TableLayoutTitle };
