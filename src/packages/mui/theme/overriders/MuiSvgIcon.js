e881e0ef30d106ee477c147cb0f5f0b3220c7bdb
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-30 09:33:31 +0330

    feat(add fontSize variants for MuiSvgIcon)

--- src/packages/mui/theme/overriders/MuiSvgIcon.js
+++ src/packages/mui/theme/overriders/MuiSvgIcon.js
@@ -2,6 +2,9 @@
  * @type {import("@mui/material").Components<import("@mui/material").Theme>["MuiSvgIcon"]}
  */
 const MuiSvgIcon = {
+  defaultProps: {
+    fontSize: "large",
+  },
   styleOverrides: {
     root: ({ theme }) => ({
       width: "20px",
@@ -10,8 +13,21 @@ const MuiSvgIcon = {
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
-      fill:"none"
+      fill: "none",
     }),
+
+    fontSizeLarge: {
+      width: "20px",
+      height: "20px",
+    },
+    fontSizeMedium: {
+      width: "18px",
+      height: "18px",
+    },
+    fontSizeSmall: {
+      width: "14px",
+      height: "14px",
+    },
   },
 };
 
