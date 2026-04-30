87917c79b761e13e5d75d7c3157ddc70b7e8f36d
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-30 09:51:57 +0330

    feat(add component to a page)
    added `ExchangeAssetForm` to just make it visible, in the future it will be responsive

--- src/app/(PanelLayout)/assets/page.jsx
+++ src/app/(PanelLayout)/assets/page.jsx
@@ -1,8 +1,9 @@
 import PageHeader from "@/components/common/Appbar/PageHeader";
 import TotalAssetCard from "@/components/template/Card/TotalAssetCard";
+import ExchangeAssetForm from "@/components/template/Form/ExchangeAssetForm";
 import PanelContainer from "@/components/ui/Layout/PanelContainer";
 import PanelPaper from "@/components/ui/Paper/PanelPaper";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import React from "react";
 
 function page() {
@@ -10,10 +11,18 @@ function page() {
     <PanelContainer>
       <PageHeader title="دارایی‌ها" subtitle="نمای کلی سرمایه و سفارشات" />
       <main>
-        <Box sx={{ mt: "41px" }}>
-          <PanelPaper sx={{ p: "20px 16px 31px 16px", width: "698px" }}>
+        <Box sx={{ mt: "41px", display: "flex", gap: "24px" }}>
+          {/* // * ----start------- Total Asset ----------- */}
+          <PanelPaper sx={{ p: "20px 16px 31px 16px", flex: "1 1 698px" }}>
             <TotalAssetCard />
           </PanelPaper>
+          {/* // * ----end------- Total Asset ----------- */}
+
+          {/* // * -----start------ Exchange Asset ----------- */}
+          <PanelPaper sx={{ p: "20px 16px 11px 16px", flex: "1 1 378px" }}>
+            <ExchangeAssetForm />
+          </PanelPaper>
+          {/* // * -----end------ Exchange Asset ----------- */}
         </Box>
       </main>
     </PanelContainer>
