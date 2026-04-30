dd9dc4ef3a12282a687d6437fd7f7242f602cd0f
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-30 21:07:43 +0330

    feat(add icon)

--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -3,3 +3,4 @@ export { default as SendIcon } from "./SendIcon";
 export { default as DownIcon } from "./DownIcon";
 export { default as DownMinimalIcon } from "./DownMinimalIcon";
 export { default as ArrowUpDownIcon } from "./ArrowUpDownIcon";
+export { default as SearchIcon } from "./SearchIcon";
