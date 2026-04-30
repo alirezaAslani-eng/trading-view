7f85117f68ce3b94fb9aa401993ea1eb729eb52b
Alireza <alirezaaslane.mob7@gmail.com>
2026-04-30 20:16:42 +0330

    feat(customized table component)
    used css in js selectors for Table component from MUI to customize elements such as `th`, `td`

---
+++ src/components/ui/Table/Table.jsx
@@ -0,0 +1,39 @@
+"use client";
+import { columnSpacing } from "@/packages/mui/theme/helpers";
+import { notDefinedColors } from "@/packages/mui/theme/shades";
+import { styled, Table as Table_ } from "@mui/material";
+
+const rowSpace = "12px";
+const columnSpace = "35.66px";
+const asideSpace = "12px";
+
+const Table = styled(Table_)(({ theme }) => ({
+  // * TableCell Styles
+  "& td, & th": {
+    textAlign: "right",
+    padding: "0px",
+    border: "none",
+    margin: "0px",
+    maxWidth: "160px",
+    fontSize: theme.typography.caption1.fontSize,
+    fontFamily: theme.typography.caption1.fontFamily,
+    lineHeight: theme.typography.caption1?.lineHeight,
+    paddingTop: rowSpace,
+    ...columnSpacing(columnSpace, asideSpace),
+  },
+  "& tr td": {
+    color: theme.palette.text.onPrimary,
+  },
+  "& tr th": {
+    paddingBottom: rowSpace,
+    borderBottom: "1px solid",
+    borderColor: notDefinedColors["#2A2B2F"],
+    whiteSpace: "nowrap",
+    color: theme.palette.text.caption,
+  },
+  "& tbody tr:first-of-type td": {
+    paddingTop: "24px",
+  },
+}));
+
+export default Table;
