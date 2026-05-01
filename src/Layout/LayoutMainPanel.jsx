cc998a71913740208bcb6366188dbfe3872c94c0
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-01 23:45:58 +0330

    feat(add component)
    A button that only used in MainLayout component, in the future the name of this component might changes or the style of it might be a variant, if it will be used more than once for different cases

--- src/Layout/LayoutMainPanel.jsx
+++ src/Layout/LayoutMainPanel.jsx
@@ -1,20 +1,15 @@
-import { Box } from "@mui/material";
+"use client";
+import { alpha, Box, Button, Portal } from "@mui/material";
 import React from "react";
 import SidebarPanel from "@/components/template/Sidebar/SidebarPanel";
+import SupportButton from "@/components/template/Button/SupportButton";
 
 function LayoutMainPanel({ children }) {
   return (
     <>
       <Box sx={{ display: "flex" }}>
         {/* // * --- sidebar --- */}
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            pt: "20px",
-            pr: "20px",
-          }}
-        >
+        <Box sx={{ display: "flex", pr: "20px", flexDirection: "column" }}>
           <Box
             sx={{
               position: "sticky",
@@ -30,6 +25,8 @@ function LayoutMainPanel({ children }) {
         {/* // * --- main content --- */}
         <Box sx={{ flex: 1, minWidth: "0px", pb: "48px" }}>{children}</Box>
       </Box>
+
+      <SupportButton size="large">{"پشتیبانی انلاین"}</SupportButton>
     </>
   );
 }
---
+++ src/components/template/Button/SupportButton.jsx
@@ -0,0 +1,27 @@
+"use client";
+import { identifySxProp } from "@/packages/mui/theme/helpers";
+import { alpha, Button } from "@mui/material";
+
+/**
+ *
+ * @param {import("@mui/material").ButtonProps} props
+ */
+const SupportButton = function (props) {
+  return (
+    <Button
+      {...props}
+      sx={(tm) => ({
+        backgroundColor: alpha(tm.palette.background.primary, 0.2),
+        position: "fixed",
+        left: "27px",
+        bottom: "29px",
+        border: "1.4px solid",
+        borderColor: "border.primary",
+        color: "text.onPrimary",
+        ...identifySxProp(tm, props.sx),
+      })}
+    />
+  );
+};
+
+export default SupportButton;
