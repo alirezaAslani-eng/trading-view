4d52bc8502eaaf3d92c6e96669923ac74c1993dc
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-02 21:07:20 +0330

    feat(add form component)
    Developed the `UI` of basic authorization form

---
+++ src/components/template/Form/BaseAuthForm.jsx
@@ -0,0 +1,46 @@
+import { InputSelect } from "@/components/ui/Input/InputSelect";
+import InputText from "@/components/ui/Input/InputText";
+import {
+  FormLayout,
+  FormLayoutField,
+  FormLayoutFieldGroup,
+  FormLayoutLable,
+  FormLayoutSubmit,
+} from "@/components/ui/Layout/FormLayout";
+import React from "react";
+
+function BaseAuthForm() {
+  return (
+    <FormLayout>
+      <FormLayoutFieldGroup>
+        <FormLayoutField>
+          <FormLayoutLable>{"کد ملی"}</FormLayoutLable>
+          <InputText placeholder="کد ملی را وارد کنید" />
+        </FormLayoutField>
+        <FormLayoutField>
+          <FormLayoutLable>{"شماره موبایل"}</FormLayoutLable>
+          <InputText placeholder="شماره موبایل خود را وارد کنید" />
+        </FormLayoutField>
+      </FormLayoutFieldGroup>
+
+      <FormLayoutField>
+        <FormLayoutLable>{"تاریخ تولد"}</FormLayoutLable>
+        <FormLayoutFieldGroup>
+          <FormLayoutField>
+            <InputSelect placeholder="سال"></InputSelect>
+          </FormLayoutField>
+          <FormLayoutField>
+            <InputSelect placeholder="ماه"></InputSelect>
+          </FormLayoutField>
+          <FormLayoutField>
+            <InputSelect placeholder="روز"></InputSelect>
+          </FormLayoutField>
+        </FormLayoutFieldGroup>
+      </FormLayoutField>
+
+      <FormLayoutSubmit color="error">{"ثبت اطلاعات"}</FormLayoutSubmit>
+    </FormLayout>
+  );
+}
+
+export default BaseAuthForm;
