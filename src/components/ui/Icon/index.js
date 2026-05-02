c9510fed0373b1837a0319feabbbeb3b31370a5e
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-02 15:19:51 +0330

    feat(add icon)

--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -5,3 +5,4 @@ export { default as DownMinimalIcon } from "./DownMinimalIcon";
 export { default as ArrowUpDownIcon } from "./ArrowUpDownIcon";
 export { default as SearchIcon } from "./SearchIcon";
 export { default as CloseIcon } from "./CloseIcon";
+export { default as WarningIcon } from "./WarningIcon";
