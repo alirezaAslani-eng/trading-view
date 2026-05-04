a281f5674b5701dd25977cc854df6fdbd1a44a81
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-04 14:47:00 +0330

    feat(add component)
    A component which semanticly shows the result of a process like error success or warning but this component dosen't have any variant because its only used onec, if it will have differente theme or size, there will be specific variants for it

---
+++ src/components/ui/Feedback/StatusBadge.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { alpha, Box } from "@mui/material";
+import React from "react";
+import { CheckedIcon } from "../Icon";
+
+function StatusBadge() {
+  return (
+    <Box
+      sx={({ palette }) => ({
+        width: "88px",
+        aspectRatio: "1/1",
+        borderRadius: "999px",
+        backgroundColor: "transparent",
+        outline: "1px solid",
+        outlineColor: alpha(palette.text.profit, 0.12),
+        boxShadow: `0px 0px 10px ${alpha(palette.text.profit, 0.48)}`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      })}
+    >
+      <Box
+        sx={({ palette }) => ({
+          width: "60.56%",
+          aspectRatio: "1/1",
+          borderRadius: "999px",
+          backgroundColor: palette.text.profit,
+          outline: "10px solid",
+          outlineColor: alpha(palette.text.profit, 0.22),
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        })}
+      >
+        <CheckedIcon sx={{ width: "22px", height: "15px" }} />
+      </Box>
+    </Box>
+  );
+}
+
+export default StatusBadge;
