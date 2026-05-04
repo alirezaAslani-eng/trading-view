804e94a7519bc05ad9b757eb63ebda93d06beac9
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-04 15:38:11 +0330

    feat(add icon)

--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -8,3 +8,4 @@ export { default as CloseIcon } from "./CloseIcon";
 export { default as WarningIcon } from "./WarningIcon";
 export { default as CheckedIcon } from "./CheckedIcon";
 export { default as HeadPhoneIcon } from "./HeadPhoneIcon";
+export { default as UnlockIcon } from "./UnlockIcon";
