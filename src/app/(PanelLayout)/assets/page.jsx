78c9f4733d293840fc243247df341383d1f3b2ad
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-04 13:21:00 +0330

    refactor(page layout structure)
    Most of the pages are using this layout structure to avoid repeating styles

--- src/app/(PanelLayout)/assets/page.jsx
+++ src/app/(PanelLayout)/assets/page.jsx
@@ -1,66 +1,64 @@
 import PageHeader from "@/components/common/Appbar/PageHeader";
 import TotalAssetCard from "@/components/template/Card/TotalAssetCard";
 import ExchangeAssetForm from "@/components/template/Form/ExchangeAssetForm";
-import PanelContainer from "@/components/ui/Layout/PanelContainer";
-import PanelPaper from "@/components/ui/Paper/PanelPaper";
-import { Box, Typography } from "@mui/material";
-
+import { Box } from "@mui/material";
 import AssetCartTable from "@/components/template/Table/AssetCartTable";
 import OpenBuysTable from "@/components/template/Table/OpenBuysTable";
 import BuySellQueueCard from "@/components/template/Card/BuySellQueueCard";
+import {
+  Page,
+  Main,
+  Header,
+  Section,
+  SectionHeading,
+  SectionTitle,
+  SectionContent,
+} from "@/components/ui/Layout/PageLayout";
 const section_shared_sx = { mt: "50px" };
 function page() {
   return (
-    <PanelContainer>
-      <PageHeader title="دارایی‌ها" subtitle="نمای کلی سرمایه و سفارشات" />
-      <main>
-        <Box sx={{ mt: "41px", display: "flex", gap: "24px" }}>
-          {/* // * ----start------- Total Asset ----------- */}
-          <PanelPaper sx={{ p: "20px 16px 31px 16px", flex: "1 1 698px" }}>
-            <TotalAssetCard />
-          </PanelPaper>
-          {/* // * ----end------- Total Asset ----------- */}
+    <Page>
+      <Header>
+        <PageHeader title="دارایی‌ها" subtitle="نمای کلی سرمایه و سفارشات" />
+      </Header>
+      <Main>
+        <Section>
+          <SectionContent>
+            <Box sx={{ width: "63.4%", minHeight: "242px" }}>
+              <TotalAssetCard />
+            </Box>
+            <Box sx={{ width: "34.36%", minHeight: "242px" }}>
+              <ExchangeAssetForm />
+            </Box>
+          </SectionContent>
+        </Section>
 
-          {/* // * -----start------ Exchange Asset ----------- */}
-          <PanelPaper sx={{ p: "20px 16px 11px 16px", flex: "1 1 378px" }}>
-            <ExchangeAssetForm />
-          </PanelPaper>
-          {/* // * -----end------ Exchange Asset ----------- */}
-        </Box>
-        {/* // * ----- Table ----- */}
-        <Box sx={section_shared_sx}>
-          <Typography variant="h6" sx={{ color: "text.heading" }}>
-            {"لیست دارایی کالا"}
-          </Typography>
-          <Box sx={{ mt: "15px" }}>
-            <AssetCartTable />
-          </Box>
-        </Box>
+        <Section>
+          <SectionHeading>
+            <SectionTitle>{"لیست دارایی کالا"}</SectionTitle>
+          </SectionHeading>
+          <SectionContent>
+            <Box sx={{ minHeight: "332px", flex: 1 }}>
+              <AssetCartTable />
+            </Box>
+          </SectionContent>
+        </Section>
 
-        {/* // * ----- Table ----- */}
-        <Box sx={section_shared_sx}>
-          <Typography variant="h6" sx={{ color: "text.heading" }}>
-            {"لیست سفارشات باز"}
-          </Typography>
-          <Box
-            sx={{
-              mt: "15px",
-              display: "flex",
-              gap: "24px",
-              alignItems: "stretch",
-            }}
-          >
-            <Box sx={{ width: "75.90%" }}>
+        <Section>
+          <SectionHeading>
+            <SectionTitle>{"سفارش های باز"}</SectionTitle>
+          </SectionHeading>
+          <SectionContent>
+            <Box sx={{ width: "75.90%", minHeight: "318px" }}>
               <OpenBuysTable />
             </Box>
-
-            <Box sx={{ width: "21.90%" }}>
+            <Box sx={{ width: "21.90%", minHeight: "318px" }}>
               <BuySellQueueCard />
             </Box>
-          </Box>
-        </Box>
-      </main>
-    </PanelContainer>
+          </SectionContent>
+        </Section>
+      </Main>
+    </Page>
   );
 }
 
