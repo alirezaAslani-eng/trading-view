b17c6c1314d5519c2aef32b96e3ac450f35225ba
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-04 13:15:13 +0330

    feat(add layout component)
    A common layout for each page sections, it applies specific spacing and can be overrided
    
    - `Page` : Container of `header` and `main`
    - `Header` : header section of the page.
    - `Main` : main content will be inside this component.

---
+++ src/components/ui/Layout/PageLayout.jsx
@@ -0,0 +1,71 @@
+"use client";
+import { Box, styled, Typography } from "@mui/material";
+
+// * Container
+const Page = styled(Box)({ padding: "0px 28px" });
+
+// * Container -> Header
+/**
+ * @param {import("@mui/material").BoxProps<"header">} props
+ */
+const _Header = (props) => <Box component={"header"} {...props} />;
+const Header = styled(_Header)({
+  marginTop: "48px",
+});
+
+// * Container -> Main
+/**
+ * @param {import("@mui/material").BoxProps<"main">} props
+ */
+const _Main = (props) => <Box component={"main"} {...props} />;
+const Main = styled(_Main)({
+  marginTop: "42px",
+  paddingBottom: "42px",
+});
+
+// * Container -> Main -> Section
+/**
+ * @param {import("@mui/material").BoxProps<"section">} props
+ */
+const _Section = (props) => <Box component={"section"} {...props} />;
+const Section = styled(_Section)({
+  marginTop: "50px",
+  ":first-of-type": {
+    marginTop: "0px",
+  },
+});
+
+// * Container -> Main -> Section -> SectionContent
+const SectionContent = styled(Box)({
+  display: "flex",
+  gap: "24px",
+});
+
+// * Container -> Main -> Section -> SectionContent ->  SectionHeading
+const SectionHeading = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  marginBottom: "12px",
+});
+
+// * Container -> Main -> Section -> SectionContent -> SectionHeading -> Title
+const SectionTitle = styled(Typography)(({ theme }) => {
+  const { typography, palette } = theme;
+  return {
+    fontSize: typography.h6.fontSize,
+    fontFamily: typography.h6.fontFamily,
+    lineHeight: typography.h6.lineHeight,
+    color: palette.text.heading,
+  };
+});
+
+export {
+  Page,
+  Header,
+  Main,
+  Section,
+  SectionContent,
+  SectionHeading,
+  SectionTitle,
+};
