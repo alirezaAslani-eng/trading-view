85bb7f8351828b7b999ad85eda8cabb976bf0254
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-06 22:06:51 +0330

    feat(add type)
    created interface of custom typography variants

---
+++ src/packages/mui/theme/types/typography.types.ts
@@ -0,0 +1,15 @@
+import { TypographyStyle } from "@mui/material/styles";
+
+interface CustomTypographyVariants extends Record<
+  | "body3"
+  | "button1"
+  | "button2"
+  | "button3"
+  | "button4"
+  | "caption1"
+  | "caption2"
+  | "h7",
+  TypographyStyle
+> {}
+
+export type { CustomTypographyVariants };
