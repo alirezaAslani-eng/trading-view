9f24302fa3800704b9af799c7d0222d59a5ee8fd
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-06 19:11:55 +0330

    refactor(deleted not needed module)

--- src/packages/mui/theme/class-selectors/toggleButtonGroupSelectors.js
+++
@@ -1,9 +0,0 @@
-const activeToggleButtonSelector = "& .Mui-selected";
-const inactiveToggleButtonSelector = "& button:not(.Mui-selected)";
-const allToggleButtonsSelector = "& button";
-
-export {
-  activeToggleButtonSelector,
-  allToggleButtonsSelector,
-  inactiveToggleButtonSelector,
-};
