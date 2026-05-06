5f8e8b382d2f627bc407d676f761f68999d3e5d5
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-06 19:49:50 +0330

    feat(add utility type)

---
+++ src/types/utils.ts
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+type ParamsType<
+  TFunction extends Function,
+  ParamIndex extends number | `${number}` = 0,
+> = TFunction extends (...params: infer P) => any
+  ? //@ts-ignore
+    P[ParamIndex]
+  : any;
+
+/**
+ * PWC = PropsWithChildren
+ */
+type PWC<T extends object = object, TChildren = ReactNode> = T & {
+  children: TChildren;
+};
+export type { ParamsType, PWC };
