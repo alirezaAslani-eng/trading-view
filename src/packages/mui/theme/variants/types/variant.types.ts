f5135109ee148996d8495e72786b6e6bc41ad637
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-07 00:05:51 +0330

    feat(add type)
    Added type of variants like size, color

---
+++ src/packages/mui/theme/variants/types/variant.types.ts
@@ -0,0 +1,6 @@
+
+type Sizes = "small" | "medium" | "large";
+type Colors = "success" | "nuteral" | "primary";
+type Variants = "contained" | "outlined" | "text";
+
+export type { Sizes, Variants, Colors };
