78cef31e040cd1fce8c2a0ed27573d73d72b5288
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-06 10:55:34 +0330

    refactor(scroll container component)
    This new code structure allows the component to apply the style from `overflowedSx` when scroll bar is visible

--- src/components/ui/ScrollContainer/ScrollContainer.jsx
+++ src/components/ui/ScrollContainer/ScrollContainer.jsx
@@ -1,12 +1,28 @@
 "use client";
+import useCheckOverflow from "@/hooks/app/useCheckOverflow";
+import { identifySxProp } from "@/packages/mui/theme/helpers";
 import { nuteralScrollbar } from "@/packages/mui/theme/shared-style";
-import { styled } from "@mui/material";
 import { Box } from "@mui/system";
-const ScrollContainer = styled(Box)(({ theme }) => {
-  return {
-    overflow: "auto",
-    ...nuteralScrollbar(theme),
-  };
-});
+/**
+ * @param {import("@mui/system").BoxProps & {overflowedSx:import("@mui/material").SxProps<import("@mui/material").Theme>}} props
+ */
+function ScrollContainer({ overflowedSx, ...props }) {
+  const { isOverflowing, scrollContainerRef } = useCheckOverflow();
+  return (
+    <Box
+      {...props}
+      ref={scrollContainerRef}
+      sx={(tm) => {
+        return {
+          overflow: "auto",
+          transition: "all ease 150ms",
+          ...nuteralScrollbar(tm),
+          ...identifySxProp(tm, props?.sx),
+          ...(isOverflowing && identifySxProp(tm, overflowedSx)),
+        };
+      }}
+    />
+  );
+}
 
 export default ScrollContainer;
