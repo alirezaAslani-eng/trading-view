800d866e731a9e26e173db47bb3f5a7cfd05b578
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-07 10:42:21 +0330

    refactor(JS to TS)
    add as const to make types of these object literal and not mutatable

--- src/packages/mui/theme/variants/defaultVariants.ts
+++ src/packages/mui/theme/variants/defaultVariants.ts
@@ -2,18 +2,18 @@ const inputDefaultVariants = {
   size: "medium",
   color: "primary",
   variant: "contained",
-};
+} as const;
 const defaultToggleGroupButtonVariants = {
   variant: "contained",
-};
+} as const;
 const defaultCheckboxVariants = {
   variant: "contained",
   color: "primary",
   size: "medium",
-};
+} as const;
 const defaultAlertVariants = {
   size: "small",
-};
+} as const;
 
 export {
   inputDefaultVariants,
