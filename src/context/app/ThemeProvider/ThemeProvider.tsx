bad1300431e17681adbba08367ac688710b7c573
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-09 22:12:12 +0330

    refactor(JS to TS)
    Refactored th Provider component `ThemeProvider` to TS and modularized its types

--- src/context/app/ThemeProvider/ThemeProvider.tsx
+++ src/context/app/ThemeProvider/ThemeProvider.tsx
@@ -2,10 +2,10 @@
 
 import { createTheme, ThemeProvider as MuiThemeProvider } from "@mui/material";
 import { theme } from "@/packages/mui/theme";
-/**
- * @param {{children:import('react').ReactNode,overrideTheme:import('@mui/material').ThemeOptions}} param0
- */
-function ThemeProvider({ children, overrideTheme }) {
+import { PWC } from "@/types/utils";
+import { ThemeProviderProps } from "@/context/app/ThemeProvider/types";
+
+function ThemeProvider({ children, overrideTheme }: PWC<ThemeProviderProps>) {
   const createdTheme = createTheme(overrideTheme ?? theme);
   return <MuiThemeProvider theme={createdTheme}>{children}</MuiThemeProvider>;
 }
