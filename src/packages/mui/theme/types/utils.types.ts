2788643f6eeda9a2d201b6481590708314009666
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-10 11:14:37 +0330

    feat(add utility type)
    This utility wll be used for when we want to add a global MuiOverrider like `MuiButton`

--- src/packages/mui/theme/types/utils.types.ts
+++ src/packages/mui/theme/types/utils.types.ts
@@ -1,7 +1,10 @@
+import type { Components, Theme } from "@mui/material";
 import { CustomPalette } from ".";
-
 type PartialPalette<Palette extends CustomPalette> = {
   [key in keyof Palette]?: Partial<Palette[key]>;
 };
 
-export type { PartialPalette };
+type MuiOverriderType<TComponent extends keyof Components<Theme>> =
+  Components<Theme>[TComponent];
+
+export type { PartialPalette, MuiOverriderType };
