7f18479e17e2580c6ecd640afaa58648f955ed52
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-11 23:21:00 +0330

    refactor(form component)
    used the layout component `LayoutForm` instead to improve consistency

--- src/components/template/Form/VerifyAuthForm.tsx
+++ src/components/template/Form/VerifyAuthForm.tsx
@@ -1,48 +1,27 @@
-"use client";
-import { identifySxProp } from "@/packages/mui/theme/helpers";
-import {
-  Box,
-  Button,
-  FormLabel,
-  Stack,
-  StackProps,
-  SvgIcon,
-  Typography,
-} from "@mui/material";
-import React from "react";
-import useRndomID from "@/hooks/app/useRndomID";
+import { Box, Typography } from "@mui/material";
 import { notDefinedColors } from "@/packages/mui/theme/shades";
 import NextLink from "@/components/ui/Link/NextLink";
 import InputVerifyCode from "@/components/template/Input/InputVerifyCode";
 import SendAuthOTP from "@/components/template/Button/SendAuthOTP";
-import RotateRight from "@/assets/svg/rotate-right.svg";
+import {
+  FormLayout,
+  FormLayoutField,
+  FormLayoutLable,
+  FormLayoutSubmit,
+} from "@/components/ui/Layout/FormLayout";
+import { RestartRightIcon } from "@/components/ui/Icon";
 
-function VerifyAuthForm({ sx }: Pick<StackProps, "sx">) {
-  const verifyInputLabelID = useRndomID();
+function VerifyAuthForm() {
   return (
-    <Stack
-      sx={(tm) => ({
-        px: "calc(50px - 18px)",
-        mt: "56px",
-        width: "100%",
-        alignItems: "center",
-        ...identifySxProp(tm, sx),
-      })}
-    >
-      <Box sx={{ px: "14.5px" }}>
-        <FormLabel htmlFor={verifyInputLabelID}>
-          <Typography variant="button2" sx={{ color: "text.onPrimary" }}>
-            {"کد تایید"}
-          </Typography>
-        </FormLabel>
+    <>
+      <FormLayout sx={{ mt: "56px" }}>
+        <FormLayoutField sx={{ px: "14.5px" }}>
+          <FormLayoutLable>{"کد تایید"}</FormLayoutLable>
+          <InputVerifyCode />
+        </FormLayoutField>
 
-        <InputVerifyCode sx={{ mt: "10px" }}  />
-      </Box>
-
-      <Button variant="contained" size="large" fullWidth sx={{ mt: "32px" }}>
-        {"تایید و ادامه"}
-      </Button>
-
+        <FormLayoutSubmit>{"تایید و ادامه"}</FormLayoutSubmit>
+      </FormLayout>
       <Box
         sx={{
           width: "100%",
@@ -62,13 +41,11 @@ function VerifyAuthForm({ sx }: Pick<StackProps, "sx">) {
         </NextLink>
 
         <SendAuthOTP>
-          <SvgIcon>
-            <RotateRight />
-          </SvgIcon>
+          <RestartRightIcon sx={{ color: "inherit" }} />
           {"اصلاح شماره موبایل"}
         </SendAuthOTP>
       </Box>
-    </Stack>
+    </>
   );
 }
 
