aa8171c07e04eda583df09d969e8e9caeb69dd7a
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-11 22:59:21 +0330

    feat(add component)
    The component `AuthFormLayoutContainer` just apply a padding right and left on the Form component

---
+++ src/components/template/Layout/AuthFormLayout/AuthFormLayoutContainer.tsx
@@ -0,0 +1,13 @@
+import { ReplaceSxWithSxOnlyObject } from "@/packages/mui/theme/types";
+import { Box, BoxProps } from "@mui/material";
+
+function AuthFormLayoutContainer(props: ReplaceSxWithSxOnlyObject<BoxProps>) {
+  return (
+    <Box
+      {...props}
+      sx={{ px: "calc(50px - 18px)", width: "100%", ...props.sx }}
+    />
+  );
+}
+
+export default AuthFormLayoutContainer;
--- src/components/template/Layout/AuthFormLayout/AuthFormLayoutToggleButoon.tsx
+++ src/components/template/Layout/AuthFormLayout/AuthFormLayoutToggleButoon.tsx
@@ -1,46 +1,38 @@
-"use client";
 import ToggleButtonGroup from "@/components/ui/ButtonGroup/ToggleButtonGroup";
 import NextLink from "@/components/ui/Link/NextLink";
-import { identifySxProp } from "@/packages/mui/theme/helpers";
-import { Box, BoxProps, ToggleButton } from "@mui/material";
+import { ToggleButton } from "@mui/material";
 
-interface AuthFormLayoutToggleButoonProps extends Pick<BoxProps, "sx"> {
+interface AuthFormLayoutToggleButoonProps {
   activeButton: "signin" | "signup";
 }
 
-
 export default function AuthFormLayoutToggleButoon({
-  sx,
   activeButton,
 }: AuthFormLayoutToggleButoonProps) {
   return (
-    <Box
-      sx={(tm) => ({ px: "50px", width: "100%", ...identifySxProp(tm, sx) })}
+    <ToggleButtonGroup
+      value={activeButton}
+      size="large"
+      color="primary"
+      fullWidth
+      sx={{
+        border: "1.4px solid",
+        borderColor: "border.default",
+        borderRadius: "50px",
+        padding: "4px",
+        gap: "15px",
+      }}
     >
-      <ToggleButtonGroup
-        value={activeButton}
-        size="large"
-        color="primary"
-        fullWidth
-        sx={({ palette }) => ({
-          border: "1.4px solid",
-          borderColor: palette.border.default,
-          borderRadius: "50px",
-          padding: "4px",
-          gap: "15px",
-        })}
-      >
-        <NextLink href={"/signin"} sx={{ flex: 1 }}>
-          <ToggleButton value="signin" fullWidth>
-            {"ورود"}
-          </ToggleButton>
-        </NextLink>
-        <NextLink href={"/signup"} sx={{ flex: 1 }}>
-          <ToggleButton value="signup" fullWidth>
-            {"ثبت نام"}
-          </ToggleButton>
-        </NextLink>
-      </ToggleButtonGroup>
-    </Box>
+      <NextLink href={"/signin"} sx={{ flex: 1 }}>
+        <ToggleButton value="signin" fullWidth>
+          {"ورود"}
+        </ToggleButton>
+      </NextLink>
+      <NextLink href={"/signup"} sx={{ flex: 1 }}>
+        <ToggleButton value="signup" fullWidth>
+          {"ثبت نام"}
+        </ToggleButton>
+      </NextLink>
+    </ToggleButtonGroup>
   );
 }
