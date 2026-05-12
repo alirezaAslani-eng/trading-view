bd0a89c03494071d1d10ad6befe53993b18cfe4b
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-12 16:18:04 +0330

    feat(add component)
    This component is only used once, for a specific case, so it has a specific feature based name and structure, but in the future it might changes.

---
+++ src/components/ui/SiklGuides/SiklGuides.tsx
@@ -0,0 +1,40 @@
+import { alpha, Box, Stack, Typography } from "@mui/material";
+import { CircleIcon } from "../Icon";
+import { SiklGuide } from "@/constant/features/transaction/types";
+
+interface SiklGuidesProps {
+  title: string;
+  guides?: Partial<SiklGuide>[];
+}
+function SiklGuides({ title, guides }: SiklGuidesProps) {
+  return (
+    <Box
+      sx={({ palette }) => ({
+        border: "1px solid",
+        borderColor: alpha(palette.border.white, 0.18),
+        borderRadius: "6px",
+        padding: "8px",
+        color: "text.secondary",
+      })}
+    >
+      <Typography variant="body2" sx={{ color: "inherit" }}>
+        {title}
+      </Typography>
+      <Stack sx={{ mt: "20px" }}>
+        {!!guides?.length &&
+          guides.map(({ guid, id }) => {
+            return (
+              <Box sx={{ display: "flex", gap: "6px" }} key={id}>
+                <CircleIcon sx={{ width: "4px", height: "4px", mt: "10px" }} />
+                <Typography variant="body2" sx={{ color: "inherit" }}>
+                  {guid}
+                </Typography>
+              </Box>
+            );
+          })}
+      </Stack>
+    </Box>
+  );
+}
+
+export default SiklGuides;
