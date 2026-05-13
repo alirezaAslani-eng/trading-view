dcd3836be5d5ea41f7f2700cec4dd84c35f18922
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-13 09:12:58 +0330

    feat(add component)
    Created a shared layout component for `withdraw` and `deposit` forms, they have smae spacing and structure.

---
+++ src/components/ui/Layout/TransactionFormLayout.tsx
@@ -0,0 +1,69 @@
+"use client";
+import {
+  Box,
+  Button,
+  ButtonProps,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
+
+const TransactionFormLayout = styled(Stack)({
+  alignItems: "center",
+  width: "100%",
+});
+const TransactionFormLayoutFieldContainer = styled(Stack)({
+  marginTop: "24px",
+  width: "100%",
+  gap: "4px",
+});
+
+const TransactionFormLayoutFieldAutoFiller = styled("button")(({ theme }) => {
+  const { typography, palette } = theme;
+  return {
+    fontSize: typography.button4.fontSize,
+    fontFamily: typography.button4.fontFamily,
+    lineHeight: typography.button4.lineHeight,
+    color: palette.text.primary2,
+    alignSelf: "end",
+    cursor: "pointer",
+  };
+});
+
+const TransactionFormLayoutField = styled(Box)({
+  display: "flex",
+  width: "100%",
+  alignItems: "center",
+  gap: "8px",
+});
+
+const TransactionFormLayoutLabel = styled(Typography)(({ theme }) => {
+  const { palette, typography } = theme;
+  return {
+    minWidth: "90px",
+    fontFamily: typography.button2.fontFamily,
+    fontSize: typography.button2.fontSize,
+    color: palette.text.onPrimary,
+    display: "flex",
+    justifyContent: "end",
+  };
+});
+
+const Button_ = (props: ButtonProps) => (
+  <Button variant="outlined" color="primary" size="medium" {...props} />
+);
+const TransactionFormLayoutSubmit = styled(Button_)(({ theme }) => {
+  return {
+    marginTop: "56px",
+    gap: "6px",
+  };
+});
+
+export {
+  TransactionFormLayout,
+  TransactionFormLayoutFieldContainer,
+  TransactionFormLayoutFieldAutoFiller,
+  TransactionFormLayoutField,
+  TransactionFormLayoutLabel,
+  TransactionFormLayoutSubmit,
+};
