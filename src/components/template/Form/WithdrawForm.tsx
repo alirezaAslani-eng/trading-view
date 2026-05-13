d0b7613ba6a4efcd536f598396000f9ab9271e45
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-13 09:31:12 +0330

    feat(add Form component)
    Develeoped the UI of a form component that allows user to withdraw from their wallet

---
+++ src/components/template/Form/WithdrawForm.tsx
@@ -0,0 +1,48 @@
+import { AddIcon } from "@/components/ui/Icon";
+import { InputSelect } from "@/components/ui/Input/InputSelect";
+import {
+  TransactionFormLayout,
+  TransactionFormLayoutField,
+  TransactionFormLayoutFieldAutoFiller,
+  TransactionFormLayoutFieldContainer,
+  TransactionFormLayoutLabel,
+  TransactionFormLayoutSubmit,
+} from "@/components/ui/Layout/TransactionFormLayout";
+
+function WithdrawForm() {
+  return (
+    <TransactionFormLayout>
+      <TransactionFormLayoutField>
+        <TransactionFormLayoutLabel>{"واریز به"}</TransactionFormLayoutLabel>
+        <InputSelect
+          sx={{ flex: 1 }}
+          size="medium"
+          placeholder="لطفا شماره شبای خود را انتخاب کنید"
+        />
+      </TransactionFormLayoutField>
+
+      <TransactionFormLayoutFieldContainer>
+        <TransactionFormLayoutFieldAutoFiller>
+          {"برداشت کل موجودی"}
+        </TransactionFormLayoutFieldAutoFiller>
+        <TransactionFormLayoutField>
+          <TransactionFormLayoutLabel>
+            {"مبلغ مورد نظر"}
+          </TransactionFormLayoutLabel>
+          <InputSelect
+            sx={{ flex: 1 }}
+            size="medium"
+            placeholder="مبلغ مورد نظر را به تومان وارد کنید"
+          />
+        </TransactionFormLayoutField>
+      </TransactionFormLayoutFieldContainer>
+
+      <TransactionFormLayoutSubmit>
+        <AddIcon sx={{ color: "inherit" }} />
+        {"افزودن شبای جدید"}
+      </TransactionFormLayoutSubmit>
+    </TransactionFormLayout>
+  );
+}
+
+export default WithdrawForm;
