c15fce4ebfc2f5be9560bf99742916fbe09346b6
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-13 11:08:47 +0330

    feat(add shared style)
    A shared style for the container component of bullet Item(s)

---
+++ src/packages/mui/theme/shared-style/app/warningBulletItemsContainerSx.ts
@@ -0,0 +1,10 @@
+import { SxPropOnlyObject } from "@/packages/mui/theme/types";
+
+const warningBulletItemsContainerSx: SxPropOnlyObject = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px", // * space between bullet items
+  mt: "32px",
+};
+
+export default warningBulletItemsContainerSx;
--- src/packages/mui/theme/shared-style/index.js
+++ src/packages/mui/theme/shared-style/index.js
@@ -1,6 +1,7 @@
 // * App -> shared styles
 export { default as hideScrollBar } from "./app/hideScrollBar";
 export { default as nuteralScrollbar } from "./app/nuteralScrollbar";
+export { default as warningBulletItemsContainerSx } from "./app/warningBulletItemsContainerSx";
 
 // * Features -> shared styles
 export { default as transactionPageFormSectionSx } from "./features/transaction/transactionPageFormSectionSx";
