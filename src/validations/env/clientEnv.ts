684dc87e12c5d127dc330e7d895c55ae79926a82
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-16 16:16:35 +0330

    feat(add env variable validator)

--- src/validations/env/clientEnv.ts
+++ src/validations/env/clientEnv.ts
@@ -1,13 +1,17 @@
 import { object, string } from "zod";
 import type { Infer } from "zod";
+import userBaseSchema from "../user/userBaseSchema";
 
 const clientEnvScheam = object({
   NEXT_PUBLIC_BASEURL: string().url(),
+  NEXT_PUBLIC_USER_IDENTIFIER: userBaseSchema.shape.phone,
 });
 
 // * ---- Validation ----
+const envObj = process.env;
 const validationResult = clientEnvScheam.safeParse({
-  NEXT_PUBLIC_BASEURL: process.env.NEXT_PUBLIC_BASEURL as string,
+  NEXT_PUBLIC_BASEURL: envObj.NEXT_PUBLIC_BASEURL as string,
+  NEXT_PUBLIC_USER_IDENTIFIER: envObj.NEXT_PUBLIC_USER_IDENTIFIER as string,
 } satisfies Infer<typeof clientEnvScheam>);
 
 if (!validationResult.success)
