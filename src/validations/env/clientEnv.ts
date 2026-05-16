2592c146aea530ddb0055649266594ea96248799
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-16 10:43:38 +0330

    refactor(enhance type safety)
    `satisfies Infer<typeof clientEnvScheam>` to gain Typescript Intellisense and type checking

--- src/validations/env/clientEnv.ts
+++ src/validations/env/clientEnv.ts
@@ -1,4 +1,5 @@
 import { object, string } from "zod";
+import type { Infer } from "zod";
 
 const clientEnvScheam = object({
   NEXT_PUBLIC_BASEURL: string().url(),
@@ -6,8 +7,8 @@ const clientEnvScheam = object({
 
 // * ---- Validation ----
 const validationResult = clientEnvScheam.safeParse({
-  NEXT_PUBLIC_BASEURL: process.env.NEXT_PUBLIC_BASEURL,
-});
+  NEXT_PUBLIC_BASEURL: process.env.NEXT_PUBLIC_BASEURL as string,
+} satisfies Infer<typeof clientEnvScheam>);
 
 if (!validationResult.success)
   console.error("Env validation error : " + validationResult.error.message);
