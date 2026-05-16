7edda7ed0d889f1376a6d6dbdcbff657ae0e1a01
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-16 20:50:22 +0330

    fix(rename module)
    queryOptions to mutationOptions

--- src/packages/react-query/index.ts
+++ src/packages/react-query/index.ts
@@ -1,5 +1,5 @@
 // * From Core
 export { default as queryClient } from "./core/queryClient";
 export { default as QueryClientProvider } from "./core/QueryClientProvider";
-export * from "./config/queryOptions";
+export * from "./config/mutationOptions";
 export * from "./keys/mutationKeys";
