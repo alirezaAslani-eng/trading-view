a384597967dde43a1d7d9c498f40caf4e0902acc
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-17 23:29:20 +0330

    refactor(error handling)
    Refactored the way the API requestAuthOTP handles error using two utilites `fetchHandler` and `responseErrorHandler` to throw a specific error structure

--- src/api/auth/requestAuthOTP.ts
+++ src/api/auth/requestAuthOTP.ts
@@ -1,24 +1,30 @@
 import clientEnv from "@/validations/env/clientEnv";
-import { BaseApiResponse } from "@/types";
 import { requestAuthOTPSchemaType } from "@/validations/types";
+import throwError from "@/utils/app/throwError";
+import ResponseError from "@/utils/app/ResponseError";
+import fetchHandler from "@/utils/app/fetchHandler";
+import responseErrorHandler from "@/utils/app/responsiveErrorHandler";
 
-async function requestAuthOTP(
-  body: requestAuthOTPSchemaType,
-): Promise<BaseApiResponse> {
-  const res = await fetch(
-    `${clientEnv?.NEXT_PUBLIC_BASEURL}/api/v1/auth/send-otp`,
-    {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    },
-  );
+async function requestAuthOTP(body: requestAuthOTPSchemaType): Promise<void> {
+  try {
+    const res = (await fetchHandler(async () => {
+      const res = await fetch(
+        `${clientEnv?.NEXT_PUBLIC_BASEURL}/api/v1/auth/send-otp`,
+        {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      return res;
+    })) as Response;
 
-  const data = (await res.json()) as BaseApiResponse;
-
-  return data;
+    await responseErrorHandler(res);
+  } catch (err) {
+    throwError(true, err as ResponseError);
+  }
 }
 
 export default requestAuthOTP;
