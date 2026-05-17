9820234808fb307bd024cd3074fa10a6a00340d2
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-17 16:09:55 +0330

    feat(add icon)

--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -17,3 +17,4 @@ export { default as TrashIcon } from "./TrashIcon";
 export { default as PenOnPaperIcon } from "./PenOnPaperIcon";
 export { default as RestartRightIcon } from "./RestartRightIcon";
 export { default as UserIcon } from "./UserIcon";
+export { default as MenueHorizontal } from "./MenueHorizontal";
