407e88ae9720f83b11052aa0cb20aec13781bc0b
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-17 23:31:55 +0330

    fix(mutation option type)
    Defined the error type of requestAuthOTP mutation

--- src/packages/react-query/config/mutationOptions.ts
+++ src/packages/react-query/config/mutationOptions.ts
@@ -1,9 +1,11 @@
 import { requestAuthOTP } from "@/api";
 import { mutationOptions } from "@tanstack/react-query";
 import { requestAuthOTPKey } from "@/packages/react-query/keys/mutationKeys";
+import { ResponseErrorType } from "@/types";
+import { requestAuthOTPSchemaType } from "@/validations/types";
 
 const requestAuthOTPConfig = () => {
-  return mutationOptions({
+  return mutationOptions<void, ResponseErrorType, requestAuthOTPSchemaType>({
     mutationKey: requestAuthOTPKey,
     mutationFn: requestAuthOTP,
   });
