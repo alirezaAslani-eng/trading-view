2067daa4b251d6cc256d574de0766a06fadc9698
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-18 09:44:08 +0330

    fix(schema validation)
    the field `identifier` is required

--- src/validations/auth/verifyAuthOTPSchema.ts
+++ src/validations/auth/verifyAuthOTPSchema.ts
@@ -1,7 +1,9 @@
 import { object, string } from "zod";
+import userBaseSchema from "../user/userBaseSchema";
 
 const verifyAuthOTPSchema = object({
   code: string().length(6),
+  identifier: userBaseSchema.shape.phone,
 });
 
 export default verifyAuthOTPSchema;
