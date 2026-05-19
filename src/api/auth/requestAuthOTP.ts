43e3a1e94cbada41fc8d74ddbe27ad5c3ac74a21
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-19 11:53:40 +0330

    refactor(removed unecessary code)
    try and catch is not required because `fetchHandler`, `responseErrorHandler` and `jsonParseHandler` automaticly throws a structured error, so no need to catch and throw it again

--- src/api/auth/requestAuthOTP.ts
+++ src/api/auth/requestAuthOTP.ts
@@ -1,31 +1,25 @@
 import clientEnv from "@/validations/env/clientEnv";
 import { requestAuthOTPSchemaType } from "@/validations/types";
-import throwError from "@/utils/app/throwError";
-import ResponseError from "@/utils/app/ResponseError";
 import fetchHandler from "@/utils/app/fetchHandler";
 import responseErrorHandler from "@/utils/app/responsiveErrorHandler";
 import mutationFetch from "@/utils/app/mutationFetch";
 
 async function requestAuthOTP(body: requestAuthOTPSchemaType): Promise<void> {
-  try {
-    const res = (await fetchHandler(async () => {
-      const res = await mutationFetch(
-        `${clientEnv?.NEXT_PUBLIC_BASEURL}/api/v1/auth/send-otp`,
-        {
-          method: "POST",
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json",
-          },
+  const res = (await fetchHandler(async () => {
+    const res = await mutationFetch(
+      `${clientEnv?.NEXT_PUBLIC_BASEURL}/api/v1/auth/send-otp`,
+      {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
-      return res;
-    })) as Response;
+      },
+    );
+    return res;
+  })) as Response;
 
-    await responseErrorHandler(res);
-  } catch (err) {
-    throwError(true, err as ResponseError);
-  }
+  await responseErrorHandler(res);
 }
 
 export default requestAuthOTP;
