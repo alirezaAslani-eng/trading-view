36cab5233f1a57578714509298dca9af3c93fb4a
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-19 10:46:05 +0330

    refactor(removed CSRF token mechanism)
    The team desided to remove CSRF mechanism, but this mechanism might come back later.

--- src/utils/app/mutationFetch.ts
+++ src/utils/app/mutationFetch.ts
@@ -1,19 +1,7 @@
-import clientEnv from "@/validations/env/clientEnv";
-import jsonParseHandler from "./jsonParseHandler";
 const mutationFetch: typeof fetch = async (url, requestInit) => {
-  // * -------- CSRF Token ----------
-  const csrf_res = await fetch(
-    `${clientEnv?.NEXT_PUBLIC_BASEURL}/api/v1/auth/csrf-token`,
-  );
-  const csrf_token = (await jsonParseHandler<string>(csrf_res)) as string;
-
-  // * -------- Mutation ----------
   const res = await fetch(url, {
     ...requestInit,
-    headers: {
-      ...requestInit?.headers,
-      "X-XSRF-TOKEN": csrf_token,
-    },
+    credentials: "include",
   });
   return res;
 };
