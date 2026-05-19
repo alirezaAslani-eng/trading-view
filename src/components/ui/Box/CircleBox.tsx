d72d16cc047cb7c973487a05d4ec7ed0426cf266
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-19 16:50:06 +0330

    feat(add component)
    A box component that is by default circle

---
+++ src/components/ui/Box/CircleBox.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Box, styled } from "@mui/material";
+
+const CircleBox = styled(Box)({
+  width: "44px",
+  aspectRatio: "1/1",
+  borderRadius: "999px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+export default CircleBox;
