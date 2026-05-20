713414638999dee14cf0ab0bf2614bf056a4e338
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-20 16:40:08 +0330

    feat(add page)
    Developed the UI of `user-profile-info` page

--- src/constant/app/sidebarNavigators.jsx
+++ src/constant/app/sidebarNavigators.jsx
@@ -6,7 +6,7 @@ import ChartIcon from "@/assets/svg/chart.svg";
 import WalletIcon from "@/assets/svg/wallet.svg";
 import MessageIcon from "@/assets/svg/message.svg";
 import SettingIcon from "@/assets/svg/setting.svg";
-import { BoxOutlinedIcon, LockIcon } from "@/components/ui/Icon";
+import { BoxOutlinedIcon, LockIcon, UserIcon } from "@/components/ui/Icon";
 const sidebarNavigators = [
   {
     text: "داشبورد",
@@ -87,6 +87,13 @@ const sidebarNavigators = [
     text: "سطوح دسترسی",
     link: "/admin/permissions/admin",
     icon: <LockIcon />,
+    id: crypto.randomUUID(),
+    submenus: [],
+  },
+  {
+    text: "پروفایل",
+    link: "/panel/profile/my-info",
+    icon: <UserIcon />,
     id: crypto.randomUUID(),
     submenus: [],
   },
