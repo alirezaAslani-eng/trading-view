fb669e55a0886202fd38d1d6db1e0d27b2a52bc7
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-20 11:58:08 +0330

    refactor(rename property)
    This naming pattern is compatible with the defined types.

--- src/constant/features/transaction/transactionLimits.ts
+++ src/constant/features/transaction/transactionLimits.ts
@@ -1,15 +1,17 @@
+import { TransactionLimits } from "@/constant/features/types";
+
 const transactionLimits = {
-  kycL1: {
+  Level1_Basic: {
     maxDepositPerDay: 25_000_000,
     maxDepositPerMonth: 750_000_000,
     maxWithdrawPerDay: 3_000_000,
     maxWithdrawPerMonth: 20_000_000,
   },
-  kycL2: {
+  Level2_Advanced: {
     maxDepositPerDay: 50_000_000,
     maxDepositPerMonth: 900_000_000,
     maxWithdrawPerDay: 10_000_000,
     maxWithdrawPerMonth: 100_000_000,
   },
-};
+} satisfies TransactionLimits;
 export { transactionLimits };
