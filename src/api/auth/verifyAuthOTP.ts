c436f6a4619e9f2610eb04c95155c6de1b10530c
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-21 09:32:35 +0330

    refactor(update API code)
    The `URL` and `body` content of this request has changed.

--- src/api/auth/verifyAuthOTP.ts
+++ src/api/auth/verifyAuthOTP.ts
@@ -1,25 +1,17 @@
 import fetchHandler from "@/utils/app/fetchHandler";
+import jsonParseHandler from "@/utils/app/jsonParseHandler";
 import responseErrorHandler from "@/utils/app/responsiveErrorHandler";
 import clientEnv from "@/validations/env/clientEnv";
 import { VerifyAuthOTPSchemaType } from "@/validations/types";
 
-const staticBody = {
-  grant_type: "otp",
-  client_id: "react-app",
-  client_secret: "secret",
-  scope: "openid profile kyc offline_access",
-};
-const URL = `${clientEnv?.NEXT_PUBLIC_BASEURL}/connect/token`;
+const URL = `${clientEnv?.NEXT_PUBLIC_BASEURL}/api/v1/auth/login-cookie`;
 
 // TODO -> The type of fetched data must be defined by TS
 async function verifyAuthOTP(body: VerifyAuthOTPSchemaType): Promise<unknown> {
   const res = (await fetchHandler(async () => {
     const res = await fetch(URL, {
       method: "POST",
-      body: JSON.stringify({
-        ...staticBody,
-        ...body,
-      }),
+      body: JSON.stringify(body),
       headers: {
         "Content-Type": "application/json",
       },
@@ -29,7 +21,7 @@ async function verifyAuthOTP(body: VerifyAuthOTPSchemaType): Promise<unknown> {
 
   await responseErrorHandler(res);
 
-  const data = await res.json();
+  const data = await jsonParseHandler(res);
 
   return data;
 }
