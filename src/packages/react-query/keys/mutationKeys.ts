88b10c57207718a18900bcdfde169b82a8e38bf6
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-21 09:56:01 +0330

    refactor(rename query key)
    The query key ["auth-otp"] didn't make sence what the mutation dose.

--- src/packages/react-query/keys/mutationKeys.ts
+++ src/packages/react-query/keys/mutationKeys.ts
@@ -1,4 +1,4 @@
-const requestAuthOTPKey = ["auth-otp"];
+const requestAuthOTPKey = ["request-auth-otp"];
 const verifyAuthOTPKey = ["verify-auth-otp"];
 
 export { requestAuthOTPKey, verifyAuthOTPKey };
