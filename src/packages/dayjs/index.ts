80ca0551b133d327d19499f97a670ce0d8775921
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-23 17:54:31 +0330

    feat(dayjs)
    Added a hook that updates date and make re-render,

--- src/packages/dayjs/index.ts
+++ src/packages/dayjs/index.ts
@@ -1,3 +1,5 @@
 export { default as convertToGregory } from "./helpers/convertToGregory";
 export { default as convertToJalali } from "./helpers/convertToJalali";
+export { default as useDate } from "./hooks/useDate";
 export * from "./core";
+export type * from "./hooks/types";
