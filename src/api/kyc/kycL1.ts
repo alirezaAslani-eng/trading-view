452f5a901a7d50768b63b536565e8204e03b09ec
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-23 13:41:57 +0330

    feat(type)
    Defined the response type for kycL1 API

--- src/api/kyc/kycL1.ts
+++ src/api/kyc/kycL1.ts
@@ -5,10 +5,7 @@ import { KycL1SchemaType } from "@/validations/types";
 
 const URL = `${clientEnv?.NEXT_PUBLIC_BASEURL}/api/v1/kyc/level1`;
 
-// TODO -> Response type must be defined by TS
-async function kycL1(body: KycL1SchemaType): Promise<unknown> {
-
-
+async function kycL1(body: KycL1SchemaType): Promise<void> {
   const res = (await fetchHandler(async () => {
     const res = await fetch(URL, {
       method: "POST",
@@ -20,13 +17,7 @@ async function kycL1(body: KycL1SchemaType): Promise<unknown> {
     return res;
   })) as Response;
 
-
-
   await responseErrorHandler(res);
-
-  const data = await res.json();
-
-  return data;
 }
 
 export default kycL1;
