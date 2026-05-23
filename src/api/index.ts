00b69aa242dc1b2843bb9e38adf6112d69828cc9
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-23 15:42:15 +0330

    feat(barrel file)
    Added missed export

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export { default as requestAuthOTP } from "./auth/requestAuthOTP";
 export { default as verifyAuthOTP } from "./auth/verifyAuthOTP";
 export { default as kycStatus } from "./kyc/kycStatus";
+export { default as kycL1 } from "./kyc/kycL1";
