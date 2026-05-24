a3fb7e17871e3de4014775b35f738b4b2d4cbb2e
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-24 13:48:42 +0330

    feat(react query)
    Added query key and options for KYC level 1 mutation

--- src/packages/react-query/config/mutationOptions.ts
+++ src/packages/react-query/config/mutationOptions.ts
@@ -1,15 +1,18 @@
 import { requestAuthOTP, verifyAuthOTP } from "@/api";
 import { mutationOptions } from "@tanstack/react-query";
 import {
+  kycLevel1Key,
   requestAuthOTPKey,
   verifyAuthOTPKey,
 } from "@/packages/react-query/keys/mutationKeys";
 import { ResponseErrorType } from "@/types";
 import {
+  KycL1SchemaType,
   RequestAuthOTPSchemaType,
   VerifyAuthOTPSchemaType,
 } from "@/validations/types";
 import { VerifyAuthOTPResponse } from "@/api/types";
+import { kycL1 } from "@/api";
 
 const requestAuthOTPConfig = () => {
   return mutationOptions<void, ResponseErrorType, RequestAuthOTPSchemaType>({
@@ -27,5 +30,11 @@ const verifyAuthOTPConfig = () => {
     mutationFn: verifyAuthOTP,
   });
 };
+const kycLevel1Config = () => {
+  return mutationOptions<void, ResponseErrorType, KycL1SchemaType>({
+    mutationKey: kycLevel1Key,
+    mutationFn: kycL1,
+  });
+};
 
-export { requestAuthOTPConfig, verifyAuthOTPConfig };
+export { requestAuthOTPConfig, verifyAuthOTPConfig, kycLevel1Config };
--- src/packages/react-query/keys/mutationKeys.ts
+++ src/packages/react-query/keys/mutationKeys.ts
@@ -1,4 +1,5 @@
-const requestAuthOTPKey = ["request-auth-otp"];
-const verifyAuthOTPKey = ["verify-auth-otp"];
+const requestAuthOTPKey = ["request-auth-otp" as const];
+const verifyAuthOTPKey = ["verify-auth-otp" as const];
+const kycLevel1Key = ["kyc-level-1" as const];
 
-export { requestAuthOTPKey, verifyAuthOTPKey };
+export { requestAuthOTPKey, verifyAuthOTPKey, kycLevel1Key };
