f7bacbf8814b5a96212924fa94f1d13ba956ccee
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-24 16:34:35 +0330

    feat(form component)
    Developed the logic of `KycL1Form` component

--- src/components/template/Form/KycL1Form.tsx
+++ src/components/template/Form/KycL1Form.tsx
@@ -1,4 +1,17 @@
-import { InputSelect } from "@/components/ui/Input/InputSelect";
+"use client";
+import { DAYS, MONTHS, YEARS } from "@/constant/app/date";
+import { kycLevel1Config, kycStatusConfig } from "@/packages/react-query";
+import safeAsync from "@/utils/app/safeAsync";
+import kvcL1Schema from "@/validations/kyc/kycL1Schema";
+import { KycL1SchemaType } from "@/validations/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { MutationOptions, useMutation, useQuery } from "@tanstack/react-query";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import {
+  InputSelect,
+  InputSelectItem,
+  InputSelectMenu,
+} from "@/components/ui/Input/InputSelect";
 import InputText from "@/components/ui/Input/InputText";
 import {
   FormLayout,
@@ -8,40 +21,148 @@ import {
   FormLayoutLable,
   FormLayoutSubmit,
 } from "@/components/ui/Layout/FormLayout";
+import { ResponseErrorType } from "@/types";
 
-function KycL1Form() {
+const kycStatusQueryConfig = kycStatusConfig();
+const kycL1Config = kycLevel1Config();
+
+interface KycL1FormProps extends Pick<
+  MutationOptions<void, ResponseErrorType, KycL1SchemaType>,
+  "onSuccess"
+> {}
+
+function KycL1Form(mutationProps: KycL1FormProps) {
+  const kycStatusQuery = useQuery(kycStatusQueryConfig);
+  const kycL1Mutation = useMutation({ ...kycL1Config, ...mutationProps });
+
+  const onSubmit: SubmitHandler<KycL1SchemaType> = async (fields) => {
+    await safeAsync(async () => {
+      await kycL1Mutation.mutateAsync(fields);
+    });
+  };
+
+  const form = useForm({
+    resolver: zodResolver(kvcL1Schema),
+    defaultValues: {
+      birthDay: "",
+      birthMonth: "",
+      birthYear: "",
+    },
+  });
+
   return (
-    <FormLayout>
+    <FormLayout onSubmit={form.handleSubmit(onSubmit)}>
       <FormLayoutAlert>
-        {"شماره شبا باید متعلق به صاحب کد ملی 2940282714 باشد"}
+        {"شماره شبا باید متعلق به صاحب کد ملی باشد"}
       </FormLayoutAlert>
       <FormLayoutFieldGroup>
+        {/* // * ----------- National Id ----------- */}
         <FormLayoutField>
           <FormLayoutLable>{"کد ملی"}</FormLayoutLable>
-          <InputText placeholder="کد ملی را وارد کنید" />
+          <InputText
+            placeholder="کد ملی را وارد کنید"
+            error={!!form.formState.errors?.nationalId?.message}
+            {...form.register("nationalId")}
+          />
         </FormLayoutField>
+        {/* // * ----------- Phone number ----------- */}
         <FormLayoutField>
           <FormLayoutLable>{"شماره موبایل"}</FormLayoutLable>
-          <InputText placeholder="شماره موبایل خود را وارد کنید" />
+          <InputText
+            placeholder="شماره موبایل خود را وارد کنید"
+            disabled
+            value={kycStatusQuery.data?.data.phoneNumber ?? ""}
+          />
         </FormLayoutField>
       </FormLayoutFieldGroup>
 
+      {/* // * ----------- Birth Date ----------- */}
       <FormLayoutField>
         <FormLayoutLable>{"تاریخ تولد"}</FormLayoutLable>
         <FormLayoutFieldGroup>
           <FormLayoutField>
-            <InputSelect placeholder="سال"></InputSelect>
+            <Controller
+              control={form.control}
+              name="birthYear"
+              render={({ field, fieldState }) => {
+                return (
+                  <InputSelect
+                    error={!!fieldState.error?.message}
+                    placeholder="سال"
+                    {...field}
+                  >
+                    <InputSelectMenu>
+                      {YEARS.map(({ label, value }) => {
+                        return (
+                          <InputSelectItem value={value}>
+                            {label}
+                          </InputSelectItem>
+                        );
+                      })}
+                    </InputSelectMenu>
+                  </InputSelect>
+                );
+              }}
+            />
           </FormLayoutField>
+
           <FormLayoutField>
-            <InputSelect placeholder="ماه"></InputSelect>
+            <Controller
+              control={form.control}
+              name="birthMonth"
+              render={({ field, fieldState }) => {
+                return (
+                  <InputSelect
+                    error={!!fieldState.error?.message}
+                    placeholder="ماه"
+                    {...field}
+                  >
+                    <InputSelectMenu>
+                      {MONTHS.map(({ label, value }) => {
+                        return (
+                          <InputSelectItem value={value}>
+                            {label}
+                          </InputSelectItem>
+                        );
+                      })}
+                    </InputSelectMenu>
+                  </InputSelect>
+                );
+              }}
+            />
           </FormLayoutField>
+
           <FormLayoutField>
-            <InputSelect placeholder="روز"></InputSelect>
+            <Controller
+              control={form.control}
+              name="birthDay"
+              render={({ field, fieldState }) => {
+                return (
+                  <InputSelect
+                    error={!!fieldState.error?.message}
+                    placeholder="روز"
+                    {...field}
+                  >
+                    <InputSelectMenu>
+                      {DAYS.map(({ label, value }) => {
+                        return (
+                          <InputSelectItem value={value}>
+                            {label}
+                          </InputSelectItem>
+                        );
+                      })}
+                    </InputSelectMenu>
+                  </InputSelect>
+                );
+              }}
+            />
           </FormLayoutField>
         </FormLayoutFieldGroup>
       </FormLayoutField>
 
-      <FormLayoutSubmit color="error">{"ثبت اطلاعات"}</FormLayoutSubmit>
+      <FormLayoutSubmit disabled={form.formState.isSubmitting}>
+        {"ثبت اطلاعات"}
+      </FormLayoutSubmit>
     </FormLayout>
   );
 }
