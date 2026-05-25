b5e0ecd0255b47b3cdc20ba4bb6cc5bd669449d5
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-25 09:49:32 +0330

    feat(utility)
    A function To check that a logic or a conditional code covers all conditions.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as compactTomanPrice } from "./features/price/compactTomanPrice";
 export { default as getTomanUnitPrice } from "./features/price/getTomanUnitPrice";
 export { default as isKycStepPassed } from "./features/kyc/isKycStepPassed";
+export { default as assertNever } from "./app/assertNever";
