573c00bf4c6d09f221c45e9e56f82a52c794f1f1
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-30 11:33:19 +0330

    fix(env)
    `NEXT_PUBLIC_BASEURL` variable must not be validate because it might be used in server.
    NOTICE `clientEnv` is only for frequent used env variables on client.

--- src/validations/env/clientEnv.ts
+++ src/validations/env/clientEnv.ts
@@ -3,14 +3,12 @@ import type { Infer } from "zod";
 import { phoneNumberValidation } from "@/validations/shared";
 
 const clientEnvScheam = object({
-  NEXT_PUBLIC_BASEURL: string(),
   NEXT_PUBLIC_USER_IDENTIFIER: phoneNumberValidation(),
 });
 
 // * ---- Validation ----
 const envObj = process.env;
 const validationResult = clientEnvScheam.safeParse({
-  NEXT_PUBLIC_BASEURL: envObj.NEXT_PUBLIC_BASEURL as string,
   NEXT_PUBLIC_USER_IDENTIFIER: envObj.NEXT_PUBLIC_USER_IDENTIFIER as string,
 } satisfies Infer<typeof clientEnvScheam>);
 
