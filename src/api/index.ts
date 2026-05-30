338db3f6044947740c0e6182a855157d06eca415
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-30 16:13:36 +0330

    feat(re-exporter)
    Added a re-exporter for an api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,3 +3,4 @@ export { default as verifyAuthOTP } from "./auth/verifyAuthOTP";
 export { default as kycStatus } from "./kyc/kycStatus";
 export { default as kycL1 } from "./kyc/kycL1";
 export { default as kycL2 } from "./kyc/kycL2";
+export { default as dashboardInfo } from "./user/dashboardInfo";
