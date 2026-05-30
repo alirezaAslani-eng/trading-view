e52e9ce5e4ccb61ec6f12c7c8511ab664b2e60c5
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-30 12:06:35 +0330

    feat(utlity)
    Added a utility function that resloves the url of fetchers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { default as getTomanUnitPrice } from "./features/price/getTomanUnitPrice
 export { default as isKycStepPassed } from "./features/kyc/isKycStepPassed";
 export { default as getNextKycLevel } from "./features/kyc/getNextKycLevel";
 export { default as assertNever } from "./app/assertNever";
+export { default as createApiUrl } from "./app/createApiUrl";
