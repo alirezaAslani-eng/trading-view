60ad321d416070c04a29724dce0d76d4ebd4906b
Alireza <alirezaaslane.mob7@gmail.com>
2026-05-31 15:50:26 +0330

    feat(component)
    Made dynamic the server component `ProfileOverviewSection`

--- src/components/template/Profile/ProfileOverviewSection.tsx
+++ src/components/template/Profile/ProfileOverviewSection.tsx
@@ -1,4 +1,3 @@
-"use client";
 import KycPassedSteps from "@/components/template/kyc/KycPassedSteps";
 import { Box, Divider, Stack, Typography } from "@mui/material";
 import BulletItem from "@/components/ui/BulletItem/BulletItem";
@@ -7,6 +6,9 @@ import BulletText from "@/components/ui/BulletItem/BulletText";
 import kycFeatures from "@/constant/features/kyc/kycFeatures";
 import Button from "@/components/ui/Button/Button";
 import NextLink from "@/components/ui/Link/NextLink";
+import { dashboardInfo } from "@/api";
+import isMaximumKycLevel from "@/utils/features/kyc/isMaximumKycLevel";
+import { cookies } from "next/headers";
 import {
   BirthDayCakeIcon,
   PenOnPaperIcon,
@@ -22,117 +24,130 @@ import {
   UserProfileItemInfo,
 } from "@/components/ui/Card/UserProfileItemCard";
 
-function ProfileOverviewSection() {
-  return (
-    <div>
-      <Stack>
-        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-          {/* // * ----start---- User Profile -------- */}
-          <UserProfile sx={{ gap: "16px" }}>
-            <UserProfileImage
-              width={70}
-              height={70}
-              src={"/images/person.png"}
-              alt="my profile"
-            />
-            <UserProfileInfo sx={{ gap: "4px" }}>
-              <Typography variant="body1" sx={{ color: "text.onPrimary" }}>
-                {"علیرضا اصلانی"}
-              </Typography>
-              <Typography variant="body2" sx={{ color: "text.tertiary" }}>
-                {"سطح یک"}
-              </Typography>
-            </UserProfileInfo>
-          </UserProfile>
-          {/* // * ----end---- User Profile -------- */}
+// ! Issiue : Some data dosen't come from server like `nationalId` and `birthdate`
+// TODO : There is a button in this server component that needs to diaspatch an action
+async function ProfileOverviewSection() {
+  const cookieStore = await cookies();
+  const dashboard_info = await dashboardInfo({
+    headers: { cookie: cookieStore.toString() },
+  })!;
 
-          {/* // * ---start--- Last Login Date ------- */}
-          <Typography
-            variant="caption1"
+  return (
+    <Stack>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        {/* // * ----start---- User Profile -------- */}
+        <UserProfile sx={{ gap: "16px" }}>
+          <UserProfileImage
+            width={70}
+            height={70}
+            src={"/images/person.png"}
+            alt="my profile"
+          />
+          <UserProfileInfo sx={{ gap: "4px" }}>
+            <Typography variant="body1" sx={{ color: "text.onPrimary" }}>
+              {dashboard_info.fullName}
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.tertiary" }}>
+              {dashboard_info.kycLevel}
+            </Typography>
+          </UserProfileInfo>
+        </UserProfile>
+        {/* // * ----end---- User Profile -------- */}
+
+        {/* // * ---start--- Last Login Date ------- */}
+        <Typography
+          variant="caption1"
+          sx={{
+            color: "text.tertiary",
+            mt: "6px",
+            whiteSpace: "pre",
+          }}
+        >
+          {"آخرین ورود   10:35   1405/02/08"}
+        </Typography>
+        {/* // * ---end--- Last Login Date ------- */}
+      </Box>
+
+      {/* // * ---start--- User Profile Items -------- */}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "18px",
+          mt: "32px",
+        }}
+      >
+        <UserProfileItemCard sx={{ flex: 1 }}>
+          <UserProfileItemInfo
+            icon={<UserGuardIcon />}
+            title="کد ملی"
+            subTitle={dashboard_info.mobile}
+          />
+        </UserProfileItemCard>
+
+        <UserProfileItemCard sx={{ flex: 1 }}>
+          <UserProfileItemInfo
+            icon={<BirthDayCakeIcon />}
+            title="تاریخ تولد"
+            // subTitle={dashboard_info}
+          />
+        </UserProfileItemCard>
+
+        <UserProfileItemCard sx={{ flex: 1 }}>
+          <UserProfileItemInfo
+            icon={<UserGuardIcon />}
+            title="شماره موبایل"
+            subTitle={dashboard_info.mobile}
+          />
+          <NextLink href="">
+            <PenOnPaperIcon />
+          </NextLink>
+        </UserProfileItemCard>
+      </Box>
+      {/* // * ---end--- User Profile Items -------- */}
+
+      <Box sx={{ px: "18px", mt: "64px" }}>
+        {/* // * ---start--- Passed KYC Steps ------- */}
+        <KycPassedSteps />
+        {/* // * ---end--- Passed KYC Steps ------- */}
+
+        {/* // * ---start--- Current KYC Features ------- */}
+        <Stack sx={{ mt: "22px", gap: "8px" }}>
+          {kycFeatures["Level2_Advanced"].map(({ feature }) => {
+            return (
+              <BulletItem key={feature}>
+                <BulletItemShape color="disabled" sx={{ mt: "5px" }} />
+                <BulletText variant="body3">{feature}</BulletText>
+              </BulletItem>
+            );
+          })}
+        </Stack>
+        {/* // * ---end--- Current KYC Features ------- */}
+      </Box>
+      {!isMaximumKycLevel(dashboard_info.kycLevel) && (
+        <>
+          <Divider
+            sx={{ borderColor: "border.dark", mt: "32px", mb: "20px" }}
+          />
+          <Box
             sx={{
-              color: "text.tertiary",
-              mt: "6px",
-              whiteSpace: "pre",
+              px: "18px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
           >
-            {"آخرین ورود   10:35   1405/02/08"}
-          </Typography>
-          {/* // * ---end--- Last Login Date ------- */}
-        </Box>
+            <Typography variant="body3" sx={{ color: "text.secondary" }}>
+              {"سقف برداشت و واریز روزانه بیشتری نیاز دارید؟"}
+            </Typography>
 
-        {/* // * ---start--- User Profile Items -------- */}
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            gap: "18px",
-            mt: "32px",
-          }}
-        >
-          <UserProfileItemCard sx={{ flex: 1 }}>
-            <UserProfileItemInfo
-              icon={<UserGuardIcon />}
-              title="کد ملی"
-              subTitle="2940282714"
-            />
-          </UserProfileItemCard>
 
-          <UserProfileItemCard sx={{ flex: 1 }}>
-            <UserProfileItemInfo
-              icon={<BirthDayCakeIcon />}
-              title="تاریخ تولد"
-              subTitle="1384/08/02"
-            />
-          </UserProfileItemCard>
-
-          <UserProfileItemCard sx={{ flex: 1 }}>
-            <UserProfileItemInfo
-              icon={<UserGuardIcon />}
-              title="شماره موبایل"
-              subTitle="09339198446"
-            />
-            <NextLink href="">
-              <PenOnPaperIcon />
-            </NextLink>
-          </UserProfileItemCard>
-        </Box>
-        {/* // * ---end--- User Profile Items -------- */}
-
-        <Box sx={{ px: "18px", mt: "64px" }}>
-          {/* // * ---start--- Passed KYC Steps ------- */}
-          <KycPassedSteps />
-          {/* // * ---end--- Passed KYC Steps ------- */}
-
-          {/* // * ---start--- Current KYC Features ------- */}
-          <Stack sx={{ mt: "22px", gap: "8px" }}>
-            {kycFeatures["Level2_Advanced"].map(({ feature }) => {
-              return (
-                <BulletItem key={feature}>
-                  <BulletItemShape color="disabled" sx={{ mt: "5px" }} />
-                  <BulletText variant="body3">{feature}</BulletText>
-                </BulletItem>
-              );
-            })}
-          </Stack>
-          {/* // * ---end--- Current KYC Features ------- */}
-        </Box>
-        <Divider sx={{ borderColor: "border.dark", mt: "32px", mb: "20px" }} />
-        <Box
-          sx={{
-            px: "18px",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="body3" sx={{ color: "text.secondary" }}>
-            {"سقف برداشت و واریز روزانه بیشتری نیاز دارید؟"}
-          </Typography>
-
-          <Button variant="on-surface">{"ارتقا سطح کاربری"}</Button>
-        </Box>
-      </Stack>
-    </div>
+              <Button variant="on-surface">{"ارتقا سطح کاربری"}</Button>
+        
+          </Box>
+        </>
+      )}
+    </Stack>
   );
 }
 
