2603d6a2f50ca2fe83a57b4d10a0c2fd6cb4533a
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-02 16:18:17 +0330

    feat(component)
    Wrapped the component NumericFormat with `styled` to sync it with Mui styling features

---
+++ src/components/ui/Input/NumericFormat.tsx
@@ -0,0 +1,7 @@
+"use client";
+import { styled } from "@mui/material";
+import { NumericFormat as NumericFormat_ } from "react-number-format";
+
+const NumericFormat = styled(NumericFormat_)();
+
+export default NumericFormat;
