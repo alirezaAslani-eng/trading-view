936a5bfaf32689823beaa1de0e1ed387ed537f25
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-03 12:39:21 +0330

    feat(component)
    This component presents buttons that shows a precent number to allow user quickly choose an amount to trade

---
+++ src/components/template/Trade/PercentButtons.tsx
@@ -0,0 +1,35 @@
+import { ReplaceSxWithSxOnlyObject } from "@/packages/mui/theme/types";
+import { Box, BoxProps, ButtonBase, Typography } from "@mui/material";
+
+function PercentButtons(props: ReplaceSxWithSxOnlyObject<BoxProps>) {
+  return (
+    <Box
+      {...props}
+      sx={{ display: "flex", alignItems: "center", gap: "12px", ...props.sx }}
+    />
+  );
+}
+function Percent(props: { precent?: string }) {
+  return (
+    <ButtonBase
+      sx={{
+        borderRadius: "999px",
+        width: "100%",
+        p: "4px 12px",
+        border: "1px solid",
+        borderColor: "border.dark",
+        backgroundColor: "background.surfaceLevel5",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "text.secondary",
+      }}
+    >
+      <Typography variant="button5" sx={{ color: "text.secondary" }}>
+        {props.precent}
+      </Typography>
+    </ButtonBase>
+  );
+}
+
+export { PercentButtons, Percent };
