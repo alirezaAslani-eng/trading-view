eb225e7796850b2b4fae72f5dbab6abedd7bf96f
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-05 17:41:49 +0330

    feat(auth)
    A slice to manage the state of a modal that allows the user to signin when the token is expired during user interactions

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { kycModalSlice } from "../features/kyc";
+import { authModalSlice } from "../features/auth";
 const store = configureStore({
   reducer: {
     kycModal: kycModalSlice.reducer,
+    authModal: authModalSlice.reducer,
   },
 });
 
 export { store };
+export const dispatch = store.dispatch;
