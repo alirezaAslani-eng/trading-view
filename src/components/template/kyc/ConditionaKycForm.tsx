8bb062b2891491f7d8fef66e0fdb5fc56ac232c1
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-06 01:37:17 +0330

    feat(UI)
    Add loading fallback to `ConditionaKycForm` component

--- src/components/template/kyc/ConditionaKycForm.tsx
+++ src/components/template/kyc/ConditionaKycForm.tsx
@@ -1,5 +1,7 @@
 "use client";
+import BouncCircleLoader from "@/components/ui/Fallback/BounceCircleLoader";
 import { kycStatusConfig } from "@/packages/react-query";
+import { Box } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { ReactElement } from "react";
 
@@ -11,11 +13,19 @@ interface ConditionalFormKycProps {
 }
 function ConditionaKycForm({ kycL1Form, kycL2Form }: ConditionalFormKycProps) {
   const kycStatus = useQuery(queryConfig);
-  // TODO Show an skeleton loader while the query is loading
+  const isSuccessQuey = kycStatus.status === "success";
   return (
     <>
-      {kycStatus.data?.kycLevel === "None" && kycL1Form}
-      {kycStatus.data?.kycLevel === "Level1_Basic" && kycL2Form}
+      {!isSuccessQuey ? (
+        <Box sx={{ display: "flex", justifyContent: "center", my: "80px" }}>
+          <BouncCircleLoader sx={{ mx: "auto" }} />
+        </Box>
+      ) : (
+        <>
+          {kycStatus.data.kycLevel === "None" && kycL1Form}
+          {kycStatus.data.kycLevel === "Level1_Basic" && kycL2Form}
+        </>
+      )}
     </>
   );
 }
