f6ea5077888886ce8fe027e6eedc322e1326921d
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-05 16:17:21 +0330

    refactor(react-query)
    Utilized mutation default error handlers

--- src/packages/react-query/core/queryClient.ts
+++ src/packages/react-query/core/queryClient.ts
@@ -1,22 +1,44 @@
 import { errorAlert, successAlert } from "@/packages/react-hot-toast";
-import { MutationCache, QueryClient } from "@tanstack/react-query";
+import { dispatch } from "@/redux/store/store";
+import { ResponseErrorType } from "@/types";
+import {
+  Mutation,
+  MutationCache,
+  QueryCache,
+  QueryClient,
+} from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   mutationCache: new MutationCache({
     onSuccess(data, variables, onMutateResult, mutation, context) {
-      const message =
-        mutation?.meta?.successMessage ?? "عملیات با موفقیت انجام شد";
-      if (!!!mutation?.meta?.disableSuccessAlert) {
-        successAlert(message);
-      }
+      mutationSuccessHandler(mutation);
     },
     onError(data, variables, onMutateResult, mutation, context) {
-      const custom_message = mutation?.meta?.errorMessage;
-      const server_message = data.message ?? "اعملیات ناموفق";
-      const message = custom_message ?? server_message;
-      errorAlert(message);
+      mutationErrorHandler(data, mutation);
     },
+  }),
+  queryCache: new QueryCache({
+    onError(error, query) {},
   }),
 });
 
 export default queryClient;
+
+// * ------- queryClient helpers -------
+function mutationErrorHandler(
+  data: ResponseErrorType,
+  mutation: Mutation<unknown, unknown, unknown, unknown>,
+) {
+  const custom_message = mutation?.meta?.errorMessage;
+  const server_message = data.message ?? "اعملیات ناموفق";
+  const message = custom_message ?? server_message;
+  errorAlert(message);
+}
+
+function mutationSuccessHandler(
+  mutation: Mutation<unknown, unknown, unknown, unknown>,
+) {
+  const message = mutation?.meta?.successMessage ?? "عملیات با موفقیت انجام شد";
+  if (!!mutation?.meta?.disableSuccessAlert) return;
+  successAlert(message);
+}
