e55e79627c070be3e8eff2386fb2a8b93d7df59d
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-06 01:24:07 +0330

    refactor(code refinement)

--- src/components/template/Form/AddCreditCardForm.tsx
+++ src/components/template/Form/AddCreditCardForm.tsx
@@ -3,50 +3,46 @@ import safeAsync from "@/utils/app/safeAsync";
 import addCardSchema from "@/validations/bank/addCardSchema";
 import { AddCardSchemaType } from "@/validations/types";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { MutationOptions, useMutation } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { SubmitHandler, useForm } from "react-hook-form";
 import InputText from "@/components/ui/Input/InputText";
 import {
   FormLayout,
   FormLayoutAlert,
   FormLayoutField,
-  FormLayoutFieldGroup,
   FormLayoutLable,
   FormLayoutSubmit,
 } from "@/components/ui/Layout/FormLayout";
-import { ResponseErrorType } from "@/types";
 import { addCardConfig } from "@/packages/react-query";
-
-interface AddCreditCardFormProps extends Pick<
-  MutationOptions<string, ResponseErrorType, AddCardSchemaType>,
-  "onSuccess" | "mutationFn"
-> {}
+import { promiseAlert } from "@/packages/react-hot-toast";
 
 const addcardconfig = addCardConfig();
 
-function AddCreditCardForm(mutationProps: AddCreditCardFormProps) {
+function AddCreditCardForm() {
   const addCardMutation = useMutation(addcardconfig);
 
   const onSubmit: SubmitHandler<AddCardSchemaType> = async (fields) => {
-    await safeAsync(async () => {
-      await addCardMutation.mutateAsync(fields);
-    });
+    await promiseAlert(
+      safeAsync(async () => {
+        await addCardMutation.mutateAsync(fields);
+      }),
+      { loading: "صبر کنید" },
+    );
   };
 
   const form = useForm({
     resolver: zodResolver(addCardSchema),
-    defaultValues:{
-      birthDateShamsi:"1384/08/02",
-      cardNumber:"",
-    }
+    defaultValues: {
+      // ! birthdate must come from server 
+      birthDateShamsi: "1384/08/02",
+      cardNumber: "",
+    },
   });
 
   return (
     <FormLayout onSubmit={form.handleSubmit(onSubmit)}>
-      <FormLayoutAlert>
-        {"کارت باید متعلق به صاحب کد ملی باشد"}
-      </FormLayoutAlert>
-      
+      <FormLayoutAlert>{"کارت باید متعلق به صاحب کد ملی باشد"}</FormLayoutAlert>
+
       {/* // * ----------- Card Number ----------- */}
       <FormLayoutField>
         <FormLayoutLable>{"شماره کارت"}</FormLayoutLable>
@@ -56,7 +52,7 @@ function AddCreditCardForm(mutationProps: AddCreditCardFormProps) {
           {...form.register("cardNumber")}
         />
       </FormLayoutField>
-  
+
       <FormLayoutSubmit disabled={form.formState.isSubmitting}>
         {"ثبت کارت"}
       </FormLayoutSubmit>
