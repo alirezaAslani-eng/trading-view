1b854cd68f91e0af8fe57473d4d872e723002c7a
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-06 11:14:10 +0330

    fix(API error handler)
    Sometimes server might respond successfully but the property `isSuccess` is false

--- src/utils/app/responseErrorHandler.ts
+++ src/utils/app/responseErrorHandler.ts
@@ -1,3 +1,4 @@
+import { BaseApiResponse } from "@/types";
 import jsonParseHandler from "./jsonParseHandler";
 import throwError from "./throwError";
 
@@ -8,10 +9,25 @@ export default async function responseErrorHandler(
     const error = await jsonParseHandler<any>(res);
     throwError(true, {
       code: error?.errorCode,
+      // TODO : message handler
       message: error?.message ?? "",
       status: res.status,
       statusText: res.statusText,
       details: error,
+    });
+  }
+
+  // * When serevr respons is ok but isSuccess is false
+  if (res.ok === true) {
+    const data = (await jsonParseHandler(res)) as BaseApiResponse<unknown>;
+    if (!!data?.isSuccess) return;
+    throwError(true, {
+      code: data?.errorCode ?? undefined,
+      // TODO : message handler
+      message: data?.message ?? "",
+      status: res.status,
+      statusText: res.statusText,
+      details: data,
     });
   }
 }
