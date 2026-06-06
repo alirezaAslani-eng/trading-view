8ad0590759df4d0c39385da3df71b359f07bbc1f
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-06 14:18:29 +0330

    feat(component)
    Created a fallback handler

---
+++ src/components/ui/Fallback/FallbackHandler.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+interface FallbackHandlerProps {
+  isLoading?: boolean;
+  isError?: boolean;
+  dataLength?: number | undefined;
+  fallbacks: {
+    loader?: ReactNode;
+    error?: ReactNode;
+    noData?: ReactNode;
+  };
+}
+function FallbackHandler(props: FallbackHandlerProps) {
+  if (props.isLoading) return props?.fallbacks?.loader;
+  if (props.isError) return props?.fallbacks?.error ?? props?.fallbacks?.loader;
+  if (props.dataLength === 0) return props?.fallbacks?.noData;
+}
+
+export default FallbackHandler;
