5a874f6f592946466f053b5e27d8821f04bb1621
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-10 16:05:01 +0330

    refactor(component)
    Removed select input temporary.

---
+++ src/components/template/Table/TableControls.tsx
@@ -0,0 +1,40 @@
+import InputText from "@/components/ui/Input/InputText";
+import InputMarker from "@/components/ui/Marker/InputMarker";
+import { SearchIcon } from "@/components/ui/Icon";
+
+function TableControls() {
+  return (
+    <InputMarker
+      right={"10px"}
+      icon={<SearchIcon fontSize="medium" sx={{ color: "text.caption" }} />}
+    >
+      <InputText
+        size={"small"}
+        placeholder="جستجو..."
+        sx={{ width: "194px", pr: "32px" }}
+      />
+    </InputMarker>
+    // <Box
+    //   sx={{
+    //     display: "flex",
+    //     alignItems: "center",
+    //     gap: "20px",
+    //     width: "374px",
+    //   }}
+    // >
+    /* <Box sx={{ width: "42.7%" }}>
+        <InputSelect placeholder="مرتب سازی" size="small" variant="outlined">
+          <InputSelectMenu>
+            <InputSelectItem value="1">متن تستی</InputSelectItem>
+            <InputSelectItem value="2">متن تستی</InputSelectItem>
+          </InputSelectMenu>
+        </InputSelect>
+      </Box> */
+    // <Box sx={{  }}>
+
+    // </Box>
+    // </Box>
+  );
+}
+
+export default TableControls;
