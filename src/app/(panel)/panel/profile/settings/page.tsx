259d3639ec17a127d963555416249a16cd9946e5
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-11 23:48:23 +0330

    refactor(delete file)
    deleted unecessary `CustomSwitch` component

--- src/app/(panel)/panel/profile/settings/page.tsx
+++ src/app/(panel)/panel/profile/settings/page.tsx
@@ -1,73 +1,67 @@
-import { Box, Divider, Stack, ToggleButton } from "@mui/material";
+import { Divider } from "@mui/material";
 import PageHeader from "@/components/common/Appbar/PageHeader";
-import ProfileOverviewSection from "@/components/template/Profile/ProfileOverviewSection";
 import { UserIcon } from "@/components/ui/Icon";
 import {
-    Header,
-    Main,
-    Page,
-    Section,
-    SectionContent,
+  Header,
+  Main,
+  Page,
+  Section,
+  SectionContent,
 } from "@/components/ui/Layout/PageLayout";
 import {
-    PagePaper,
-    PagePaperHeading,
-    PagePaperTitle,
+  PagePaper,
+  PagePaperHeading,
+  PagePaperTitle,
 } from "@/components/ui/Layout/PaperLayout";
 import {
-    PageSubNavigation,
-    PageSubNavigationLink,
+  PageSubNavigation,
+  PageSubNavigationLink,
 } from "@/components/ui/PageSubNavigation/PageSubNavigation";
-import BankInfoOverviewSection from "@/components/template/Profile/BankTableList";
-import ToggleTabGroup from "@/components/ui/ButtonGroup/ToggleTabGroup";
-import EditPasswordForm from "@/components/template/Form/EditPasswordForm";
-import CustomizedSwitches from "@/components/ui/Switch/Switch";
 
 function page() {
-    return (
-        <Page>
-            <Header>
-                <PageHeader
-                    title="پروفایل من"
-                    subtitle="مدیریت اطلاعات و تنظیمات حساب"
-                />
-            </Header>
-            <Main>
-                <Section>
-                    <SectionContent sx={{ gap: "32px" }}>
-                        <PageSubNavigation>
-                            <PageSubNavigationLink href="/panel/profile/my-info">
-                                <UserIcon />
-                                {"مشخصات کاربری"}
-                            </PageSubNavigationLink>
-                            <PageSubNavigationLink href="/panel/profile/banks">
-                                <UserIcon />
-                                {"اطلاعات بانکی"}
-                            </PageSubNavigationLink>
-                            <PageSubNavigationLink href="/panel/profile/security">
-                                <UserIcon />
-                                {"امنیت"}
-                            </PageSubNavigationLink>
-                            <PageSubNavigationLink href="/panel/profile/settings">
-                                <UserIcon />
-                                {"تنظیمات"}
-                            </PageSubNavigationLink>
-                        </PageSubNavigation>
+  return (
+    <Page>
+      <Header>
+        <PageHeader
+          title="پروفایل من"
+          subtitle="مدیریت اطلاعات و تنظیمات حساب"
+        />
+      </Header>
+      <Main>
+        <Section>
+          <SectionContent sx={{ gap: "32px" }}>
+            <PageSubNavigation>
+              <PageSubNavigationLink href="/panel/profile/my-info">
+                <UserIcon />
+                {"مشخصات کاربری"}
+              </PageSubNavigationLink>
+              <PageSubNavigationLink href="/panel/profile/banks">
+                <UserIcon />
+                {"اطلاعات بانکی"}
+              </PageSubNavigationLink>
+              <PageSubNavigationLink href="/panel/profile/security">
+                <UserIcon />
+                {"امنیت"}
+              </PageSubNavigationLink>
+              <PageSubNavigationLink href="/panel/profile/settings">
+                <UserIcon />
+                {"تنظیمات"}
+              </PageSubNavigationLink>
+            </PageSubNavigation>
 
-                        <PagePaper>
-                            <PagePaperHeading>
-                                <PagePaperTitle>{"تنظیمات"}</PagePaperTitle>
-                            </PagePaperHeading>
-                            <Divider
-                                sx={{ mt: "12px", mb: "32px", borderColor: "border.dark" }}
-                            />
-                        </PagePaper>
-                        
-                    </SectionContent>
-                </Section>
-            </Main>
-        </Page>
-    )
+            <PagePaper>
+              <PagePaperHeading>
+                <PagePaperTitle>{"تنظیمات"}</PagePaperTitle>
+              </PagePaperHeading>
+              <Divider
+                sx={{ mt: "12px", mb: "32px", borderColor: "border.dark" }}
+              />
+            </PagePaper>
+          </SectionContent>
+        </Section>
+      </Main>
+    </Page>
+  );
 }
 
 export default page;
--- src/components/template/Profile/Settings.tsx
+++ src/components/template/Profile/Settings.tsx
@@ -1,29 +1,21 @@
-import KycPassedSteps from "@/components/template/kyc/KycPassedSteps";
-import CustomizedSwitches from "@/components/ui/Switch/Switch";
-import {
-    Box, Divider, Stack, ToggleButton, Typography, TableCell,
-    TableBody,
-    TableHead,
-    TableRow,
-    Switch,
-} from "@mui/material";
+import { Box, Stack, Switch } from "@mui/material";
 
 // ! Issiue : Some data dosen't come from server like `nationalId` and `birthdate`
 async function Settings() {
-    return (
-        <Stack>
-            <Box
-                sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: "18px",
-                    mt: "32px",
-                }}
-            >
-                <CustomizedSwitches />
-            </Box>
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "18px",
+          mt: "32px",
+        }}
+      >
+        <Switch />
+      </Box>
+    </Stack>
+  );
 }
 
 export default Settings;
