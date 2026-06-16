4732c553f12734f064d54dcfbb125d92eabaacef
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-16 18:38:13 +0330

    fix(type)

--- src/Layout/LayoutAuthPage.tsx
+++ src/Layout/LayoutAuthPage.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { Container, Stack } from "@mui/material";
 import AuthPageBackground from "@/components/ui/background/AuthPageBackground";
+import { PropsWithChildren } from "react";
 
-function LayoutAuthPage({ children }) {
+function LayoutAuthPage({ children }:PropsWithChildren) {
   return (
     <>
       <Stack sx={{ minHeight: "100svh", position: "relative" }}>
