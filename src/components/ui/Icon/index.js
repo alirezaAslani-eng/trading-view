49034f15c41856341b736e3fbfc9124526e81bad
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-16 17:22:28 +0330

    feat(icon)

--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -27,3 +27,4 @@ export { default as TradeOrderIcon } from "./TradeOrderIcon";
 export { default as ArrowUpIcon } from "./ArrowUpIcon";
 export { default as StockIcon } from "./StockIcon";
 export { default as FlashIcon } from "./FlashIcon";
+export { default as BrandIcon } from "./BrandIcon";
