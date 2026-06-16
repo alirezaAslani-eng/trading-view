85d680ccb5d1fb3c9614c3c7794d05fa8e05ece1
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-16 09:55:27 +0330

    fix(component)
    forwarded props to `Box` component

--- src/components/ui/Marker/InputMarker.tsx
+++ src/components/ui/Marker/InputMarker.tsx
@@ -1,15 +1,21 @@
-import { PWC } from "@/types/utils";
-import { Box } from "@mui/material";
+import { ReplaceSxWithSxOnlyObject } from "@/packages/mui/theme/types";
+import { Box, BoxProps } from "@mui/material";
 import { ReactNode } from "react";
 
-interface InputMarkerProps {
+interface InputMarkerProps extends ReplaceSxWithSxOnlyObject<BoxProps> {
   icon?: ReactNode;
   right?: string;
   left?: string;
 }
-function InputMarker({ icon, children, right, left }: PWC<InputMarkerProps>) {
+function InputMarker({
+  icon,
+  children,
+  right,
+  left,
+  ...props
+}: InputMarkerProps) {
   return (
-    <Box sx={{ position: "relative" }}>
+    <Box {...props} sx={{ position: "relative", ...props.sx }}>
       <Box
         sx={{
           position: "absolute",
