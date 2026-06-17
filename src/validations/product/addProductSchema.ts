cbc15af4b8a9d2046019cb012cb14fc2eb3ac17d
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-18 00:11:13 +0330

    fix(schema)
    Removed unecessary field

--- src/validations/product/addProductSchema.ts
+++ src/validations/product/addProductSchema.ts
@@ -3,7 +3,6 @@ import { object, string, number, boolean, coerce } from "zod";
 const addProductSchema = object({
   productName: string(" "),
   productCode: string(" "),
-  name: string(" "),
   unitOfMeasure: string(" "),
   productStatusId: boolean(" "),
   initialPrice: number(" "),
