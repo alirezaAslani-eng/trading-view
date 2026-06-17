5fb152ae4c023cc066cf2d06947051c8c480c700
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-18 00:04:54 +0330

    fix(style)
    Removed default direction because it must be responsive.

--- src/components/ui/Input/InputNumeric.tsx
+++ src/components/ui/Input/InputNumeric.tsx
@@ -29,7 +29,6 @@ const StyledNumericFormat = styled(NumericFormat, {
   return {
     outline: "none",
     width: "100%",
-    direction: "ltr", // * Only number
     ...input_theme.rootTheme,
     ...input_size.rootSize,
     "::placeholder": {
