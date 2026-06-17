3d3c9a5c0cef6218ee45f26a53e6d92b5ed0559c
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-17 12:59:48 +0330

    feat(tradingview)
    Implemented an Abstraction on trading view chart and its APIs

--- src/packages/tradingview/index.ts
+++ src/packages/tradingview/index.ts
@@ -1,2 +1,4 @@
 export { widget } from "./charting_library/charting_library.esm";
 export type * from "./charting_library/charting_library";
+export type * from "./types";
+export * from "./TradingViewChart";
