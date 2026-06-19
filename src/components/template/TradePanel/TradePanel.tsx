c913257c0bde59087e9ca1645f1930a024b035f5
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-19 23:43:59 +0330

    fix(trading)
    useQueryState must parse the `symbol` as uppercase

--- src/components/template/TradePanel/TradePanel.tsx
+++ src/components/template/TradePanel/TradePanel.tsx
@@ -31,6 +31,7 @@ import {
 import LimitedPriceForm from "./LimitedPriceForm";
 import MarketPriceForm from "./MarketPriceForm";
 import { TradeFormSubscriber } from "./types";
+import { uppercaseParser } from "@/packages/nuqs/parsers";
 
 type OrderTypes = TradeFormSchemaInputType["orderType"];
 type OrderSide = TradeFormSchemaInputType["orderSide"];
@@ -39,8 +40,9 @@ const placeOrderMutationConfig = placeOrderConfig();
 
 function TradePanel() {
   // * -------- productCode/Symbol --------
-  const [symbol] = useQueryState(symbolKey);
-
+  const [symbol] = useQueryState(symbolKey, uppercaseParser);
+  
+  
   // * --------- From API ---------
   const placeOrderApi = useMutation({
     ...placeOrderMutationConfig,
