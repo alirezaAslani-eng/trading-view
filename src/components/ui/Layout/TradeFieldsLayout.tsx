81b5a5514eb7329e88385c9c8fa87b0c820be026
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-19 22:16:06 +0330

    feat(component)
    Created a layout component for trading panel in order to share the layout spacing.

---
+++ src/components/ui/Layout/TradeFieldsLayout.tsx
@@ -0,0 +1,34 @@
+import type { ReplaceSxWithSxOnlyObject } from "@/packages/mui/theme/types";
+import { Box, BoxProps, Stack, StackProps } from "@mui/material";
+
+function TradeFieldsLayout(props: StackProps) {
+  return <Stack {...props} />;
+}
+
+function TradeFieldsLayoutPrice(props: ReplaceSxWithSxOnlyObject<BoxProps>) {
+  return <Box sx={{ mt: "32px", ...props.sx }} {...props} />;
+}
+
+function TradeFieldsLayoutAmount(props: ReplaceSxWithSxOnlyObject<BoxProps>) {
+  return <Box sx={{ mt: "14px", ...props.sx }} {...props} />;
+}
+
+function TradeFieldsLayoutPrecentage(
+  props: ReplaceSxWithSxOnlyObject<BoxProps>,
+) {
+  return <Box sx={{ mt: "8px", ...props.sx }} {...props} />;
+}
+
+function TradeFieldsLayoutTotalPrice(
+  props: ReplaceSxWithSxOnlyObject<BoxProps>,
+) {
+  return <Box sx={{ mt: "24px", ...props.sx }} {...props} />;
+}
+
+export {
+  TradeFieldsLayout,
+  TradeFieldsLayoutPrice,
+  TradeFieldsLayoutAmount,
+  TradeFieldsLayoutPrecentage,
+  TradeFieldsLayoutTotalPrice,
+};
