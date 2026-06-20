64fe8be9ac7205a78e5ab1b543334a64df6460ed
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-20 11:21:13 +0330

    feat(market)
    Integrated Price overview section to API

--- src/components/template/Trade/PriceOverview.tsx
+++ src/components/template/Trade/PriceOverview.tsx
@@ -11,13 +11,18 @@ import {
 } from "@/components/ui/Typography/Price";
 import {
   Box,
+  Skeleton,
   Stack,
   StackProps,
+  SxProps,
+  Theme,
   Typography,
   TypographyProps,
 } from "@mui/material";
 import { symbolKey } from "@/packages/nuqs";
 import { parseAsUppercase } from "@/packages/nuqs/parsers";
+import { useQuery } from "@tanstack/react-query";
+import { marketTickerInfoConfig } from "@/packages/react-query";
 const price_sx = { color: "text.onPrimary" };
 const oveview_card_title_sx = { color: "text.caption" };
 
@@ -33,62 +38,7 @@ function PriceOverview() {
       }}
     >
       <ProductCodeSelector />
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          flex: 1,
-          minWidth: "0px",
-          gap: "40px",
-        }}
-      >
-        <PriceOverviewCard>
-          <PriceOverviewCardTitle>{"آخرین قیمت"}</PriceOverviewCardTitle>
-          <Price>
-            <PriceAmount sx={{ color: "text.profit" }}>
-              {formatFaPrice(28000)}
-            </PriceAmount>
-            <PriceUnit variant="caption1" />
-          </Price>
-        </PriceOverviewCard>
-
-        <PriceOverviewCard>
-          <PriceOverviewCardTitle sx={oveview_card_title_sx}>
-            {"درصد تغییرات"}
-          </PriceOverviewCardTitle>
-          <Typography variant="button2" sx={{ color: "text.profit" }}>
-            {"+ 0.11%"}
-          </Typography>
-        </PriceOverviewCard>
-
-        <PriceOverviewCard>
-          <PriceOverviewCardTitle sx={oveview_card_title_sx}>
-            {"آخرین قیمت"}
-          </PriceOverviewCardTitle>
-          <Price sx={price_sx}>
-            <PriceAmount>{formatFaPrice(28000)}</PriceAmount>
-            <PriceUnit variant="caption1" />
-          </Price>
-        </PriceOverviewCard>
-        <PriceOverviewCard>
-          <PriceOverviewCardTitle sx={oveview_card_title_sx}>
-            {"بیشترین قیمت"}
-          </PriceOverviewCardTitle>
-          <Price sx={price_sx}>
-            <PriceAmount>{formatFaPrice(28000)}</PriceAmount>
-            <PriceUnit variant="caption1" />
-          </Price>
-        </PriceOverviewCard>
-        <PriceOverviewCard>
-          <PriceOverviewCardTitle sx={oveview_card_title_sx}>
-            {"قیمت مبنا"}
-          </PriceOverviewCardTitle>
-          <Price sx={price_sx}>
-            <PriceAmount>{formatFaPrice(28000)}</PriceAmount>
-            <PriceUnit variant="caption1" />
-          </Price>
-        </PriceOverviewCard>
-      </Box>
+      <PriceOverViewSection />
     </PanelPaper>
   );
 }
@@ -127,3 +77,92 @@ function ProductCodeSelector() {
   );
 }
 export default PriceOverview;
+
+const skleton_sx: SxProps<Theme> = {
+  width: "100px",
+};
+function PriceOverViewSection() {
+  const [symbol] = useQueryState(symbolKey, parseAsUppercase);
+  const tickerInfoQuery = useQuery(marketTickerInfoConfig(symbol ?? ""));
+  const isLoading = tickerInfoQuery.isLoading;
+  // tickerInfoQuery.data.
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        flex: 1,
+        minWidth: "0px",
+        gap: "40px",
+      }}
+    >
+      <PriceOverviewCard>
+        <PriceOverviewCardTitle>{"آخرین قیمت"}</PriceOverviewCardTitle>
+        {isLoading && <Skeleton animation="wave" sx={skleton_sx} />}
+        {!isLoading && (
+          <Price>
+            <PriceAmount>
+              {formatFaPrice(tickerInfoQuery.data?.lastPrice ?? "")}
+            </PriceAmount>
+            <PriceUnit variant="caption1" />
+          </Price>
+        )}
+      </PriceOverviewCard>
+
+      <PriceOverviewCard>
+        <PriceOverviewCardTitle sx={oveview_card_title_sx}>
+          {"درصد تغییرات"}
+        </PriceOverviewCardTitle>
+        {isLoading && <Skeleton sx={skleton_sx} />}
+        {!isLoading && (
+          <Typography variant="button2" sx={{ color: "text.profit" }}>
+            {`% ${tickerInfoQuery.data?.change24h}`}
+          </Typography>
+        )}
+      </PriceOverviewCard>
+
+      <PriceOverviewCard>
+        <PriceOverviewCardTitle sx={oveview_card_title_sx}>
+          {"کم ترین قیمت 24h"}
+        </PriceOverviewCardTitle>
+        {isLoading && <Skeleton sx={skleton_sx} />}
+        {!isLoading && (
+          <Price sx={price_sx}>
+            <PriceAmount>
+              {formatFaPrice(tickerInfoQuery.data?.low24h ?? "")}
+            </PriceAmount>
+            <PriceUnit variant="caption1" />
+          </Price>
+        )}
+      </PriceOverviewCard>
+      <PriceOverviewCard>
+        <PriceOverviewCardTitle sx={oveview_card_title_sx}>
+          {"بیشترین قیمت 24h"}
+        </PriceOverviewCardTitle>
+        {isLoading && <Skeleton sx={skleton_sx} />}
+        {!isLoading && (
+          <Price sx={price_sx}>
+            <PriceAmount>
+              {formatFaPrice(tickerInfoQuery.data?.high24h ?? "")}
+            </PriceAmount>
+            <PriceUnit variant="caption1" />
+          </Price>
+        )}
+      </PriceOverviewCard>
+      <PriceOverviewCard>
+        <PriceOverviewCardTitle sx={oveview_card_title_sx}>
+          {"حجم معامله"}
+        </PriceOverviewCardTitle>
+        {isLoading && <Skeleton sx={skleton_sx} />}
+        {!isLoading && (
+          <Price sx={price_sx}>
+            <PriceAmount>
+              {formatFaPrice(tickerInfoQuery.data?.volume24h ?? "")}
+            </PriceAmount>
+            <PriceUnit variant="caption1" />
+          </Price>
+        )}
+      </PriceOverviewCard>
+    </Box>
+  );
+}
