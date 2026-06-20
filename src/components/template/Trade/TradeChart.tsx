e522b5d3ebd4265e27180e41cee345462e2e4083
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-20 23:56:27 +0330

    feat(trading)
    Implemented the configuration of trading chart

--- src/components/template/Trade/TradeChart.tsx
+++ src/components/template/Trade/TradeChart.tsx
@@ -1,7 +1,31 @@
-import { TradingViewChart } from "@/packages/tradingview";
+"use client";
+import { widgetOptions } from "@/constant/features/trading/chartConfig";
+import { symbolKey } from "@/packages/nuqs";
+import { parseAsUppercase } from "@/packages/nuqs/parsers";
+import { TradingViewChart, WidgetOptions } from "@/packages/tradingview";
+import { useQueryState } from "nuqs";
+import { useMemo } from "react";
+
 function TradeChart() {
-  // TODO Integrate the chart with APIs
-  return "TradeChart";
+  const [symbol, setSymbol] = useQueryState(symbolKey, parseAsUppercase);
+
+  const widgetConfig = useMemo(
+    () =>
+      ({
+        ...widgetOptions,
+        theme: "Dark",
+        symbol: symbol!,
+      }) satisfies Omit<WidgetOptions, "container">,
+    [symbol],
+  );
+  return (
+    <TradingViewChart
+      widgetOptions={widgetConfig}
+      onSymbolChange={(symbolName) => {
+        setSymbol(symbolName);
+      }}
+    />
+  );
 }
 
 export default TradeChart;
---
+++ src/constant/features/trading/chartConfig.ts
@@ -0,0 +1,139 @@
+"use client";
+import { candlestickHistory, searchSymbols, tradingViewConfig } from "@/api";
+import symbolDetails from "@/api/trading/symbolDetails";
+import { ResolutionString, WidgetOptions } from "@/packages/tradingview";
+import safeAsync from "@/utils/app/safeAsync";
+import {
+  getConnection,
+  onTradeExecuted,
+  OnTradeExecutedInfo,
+  start,
+  subscribeToMarket,
+} from "@/packages/signalr";
+
+//@ts-ignore
+const datafeed: WidgetOptions["datafeed"] = {
+  onReady: async (cb) => {
+    const config = await safeAsync(() => tradingViewConfig());
+    if (!config.ok) return;
+
+    // @ts-ignore
+    cb({
+      ...config.data,
+      symbols_types: [{ name: "All", value: "all" }],
+    });
+  },
+
+  resolveSymbol: async (symbolName, onResolve, onError) => {
+    const symbolInfo = await safeAsync(() => symbolDetails(symbolName));
+
+    if (!symbolInfo.ok) {
+      console.log("Faild to find symbol", symbolInfo.error);
+      onError("Faild to find symbol");
+      return;
+    }
+
+    onResolve({
+      ...symbolInfo.data,
+      format: "price",
+      full_name: symbolInfo.data.description,
+      has_intraday: true,
+    });
+  },
+
+  getBars: async (symbolInfo, resolution, periodParams, onResult, onError) => {
+    const candleHistory = await safeAsync(() =>
+      candlestickHistory({
+        queries: {
+          from: String(periodParams.from),
+          to: String(periodParams.to),
+          resolution,
+          symbol: symbolInfo.name,
+        },
+      }),
+    );
+
+    if (!candleHistory.ok) {
+      const errMessage = "Faild to load candle history data";
+      console.log(errMessage, candleHistory.error);
+      onError(errMessage);
+      return;
+    }
+
+    onResult(
+      candleHistory.data.map((candle) => ({
+        time: candle.t,
+        open: candle.o,
+        close: candle.c,
+        high: candle.h,
+        low: candle.l,
+        volume: candle.v,
+      })),
+      {
+        noData: candleHistory.data.length === 0,
+      },
+    );
+  },
+
+  searchSymbols: async (query, _, __, onResult) => {
+    const searchResult = await safeAsync(() => searchSymbols(query));
+    if (!searchResult.ok) {
+      onResult([]);
+      console.log("search symbol error :", searchResult.error);
+      return;
+    }
+    onResult(
+      searchResult.data.map((item) => {
+        return { ...item, ticker: item.symbol };
+      }),
+    );
+  },
+
+  subscribeBars: (symbolInfo, _, onTick) => {
+    const con = getConnection()!;
+    const startConection = async () => {
+      await start(con);
+      con.invoke(subscribeToMarket, symbolInfo.name);
+    };
+    startConection();
+    const handler = (trade: OnTradeExecutedInfo) => {
+      onTick({
+        volume: trade.volum,
+        time: trade.time * 1000,
+        close: trade.price,
+        high: trade.price,
+        low: trade.price,
+        open: trade.price,
+      });
+    };
+    con.on(onTradeExecuted, handler);
+
+    (window as any)._con_ = con;
+    (window as any)._handler_ = handler;
+  },
+  unsubscribeBars: () => {
+    const con = (window as any)._con_;
+    const handler = (window as any)._handler_;
+
+    if (!con) return;
+    con.off(onTradeExecuted, handler);
+  },
+};
+
+const widgetOptions = {
+  datafeed,
+  
+  library_path: "/charting_library/",
+
+  interval: "1" as ResolutionString,
+
+  locale: "fa",
+
+  timezone: "Asia/Tehran",
+
+  autosize: true,
+
+  debug: true,
+} satisfies Partial<WidgetOptions>;
+
+export { datafeed, widgetOptions };
