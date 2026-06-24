d4fde94e82c10633c24f20ba147fcfb2c4094a6f
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-24 18:18:14 +0330

    feat(type)
    Added types of order filtering

--- src/types/order.types.ts
+++ src/types/order.types.ts
@@ -8,5 +8,14 @@ type OrderStatus =
 
 type OrderType = "Market" | "Limit";
 type OrderSide = "Buy" | "Sell";
+interface UserOrderFilters {
+  page: number;
+  // search: string;
+  // symbol: null | string;
+  // orderType: null | string;
+  // side: null | OrderSide;
+  // sort: null | "ASC" | "DESC";
+  // onlyOpenOrders: boolean;
+}
 
-export type { OrderStatus, OrderSide, OrderType };
+export type { OrderStatus, OrderSide, OrderType, UserOrderFilters };
