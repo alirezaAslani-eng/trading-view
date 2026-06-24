13c2e0a0f74e90d7afdaecb8b35a45c4a71ba441
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-24 21:46:54 +0330

    fix(utility)
    queries that have null value must be deleted

--- src/utils/app/serializeQueries.ts
+++ src/utils/app/serializeQueries.ts
@@ -1,6 +1,17 @@
 const serializeQueries = (
-  queries: Record<string, string | number | boolean> | {},
-): URLSearchParams =>
-  new URLSearchParams(Object.entries(queries).map(([k, v]) => [k, String(v)]));
+  queries: Record<string, string | number | boolean | null> | {},
+): URLSearchParams => {
+  const entrisedQueries = Object.entries(queries).map(([k, v]) => [
+    k,
+    String(v),
+  ]);
 
+  const searchParams = new URLSearchParams(entrisedQueries);
+
+  entrisedQueries.forEach(([k, v]) => {
+    if (v === "null") searchParams.delete(k);
+  });
+  
+  return searchParams;
+};
 export default serializeQueries;
