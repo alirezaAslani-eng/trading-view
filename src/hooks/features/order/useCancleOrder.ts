97bafbbc49cfcdd03ca120d52635f60f9ce0f2b4
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-26 20:07:34 +0330

    refctor(order)
    Removed optimistic update just for now

--- src/hooks/features/order/useCancleOrder.ts
+++ src/hooks/features/order/useCancleOrder.ts
@@ -1,5 +1,3 @@
-import { OrdersResponse } from "@/api/types";
-import { ORDER_STATUS } from "@/constant/features/order/orderStatus";
 import { cancleOrderConfig, ordersKey } from "@/packages/react-query";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
@@ -8,40 +6,7 @@ function useCancelOrderMutation() {
 
   return useMutation({
     ...cancleOrderConfig(),
-
-    onMutate: async (variables) => {
-      await queryClient.cancelQueries({ queryKey: ordersKey });
-
-      const previousOrders = queryClient.getQueriesData<OrdersResponse>({
-        queryKey: ordersKey,
-      });
-
-      queryClient.setQueriesData<OrdersResponse>(
-        { queryKey: ordersKey },
-        (old) => {
-          if (!old) return old;
-
-          return {
-            ...old,
-            items: old.items.map((item) =>
-              item.orderId === variables
-                ? { ...item, status: ORDER_STATUS.Cancelled }
-                : item,
-            ),
-          };
-        },
-      );
-
-      return { previousOrders };
-    },
-
-    onError: (_err, _variables, context) => {
-      if (context?.previousOrders) {
-        queryClient.setQueryData(ordersKey, context.previousOrders);
-      }
-    },
-
-    onSettled: () => {
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ordersKey });
     },
   });
