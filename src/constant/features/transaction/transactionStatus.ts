0d9b887c14c5568870000d789dc63c145a17a5f2
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-28 14:21:44 +0330

    feat(transaction)
    Created `TRANSACTION_STATUS_LABELS`,  `TRANSACTION_TYPE_LABELS`

--- src/constant/features/transaction/transactionStatus.ts
+++ src/constant/features/transaction/transactionStatus.ts
@@ -7,5 +7,11 @@ const TRANSACTION_STATUS = {
 type TransactionStatus =
   (typeof TRANSACTION_STATUS)[keyof typeof TRANSACTION_STATUS];
 
-export { TRANSACTION_STATUS };
+const TRANSACTION_STATUS_LABELS: Record<TransactionStatus, string> = {
+  [TRANSACTION_STATUS.Pending]: "در انتظار",
+  [TRANSACTION_STATUS.Success]: "موفق",
+  [TRANSACTION_STATUS.Failed]: "ناموفق",
+};
+
+export { TRANSACTION_STATUS, TRANSACTION_STATUS_LABELS };
 export type { TransactionStatus };
--- src/constant/features/transaction/transactionType.ts
+++ src/constant/features/transaction/transactionType.ts
@@ -5,7 +5,14 @@ const TRANSACTION_TYPE = {
   Trade: "Trade",
 } as const;
 
+const TRANSACTION_TYPE_LABELS: Record<TransactionType, string> = {
+  [TRANSACTION_TYPE.Deposit]: "واریز",
+  [TRANSACTION_TYPE.Withdrawal]: "برداشت",
+  [TRANSACTION_TYPE.TradeLock]: "قفل موجودی",
+  [TRANSACTION_TYPE.Trade]: "معامله",
+};
+
 type TransactionType = (typeof TRANSACTION_TYPE)[keyof typeof TRANSACTION_TYPE];
 
-export { TRANSACTION_TYPE };
+export { TRANSACTION_TYPE, TRANSACTION_TYPE_LABELS };
 export type { TransactionType };
