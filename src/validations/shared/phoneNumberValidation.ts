e73cc549514a8cb77d7cbf41315c6af2b124b8cb
Alireza <alirezaaslane.mob7@gmail.com>
2026-06-30 15:36:53 +0330

    feat(auth)
    Fixed Request OTP schema, Users can also enter phone number without 0

--- src/validations/shared/phoneNumberValidation.ts
+++ src/validations/shared/phoneNumberValidation.ts
@@ -1,6 +1,8 @@
 import { string } from "zod";
 
 const phoneNumberValidation = () =>
-  string().regex(/^09[0-9]{9}$/, "شماره تلفن نامعتبر هست");
+  string()
+    .regex(/^(0?9[0-9]{9})$/, "شماره تلفن نامعتبر هست")
+    .transform((val) => (val.startsWith("0") ? val : `0${val}`));
 
 export default phoneNumberValidation;
