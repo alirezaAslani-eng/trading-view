c045d4fef2f1f3a1e3d91828eda54fa156f6df3d
Alireza <alirezaaslane.mob7@gmail.com>
2026-07-03 00:20:17 +0330

    feat(static content)
    Shared kyc modals' content

---
+++ src/content/kyc.ts
@@ -0,0 +1,10 @@
+const kycContent = {
+  kycL1ModalFormTitle: "احراز هویت",
+  kycL1ModalFormSubTitle:
+    "برای تکمیل احراز هویت پایه، اطلاعات زیر را وارد کنید",
+  kycL2ModalFormTitle: "احراز هویت سطح 2",
+  kycL2ModalFormSubTitle:
+    "برای تکمیل احراز هویت سطح 2، اطلاعات زیر را وارد کنید",
+};
+
+export { kycContent };
