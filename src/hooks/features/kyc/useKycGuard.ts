c8acbdf4faaba1b0bf39dd370ea8920369914a06
soosan <soosan.adh@gmail.com>
2026-07-07 11:30:11 +0330

    feat(kyc)
    feat(kyc): add useKycGuard hook for KYC access control

---
+++ src/hooks/features/kyc/useKycGuard.ts
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux";
+import { useQuery } from "@tanstack/react-query";
+import isKycStepPassed from "@/utils/features/kyc/isKycStepPassed";
+import { KycLevel } from "@/types";
+import { kycStatusConfig } from "@/packages/react-query";
+import { upgradeKycLevel } from "@/redux/features/kyc";
+
+function useKycGuard() {
+  const dispatch = useDispatch();
+
+  const { data, isSuccess } = useQuery(kycStatusConfig());
+
+  const currentLevel = data?.kycLevel;
+
+  function checkAccess(requiredLevel: KycLevel): boolean {
+    if (isSuccess) return false;
+
+    const passed = isKycStepPassed(currentLevel, requiredLevel);
+
+    if (!passed) {
+      dispatch(upgradeKycLevel());
+    }
+
+    return passed;
+  }
+
+  return { checkAccess, currentLevel };
+}
+
+export default useKycGuard;
