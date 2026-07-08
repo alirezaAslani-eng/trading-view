021fff925a3b722b15159ce6d9bfd9dcc6c21032
soosan <soosan.adh@gmail.com>
2026-07-08 11:45:12 +0330

    feat(onSubmitHandler)
    add checkAccess logic

--- src/components/template/Form/DepositForm.tsx
+++ src/components/template/Form/DepositForm.tsx
@@ -15,14 +15,19 @@ import {
 } from "@/components/ui/Layout/TransactionFormLayout";
 import alertMessages from "@/constant/app/alertMessages";
 import { PRICE_UNITS } from "@/constant/features/priceConfig";
+import KYC_REQUIRED_LEVELS from "@/constant/features/kyc/kycAccess";
+import useKycGuard from "@/hooks/features/kyc/useKycGuard";
 
 const withdrawMutationConfig = depositConfig();
 function DepositForm() {
   const depositMutate = useMutation(withdrawMutationConfig);
+  const { checkAccess } = useKycGuard();
 
   const form = useForm();
 
   const onSubmitHandler = async (fields: any) => {
+    const hasAccess = checkAccess(KYC_REQUIRED_LEVELS.deposit);
+    if (!hasAccess) return;
     await promiseAlert(
       safeAsync(async () => {
         await depositMutate.mutateAsync({
