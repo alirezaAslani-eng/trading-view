8e407a14e9c2fedacd2454adb7539458caa163d2
Alireza <alirezaaslane.mob7@gmail.com>
2026-07-08 09:45:48 +0330

    fix(price)
    Compacted large price

--- src/utils/features/price/formatFaPrice.ts
+++ src/utils/features/price/formatFaPrice.ts
@@ -1,7 +1,25 @@
-function formatFaPrice(price: number | string | undefined | unknown): string {
+const defaultOptions: Intl.NumberFormatOptions = {
+  style: "decimal",
+  maximumFractionDigits: 2,
+  notation: "compact",
+  minimumFractionDigits: 0,
+  useGrouping: true,
+};
+
+function formatFaPrice(
+  price: number | string | undefined | unknown,
+  options?: Intl.NumberFormatOptions,
+): string {
   const priceAsNumber = Number(price);
-  if (!!!priceAsNumber) return String(price);
-  return priceAsNumber.toLocaleString("fa-IR");
+
+  if (!Number.isFinite(priceAsNumber)) return String(price);
+
+  const mergedOptions: Intl.NumberFormatOptions = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  return new Intl.NumberFormat("fa-IR", mergedOptions).format(priceAsNumber);
 }
 
 export default formatFaPrice;
