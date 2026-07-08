529c939ff00c1c047602dd4b0ad1ab41b610c632
soosan <soosan.adh@gmail.com>
2026-07-08 10:29:31 +0330

    feat(delete AccessLevelModal)

--- src/app/(panel)/layout.tsx
+++ src/app/(panel)/layout.tsx
@@ -2,13 +2,11 @@ import LayoutMainPanel from "@/Layout/LayoutMainPanel";
 import { PWC } from "@/types/utils";
 import KycGlobalModals from "@/components/template/Modal/KycGlobalModals";
 import { SidebarProvider } from "@/context/app/Sidebar";
-import AccessLevelModal from "@/components/template/Modal/AccessLevelModal";
 
 function layout({ children }: PWC) {
   return (
     <SidebarProvider>
       <KycGlobalModals />
-      <AccessLevelModal />
       <LayoutMainPanel>{children}</LayoutMainPanel>
     </SidebarProvider>
   );
