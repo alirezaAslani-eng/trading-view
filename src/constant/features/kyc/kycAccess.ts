b02c18b402f706bff5f6f25114a0ee89bb4c9078
soosan <soosan.adh@gmail.com>
2026-07-11 22:45:38 +0330

    feat( KYC_REQUIRED_LEVELS)
    add bankAccount accessLevel

--- src/constant/features/kyc/kycAccess.ts
+++ src/constant/features/kyc/kycAccess.ts
@@ -1,9 +1,10 @@
 import { KYC_LEVELS } from "@/constant/features/kyc/kycLevelOreder";
 
 const KYC_REQUIRED_LEVELS = {
-  trade: KYC_LEVELS.LEVEL_2,
-  withdraw: KYC_LEVELS.LEVEL_2,
-  deposit: KYC_LEVELS.LEVEL_2,
+  trade: KYC_LEVELS.LEVEL_1,
+  withdraw: KYC_LEVELS.LEVEL_1,
+  deposit: KYC_LEVELS.LEVEL_1,
+  bankAccount: KYC_LEVELS.LEVEL_2,
 } as const;
 
 export default KYC_REQUIRED_LEVELS;
