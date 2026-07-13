0289c167cf32e7af11c1f033dcd3ac4a8080772b
soosan <soosan.adh@gmail.com>
2026-07-13 11:24:42 +0330

    feat(MarketSubscribeProvider)

---
+++ src/context/feature/market/MarketSubscribeProvider.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import { symbolsConfig } from "@/packages/react-query";
+import { getConnection, start, subscribeToMarket } from "@/packages/signalr";
+
+function useSubscribeMarket() {
+  const { data, isSuccess } = useQuery(symbolsConfig());
+
+  useEffect(() => {
+    
+    const con = getConnection()!;
+
+    async function init() {
+      if (!isSuccess || !data) return;
+      try {
+        await start(con);
+
+        data.forEach((item) => {
+          con.invoke(subscribeToMarket, item.ticker).catch(console.error);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    init();
+  }, [data, isSuccess]);
+}
+
+function MarketSubscribeProvider() {
+  useSubscribeMarket();
+
+  return null;
+}
+
+export default MarketSubscribeProvider;
