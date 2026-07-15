47073dc86dd721438cf2ca848aeb531d88a29d8d
soosan <soosan.adh@gmail.com>
2026-07-15 14:55:08 +0330

    feat(add  needKyc)
    before  KycL1ModalForm

--- src/redux/features/kyc/types.ts
+++ src/redux/features/kyc/types.ts
@@ -1,7 +1,7 @@
 
 // * --start-- kycModalSlice.ts type ----
 interface KycFlowState {
-  modalFlow: "upgradeKyc" | "successKyc" | null;
+  modalFlow:"needKyc"| "upgradeKyc" | "successKyc" | null;
   
 }
 // * --end-- kycModalSlice.ts type ----
