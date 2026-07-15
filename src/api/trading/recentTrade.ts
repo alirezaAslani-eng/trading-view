1dfb9dc63f233ef7936afa5f8f76de550c98fba4
soosan <soosan.adh@gmail.com>
2026-07-15 19:05:43 +0330

    fix(recentTrades)
    change url

--- src/api/trading/recentTrade.ts
+++ src/api/trading/recentTrade.ts
@@ -1,24 +1,29 @@
 import fetchHandler from "@/utils/app/fetchHandler";
 import handleApiResponse from "@/utils/app/handleApiResponse";
-import { RecentTradeResponse, SymbolDetailsResponse } from "@/api/types";
+import { RecentTradeResponse } from "@/api/types";
 import { BaseApiResponse } from "@/types";
+import { sharedRequestInit } from "../sharedRequestInit";
 
-const URL = (symbol:string) =>
- 
-  `${process.env.BASEURL}/api/v1/market/tickers/${symbol}/price-history?limit=50`;
+const URL = (symbol: string) =>
+  `${process.env.NEXT_PUBLIC_AUTH_BASEURL}/api/v1/market/tickers/${symbol}/price-history?limit=50`;
 
 async function recentTrades(
   symbol: string,
 ): Promise<RecentTradeResponse> {
   const res = (await fetchHandler(async () => {
-    const response = await fetch(URL(symbol));
+    const response = await fetch(URL(symbol), {
+      ...sharedRequestInit,
+      method: "GET",
+    });
+
     return response;
   })) as Response;
 
-  const data = (await handleApiResponse(res)) as BaseApiResponse<RecentTradeResponse>;
+  const data = (await handleApiResponse(
+    res,
+  )) as BaseApiResponse<RecentTradeResponse>;
 
   return data.data;
 }
 
 export default recentTrades;
-
