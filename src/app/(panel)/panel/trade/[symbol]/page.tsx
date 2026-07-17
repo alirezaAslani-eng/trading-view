7654fcb587342f213f34b89b72b5b54a2850fcdf
Alireza <alirezaaslane.mob7@gmail.com>
2026-07-17 22:38:07 +0330

    fix(style)
    Changed the height of TradePanel container

--- src/app/(panel)/panel/trade/[symbol]/page.tsx
+++ src/app/(panel)/panel/trade/[symbol]/page.tsx
@@ -32,7 +32,7 @@ function page() {
             sx={{
               ...tradePageSpacing_sx.sectionContent_sx,
               alignItems: "stretch",
-              height:"540px"
+              height: "575px",
             }}
           >
             {/* // * -------- Trade Panel -------- */}
