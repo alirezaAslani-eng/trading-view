14818a9f5376117fa04ee6ef96bfb13b8fc4ea7c
Alireza <alirezaaslane.mob7@gmail.com>
2026-07-17 18:29:24 +0330

    fix(context)
    Created a Context for the entire app to provide all the public cookies from server for the use case of presistent state like `dark & light` theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,34 +15,37 @@ import RecentTradeSyncProvider from "@/context/feature/trade/RecentTradeSyncProv
 import MarketSubscribeProvider from "@/context/feature/market/MarketSubscribeProvider";
 import WalletPortfolioSyncProvider from "@/context/feature/Portfolio/WalletPortfolioSyncProvider";
 import { LocalizationProvider } from "@/packages/mui";
+import { ServerCookieProvider } from "@/context/app/Cookies";
 export default function RootLayout({ children }: PWC) {
   return (
     <QueryClientProvider>
-      <ReduxProvider>
-        <TickerInfoSyncProvider />
-        <MarketTickersSyncProvider />
-        <MarketSubscribeProvider />
-        <WalletPortfolioSyncProvider />
-        <RecentTradeSyncProvider />
-        <OrderBookSyncProvider />
-        <AppRouterCacheProvider>
-          <LocalizationProvider>
-            <ThemeProvider>
-              <CssBaseline />
-              <html lang="fa" dir="rtl">
-                <Box
-                  component={"body"}
-                  sx={{ minHeight: "100svh" }}
-                  className={`${IranYekanMedium.variable} ${IranYekanDemibold.variable} ${IranYekanRegular.variable}`}
-                >
-                  {children}
-                  <Toaster />
-                </Box>
-              </html>
-            </ThemeProvider>
-          </LocalizationProvider>
-        </AppRouterCacheProvider>
-      </ReduxProvider>
+      <ServerCookieProvider>
+          <ReduxProvider>
+            <TickerInfoSyncProvider />
+            <MarketTickersSyncProvider />
+            <MarketSubscribeProvider />
+            <WalletPortfolioSyncProvider />
+            <RecentTradeSyncProvider />
+            <OrderBookSyncProvider />
+            <AppRouterCacheProvider>
+              <LocalizationProvider>
+                <ThemeProvider>
+                  <CssBaseline />
+                  <html lang="fa" dir="rtl">
+                    <Box
+                      component={"body"}
+                      sx={{ minHeight: "100svh" }}
+                      className={`${IranYekanMedium.variable} ${IranYekanDemibold.variable} ${IranYekanRegular.variable}`}
+                    >
+                      {children}
+                      <Toaster />
+                    </Box>
+                  </html>
+                </ThemeProvider>
+              </LocalizationProvider>
+            </AppRouterCacheProvider>
+          </ReduxProvider>
+      </ServerCookieProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/context/app/Cookies/CookiesContext.tsx
@@ -0,0 +1,23 @@
+"use server";
+import { cookies } from "next/headers";
+// import { CookiesProvider } from "./ClientProvider";
+import { PropsWithChildren } from "react";
+import { CookiesProvider } from "./ClientProvider";
+
+const EXCLUDED_COOKIES = ["access_token", "refresh_token"];
+
+export async function ServerCookieProvider({ children }: PropsWithChildren) {
+  const cookieStore = await cookies();
+
+  const cookieValues = cookieStore
+    .getAll()
+    .reduce<Record<string, string>>((acc, cookie) => {
+      if (!EXCLUDED_COOKIES.includes(cookie.name)) {
+        acc[cookie.name] = cookie.value;
+      }
+
+      return acc;
+    }, {});
+
+  return <CookiesProvider cookies={cookieValues}>{children}</CookiesProvider>;
+}
