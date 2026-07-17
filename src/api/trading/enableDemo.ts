2a01a9aca27157e64ce2d889cbde10f7902eee67
Alireza <alirezaaslane.mob7@gmail.com>
2026-07-17 18:26:11 +0330

    fix(API)
    Fixed response type

--- src/api/trading/enableDemo.ts
+++ src/api/trading/enableDemo.ts
@@ -2,17 +2,26 @@ import fetchHandler from "@/utils/app/fetchHandler";
 import handleApiResponse from "@/utils/app/handleApiResponse";
 import { sharedRequestInit } from "../sharedRequestInit";
 import mutationFetch from "@/utils/app/mutationFetch";
+import { BaseApiResponse } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_AUTH_BASEURL}/api/v1/wallet/demo/activate`;
-async function enableDemo(): Promise<void> {
+
+type EnableDemoResponse = { expirationDate: string };
+async function enableDemo(): Promise<EnableDemoResponse> {
   const res = (await fetchHandler(() => {
     return mutationFetch(URL, {
-      method: "PUT",
+      method: "POST",
       ...sharedRequestInit,
     });
   })) as Response;
 
-  await handleApiResponse(res);
+  const data = (await handleApiResponse(res)) as BaseApiResponse<{
+    message: string;
+  }>;
+
+  return {
+    expirationDate: data.data.message,
+  };
 }
 
 export default enableDemo;
