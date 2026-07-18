b655884403f4a6efcfabb7a9a9a9b53184165867
Alireza <alirezaasalne.mob7@gmail.com>
2026-07-18 12:40:02 +0330

    refactor(component)
    Addd `StyledAlertProps` to `StyledAlert` styled component & Refactored to TS

--- src/components/ui/Alert/Alert.tsx
+++ src/components/ui/Alert/Alert.tsx
@@ -2,13 +2,24 @@
 import { alertSize, alertTheme } from "@/packages/mui/theme/variants";
 import { Alert as MuiAlerts, styled } from "@mui/material";
 import { WarningIcon } from "@/components/ui/Icon";
+import {
+  AlertSizeProps,
+  AlertThemeProps,
+} from "@/packages/mui/theme/variants/types";
+import { ComponentProps } from "react";
+
+interface StyledAlertProps {
+  size: AlertSizeProps["size"];
+  color: AlertThemeProps["color"];
+  variant: AlertThemeProps["variant"];
+}
 
 const StyledAlert = styled(MuiAlerts, {
   shouldForwardProp: (prop) => {
     return prop !== "size";
   },
-})(({ theme, color, variant, size }) => {
-  const alert_theme = alertTheme({ color, theme, variant, size });
+})<StyledAlertProps>(({ theme, color, variant, size }) => {
+  const alert_theme = alertTheme({ color, theme, variant });
   const alert_size = alertSize({ theme, size });
 
   return {
@@ -21,10 +32,7 @@ const StyledAlert = styled(MuiAlerts, {
   };
 });
 
-/**
- * @param {import("react").ComponentProps<typeof StyledAlert>} props
- */
-function Alert(props) {
+function Alert(props: ComponentProps<typeof StyledAlert>) {
   const icon = <WarningIcon />;
   return <StyledAlert {...props} icon={props?.icon ?? icon} />;
 }
