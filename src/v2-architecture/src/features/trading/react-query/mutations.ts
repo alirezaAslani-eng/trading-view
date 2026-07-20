9f8998aea0fe1db3d4d5cb1e91a578d4a55c35c7
Alireza <alirezaasalne.mob7@gmail.com>
2026-07-20 17:50:33 +0330

    fix(trading)
    setDemoSetting API success response must invalidate `demoSettingKey` key otherwise data cant be updated

--- src/v2-architecture/src/features/trading/react-query/mutations.ts
+++ src/v2-architecture/src/features/trading/react-query/mutations.ts
@@ -1,8 +1,12 @@
 import { createMutationOptions } from "@/v2-architecture/src/shared/lib/react-query";
 import { setDemoSetting, SetDemoSettingVariables } from "../api";
+import { demoSettingKey } from "./keys";
 
 export const setDemoConfig = createMutationOptions({
   mutationFn: (vars: SetDemoSettingVariables) => {
     return setDemoSetting({ body: vars });
+  },
+  meta: {
+    invalidates: [demoSettingKey],
   },
 });
