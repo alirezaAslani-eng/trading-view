654114325f8da54f18b72e1f9b947852a011e9f3
Alireza <alirezaasalne.mob7@gmail.com>
2026-07-21 13:02:18 +0330

    feat(validation)
    Added `isCompany` field

--- src/validations/kyc/kycL1Schema.ts
+++ src/validations/kyc/kycL1Schema.ts
@@ -1,16 +1,18 @@
-import { number, object, string } from "zod";
+import {  discriminatedUnion, literal, object, string } from "zod";
 import isValidNationalCodeFormat from "@/utils/features/identification/isValidNationalCodeFormat";
-import { dayjs } from "@/packages/dayjs";
 const inValidDateError = "تاریخ معتبر نیست";
 const invalidNationalError = "کد ملی نا معتبر";
 
-const kvcL1Schema = object({
-  nationalId: string(invalidNationalError)
-    .length(10, invalidNationalError)
-    .regex(/^\d+$/, invalidNationalError)
-    .refine((v) => isValidNationalCodeFormat(v), {
-      message: invalidNationalError,
-    }),
+
+const companySchema = object({
+  isCompany: literal(true),
+  nationalId: string(invalidNationalError).regex(/^\d+$/, invalidNationalError),
+});
+
+const personSchema = object({
+  isCompany: literal(false),
+  nationalId: string(invalidNationalError).regex(/^\d+$/, invalidNationalError),
+  birthYear: string(inValidDateError).regex(/^\d+$/, inValidDateError),
   birthDay: string(inValidDateError)
     .regex(/^\d+$/, inValidDateError)
     .min(1, inValidDateError)
@@ -19,8 +21,11 @@ const kvcL1Schema = object({
     .regex(/^\d+$/, inValidDateError)
     .min(0, inValidDateError)
     .max(11, inValidDateError)
-    .transform((v) => String(Number(v) + 1)),
-  birthYear: string(inValidDateError).regex(/^\d+$/, inValidDateError),
+    .transform((v) => {
+      return String(Number(v) + 1);
+    }),
 });
+
+const kvcL1Schema = discriminatedUnion("isCompany", [companySchema, personSchema]);
 
 export default kvcL1Schema;
--- src/validations/types/kyc.type.ts
+++ src/validations/types/kyc.type.ts
@@ -1,13 +1,20 @@
-import { Infer } from "zod";
+import { Infer, input, output } from "zod";
 import type kvcL1Schema from "../kyc/kycL1Schema";
 import type kycL2Schema from "../kyc/kycL2Schema";
 
 // * -----start---- kvcL1Schema.ts ------------
 type KycL1SchemaType = Infer<typeof kvcL1Schema>;
+type KycL1SchemaInput = input<typeof kvcL1Schema>;
+type KycL1SchemaOutput = output<typeof kvcL1Schema>;
 // * -----end---- kvcL1Schema.ts ------------
 
 // * -----start---- kvcL2Schema.ts ------------
 type KycL2SchemaType = Infer<typeof kycL2Schema>;
 // * -----end---- kvcL2Schema.ts ------------
 
-export type { KycL1SchemaType, KycL2SchemaType };
+export type {
+  KycL1SchemaType,
+  KycL2SchemaType,
+  KycL1SchemaInput,
+  KycL1SchemaOutput,
+};
