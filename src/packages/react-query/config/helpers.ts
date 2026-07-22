d687890abb7ef89b43e5ce5dbf6f714f191fc107
Alireza <alirezaasalne.mob7@gmail.com>
2026-07-22 10:49:53 +0330

    fix(trading)
    Renamed `isDemo` to `isdemo` query string

--- src/packages/react-query/config/helpers.ts
+++ src/packages/react-query/config/helpers.ts
@@ -3,6 +3,6 @@ import { TradeModeStore } from "@/context/feature/trade/TradeMode/helpers";
 export function buildTradeModeQueries() {
   const isDemo = TradeModeStore.getTradeModeConfig().isDemo;
   return {
-    ...(isDemo && { isDemo: "true" }),
+    ...(isDemo && { isdemo: "true" }),
   };
 }
