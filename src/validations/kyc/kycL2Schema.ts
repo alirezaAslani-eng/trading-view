b6fdb064bdca3de8cbfb3ffadfb4f28d3df654e1
Alireza <alirezaasalne.mob7@gmail.com>
2026-07-22 18:59:53 +0330

    feat(kyc)
    Updated kyc level 2 fields

--- src/validations/kyc/kycL2Schema.ts
+++ src/validations/kyc/kycL2Schema.ts
@@ -1,7 +1,21 @@
-import { object, string } from "zod";
+import z from "zod";
 
-const kycL2Schema = object({
-  postalCode: string().length(10),
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/webp"];
+
+const fileScheam = z
+  .instanceof(File)
+  .refine(
+    (file) => file.size <= MAX_FILE_SIZE,
+    "حجم فایل باید کمتر از ۵ مگابایت باشد"
+  )
+  .refine(
+    (file) => ACCEPTED_IMAGE_TYPES.includes(file.type),
+    "فرمت فایل باید jpg، png یا webp باشد"
+  );
+
+const kycL2Schema = z.object({
+  file: z.array(fileScheam,"فایل نا معتبر").length(1, "فقط عکس روی کارت ملی را آپلود کنید"),
 });
 
 export default kycL2Schema;
