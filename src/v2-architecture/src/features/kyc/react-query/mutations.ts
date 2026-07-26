4d9978ccf2acb6a8be9f21d89746ba91d43167c2
Alireza <alirezaasalne.mob7@gmail.com>
2026-07-26 12:10:54 +0330

    feat(react query)
    Added cache invalidations

--- src/v2-architecture/src/features/kyc/react-query/mutations.ts
+++ src/v2-architecture/src/features/kyc/react-query/mutations.ts
@@ -1,10 +1,14 @@
 import { createMutationOptions } from "@/v2-architecture/src/shared/lib/react-query";
-import { kycL3Key } from "./keys";
+import { kycL3Key, kycProgressKey } from "./keys";
 import { kycL3, KycL3Variables } from "../api";
+import { dashboardInfoKey, kycStatusKey } from "@/packages/react-query";
 
 export const kycL3Config = createMutationOptions({
   mutationKey: kycL3Key,
-  meta: { successMessage: "احراز سطح 3 شما ثبت شد" },
+  meta: {
+    successMessage: "احراز سطح 3 شما ثبت شد",
+    invalidates: [kycStatusKey, kycProgressKey, dashboardInfoKey],
+  },
   mutationFn: (vars: KycL3Variables) => {
     return kycL3({ body: vars });
   },
--- src/v2-architecture/src/features/user/react-query/mutations.ts
+++ src/v2-architecture/src/features/user/react-query/mutations.ts
@@ -1,8 +1,12 @@
 import { createMutationOptions } from "@/v2-architecture/src/shared/lib/react-query";
 import { updateAvatar, UpdateAvatarVariables } from "../api";
+import { dashboardInfoKey } from "@/packages/react-query";
 
 export const updateAvatarConfig = createMutationOptions({
   mutationFn: (vars: UpdateAvatarVariables) => {
     return updateAvatar({ body: vars });
+  },
+  meta: {
+    invalidates: [dashboardInfoKey],
   },
 });
