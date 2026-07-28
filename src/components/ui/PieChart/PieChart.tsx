c8fef03a66214b10766ce691707c803405c6a093
Alireza <alirezaasalne.mob7@gmail.com>
2026-07-28 13:49:11 +0330

    feat(component)
    Created  `PieChart`

---
+++ src/components/ui/PieChart/PieChart.tsx
@@ -0,0 +1,39 @@
+import { PieChart as MuiPieChart } from "@mui/x-charts/PieChart";
+import type { PieChartProps } from "@mui/x-charts/PieChart";
+import { useMemo } from "react";
+
+const DEFAULT_SERIES = {
+  innerRadius: 60,
+  outerRadius: 100,
+  paddingAngle: 2,
+  cornerRadius: 4,
+};
+function PieChart({ slotProps, ...props }: PieChartProps) {
+  const series = useMemo(
+    () =>
+      props?.series?.map((item) => {
+        return {
+          ...DEFAULT_SERIES,
+          ...item,
+        };
+      }),
+    [props?.series],
+  );
+  return (
+    <MuiPieChart
+      {...props}
+      slotProps={{
+        ...slotProps,
+        tooltip: {
+          container() {
+            return document.body;
+          },
+          ...slotProps?.tooltip,
+        },
+      }}
+      series={series}
+    />
+  );
+}
+
+export default PieChart;
