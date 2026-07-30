efc567f277123ea6eb4c801a5cea12f54395c8dd
Alireza <alirezaaslane.mob7@gmail.com>
2026-07-30 17:31:24 +0330

    fix(component)
    `FallbackHandler` can also accept the children and render it

--- src/components/ui/Fallback/FallbackHandler.tsx
+++ src/components/ui/Fallback/FallbackHandler.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 
 interface FallbackHandlerProps {
   isLoading?: boolean;
@@ -10,10 +10,11 @@ interface FallbackHandlerProps {
     noData?: ReactNode;
   };
 }
-function FallbackHandler(props: FallbackHandlerProps) {
+function FallbackHandler(props: PropsWithChildren<FallbackHandlerProps>) {
   if (props.isLoading) return props?.fallbacks?.loader;
   if (props.isError) return props?.fallbacks?.error ?? props?.fallbacks?.loader;
   if (props.dataLength === 0) return props?.fallbacks?.noData;
+  return props.children;
 }
 
 export default FallbackHandler;
