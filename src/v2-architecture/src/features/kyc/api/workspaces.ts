40a6193ddcb9fc5a699b2b04886ee2857caa16ba
Alireza <alirezaaslane.mob7@gmail.com>
2026-07-31 18:24:22 +0330

    feat(kyc)
    Added fake data to `workspaces` API

--- src/v2-architecture/src/features/kyc/api/workspaces.ts
+++ src/v2-architecture/src/features/kyc/api/workspaces.ts
@@ -1,9 +1,4 @@
-import {
-  apiClient,
-  ApiConfig,
-  apiError,
-  BaseApiResponse,
-} from "@/v2-architecture/src/api";
+import { ApiConfig } from "@/v2-architecture/src/api";
 
 export const MY_COMPANY_ROLE = {
   owner: "Owner",
@@ -19,12 +14,33 @@ export type WorkspacesData = {
   myRole: MyCompanyRole;
 }[];
 
-const URL = apiClient.authBaseURL("/api/v1/workspaces");
+const FAKE_WORKSPACES: WorkspacesData = [
+  {
+    companyId: "1",
+    companyName: "شرکت آریا تجارت",
+    companyNationalId: "14007654321",
+    myRole: MY_COMPANY_ROLE.owner,
+  },
+  {
+    companyId: "2",
+    companyName: "بازرگانی پارسیان",
+    companyNationalId: "14009876543",
+    myRole: MY_COMPANY_ROLE.trader,
+  },
+  {
+    companyId: "3",
+    companyName: "هلدینگ سپهر نوین",
+    companyNationalId: "14001122334",
+    myRole: MY_COMPANY_ROLE.owner,
+  },
+];
 
 export const workspaces = async ({
   signal,
 }: ApiConfig = {}): Promise<WorkspacesData> => {
-  const res = await apiClient.get(URL, { signal });
-  const raw = await apiError.jsonHandler<BaseApiResponse<WorkspacesData>>(res);
-  return raw.data;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(FAKE_WORKSPACES);
+    }, 1200); // شبیه‌سازی تاخیر شبکه
+  });
 };
