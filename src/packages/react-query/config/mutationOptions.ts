3c0ca17f28ca4ce18137fe9c5007246527a323f9
Alireza <alirezaasalne.mob7@gmail.com>
2026-08-02 12:35:58 +0330

    feat(kyc)
    If the error `Kyc.RequireMerge` occured user must confirm to merge their account otherwise the queryFn behaves like an api and throws an error

--- src/packages/react-query/config/mutationOptions.ts
+++ src/packages/react-query/config/mutationOptions.ts
@@ -1,3 +1,4 @@
+"use client";
 import { kycL1, addShaba, enableDemo, KycL1Variables } from "@/api";
 import addCard from "@/api/bank/addcard";
 import { deposit } from "@/api";
@@ -53,6 +54,11 @@ import {
 } from "../keys/queryKeys";
 import { TradeModeStore } from "@/context/feature/trade/TradeMode/helpers";
 import { kycProgressKey } from "@/v2-architecture/src/features/kyc/react-query/keys";
+import safeAsync from "@/utils/app/safeAsync";
+import { kycMergeAccount } from "@/v2-architecture/src/features/kyc/api";
+import { show } from "@ebay/nice-modal-react";
+import { GenericConfirmDialog } from "@/packages/nice-modal-react";
+import { apiError } from "@/v2-architecture/src/api";
 
 const requestAuthOTPConfig = createMutationOptions({
   mutationKey: requestAuthOTPKey,
@@ -69,10 +75,27 @@ const verifyAuthOTPConfig = createMutationOptions({
   },
 });
 
+// REFACTOR : This mutation option might makes bugs or it may be refactord to a beetter structure because it has certain dependencies
 const kycLevel1Config = createMutationOptions({
   mutationKey: kycLevel1Key,
-  mutationFn: (vars: KycL1Variables) => {
-    return kycL1({ body: vars });
+  mutationFn: async (vars: KycL1Variables) => {
+    const kycL1_res = await safeAsync(() => kycL1({ body: vars }));
+    if (!kycL1_res.ok) {
+      if (true) {
+        const isConfirm = await show(GenericConfirmDialog, {
+          color: "primary",
+          title: "نیاز به ادغام حساب کاربری",
+          description:
+            "این شماره موبایل قبلاً برای حساب دیگری که با همین کد ملی ثبت شده استفاده شده است. آیا می‌خواهید اطلاعات آن حساب به حساب فعلی منتقل شود؟",
+        });
+        apiError.throwError(!isConfirm, {
+          message: "برای احراز نیاز به ادغام حساب هست",
+        });
+        return kycMergeAccount({
+          body: { nationalId: vars.nationalId },
+        });
+      }
+    }
   },
   meta: {
     invalidates: [kycStatusKey, dashboardInfoKey, kycProgressKey],
