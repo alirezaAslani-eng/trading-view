5ff67b98d15c8c5942e8f6f221045a2c0c5c83e8
Alireza <alirezaasalne.mob7@gmail.com>
2026-08-02 18:24:18 +0330

    fix(kyc)
    Just some type error reported by TS

--- src/validations/types/kyc.type.ts
+++ src/validations/types/kyc.type.ts
@@ -1,11 +1,11 @@
 import { Infer, input, output } from "zod";
-import type kvcL1Schema from "../kyc/kycL1Schema";
+import { kycL1Schema } from "../kyc/kycL1Schema";
 import type kycL2Schema from "../kyc/kycL2Schema";
 
 // * -----start---- kvcL1Schema.ts ------------
-type KycL1SchemaType = Infer<typeof kvcL1Schema>;
-type KycL1SchemaInput = input<typeof kvcL1Schema>;
-type KycL1SchemaOutput = output<typeof kvcL1Schema>;
+type KycL1SchemaType = Infer<typeof kycL1Schema>;
+type KycL1SchemaInput = input<typeof kycL1Schema>;
+type KycL1SchemaOutput = output<typeof kycL1Schema>;
 // * -----end---- kvcL1Schema.ts ------------
 
 // * -----start---- kvcL2Schema.ts ------------
