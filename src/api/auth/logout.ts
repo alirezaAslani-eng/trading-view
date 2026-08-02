f46498c3969e689ef9fcccb6754db4853598f0cd
Alireza <alirezaasalne.mob7@gmail.com>
2026-08-02 11:41:37 +0330

    fix(auth)
    This API must use `NEXT_PUBLIC_BASEURL` because the request dosen't need to be authorized

--- src/api/auth/logout.ts
+++ src/api/auth/logout.ts
@@ -3,18 +3,17 @@ import handleApiResponse from "@/utils/app/handleApiResponse";
 import mutationFetch from "@/utils/app/mutationFetch";
 import { sharedRequestInit } from "../sharedRequestInit";
 
-const URL = `${process.env.NEXT_PUBLIC_AUTH_BASEURL}/api/v1/auth/logout`;
+const URL = `${process.env.NEXT_PUBLIC_BASEURL}/api/v1/auth/logout`;
 
-async function logout():Promise<void> {
+async function logout(): Promise<void> {
   const res = (await fetchHandler(async () => {
     return mutationFetch(URL, {
       ...sharedRequestInit,
       method: "POST",
-      
     });
   })) as Response;
 
-  await handleApiResponse(res) 
+  await handleApiResponse(res);
 }
 
 export default logout;
