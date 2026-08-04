806c42d8b0faa99bb6d82747cfeb2266c39d81d4
Alireza <alirezaasalne.mob7@gmail.com>
2026-08-04 15:05:49 +0330

    feat(kyc)
    Integrated `KycL2ModalForm` with updated kyc APIs

--- src/components/template/Form/KycL2ModalForm.tsx
+++ src/components/template/Form/KycL2ModalForm.tsx
@@ -2,14 +2,12 @@
 
 import InputText from "@/components/ui/Input/InputText";
 import { kycLevel2Config } from "@/packages/react-query";
-import kycL2Schema from "@/validations/kyc/kycL2Schema";
-import { KycL2SchemaType } from "@/validations/types";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import safeAsync from "@/utils/app/safeAsync";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Controller, useForm } from "react-hook-form";
 import { useDispatch } from "@/packages/redux";
 import { exitKycFlow } from "@/redux/features/kyc";
+import { FormControl, Typography } from "@mui/material";
 import {
   FormLayout,
   FormLayoutField,
@@ -26,26 +24,25 @@ import {
 } from "@/components/ui/Layout/ModalLayout";
 import { kycContent } from "@/content/kyc";
 import InputFile from "@/components/ui/Input/InputFile";
-import { Typography } from "@mui/material";
 import {
   InputSelect,
   InputSelectItem,
   InputSelectMenu,
 } from "@/components/ui/Input/InputSelect";
-
-const activityFields = [
-  { value: "software", label: "فناوری اطلاعات" },
-  { value: "finance", label: "مالی و حسابداری" },
-  { value: "health", label: "پزشکی و سلامت" },
-  { value: "education", label: "آموزش" },
-  { value: "other", label: "سایر" },
-];
+import kycL2Schema from "@/validations/kyc/kycL2Schema";
+import { KycL2Variables } from "@/api";
+import {
+  activityFieldsConfig,
+  addressInquiryConfig,
+} from "@/v2-architecture/src/features/kyc/react-query";
+import useUpdateEffect from "@/hooks/app/useUpdateEffect";
+import safeAsync from "@/utils/app/safeAsync";
 
 function KycL2Form() {
   const dispatch = useDispatch();
-
   const closeKycModal = () => dispatch(exitKycFlow());
 
+  //#region // * ------------ Form Mutation ------------
   const mutation = useMutation(
     kycLevel2Config({
       onSuccess: closeKycModal,
@@ -54,15 +51,38 @@ function KycL2Form() {
       },
     }),
   );
+  //#endregion // * ------------ Form Mutation ------------
 
-  const form = useForm({ resolver: zodResolver(kycL2Schema) });
-
-  const submitHandler: SubmitHandler<KycL2SchemaType> = async (fields) => {
-    await safeAsync(async () => {
-      await mutation.mutateAsync(fields);
-    });
+  //#region // * ------------ Form State ------------
+  const form = useForm({
+    resolver: zodResolver(kycL2Schema),
+    defaultValues: {
+      activityField: "",
+      city: "",
+      fullAddress: "",
+      jobTitle: "",
+      postalCode: "",
+      state: "",
+    },
+  });
+  const submitHandler = async (fields: KycL2Variables) => {
+    await safeAsync(() => mutation.mutateAsync(fields));
   };
+  //#endregion // * ------------ Form State ------------
 
+  //#region // * ------------ Form Data ------------
+  // * ActivityFields
+  const { data: activityFields = [] } = useQuery(activityFieldsConfig());
+  // * AddressInquiry
+  const postalCode = form.watch("postalCode");
+  const { data: addressInquiry } = useQuery(addressInquiryConfig(postalCode));
+  useUpdateEffect(() => {
+    if (!addressInquiry) return;
+    form.setValue("state", addressInquiry.state);
+    form.setValue("city", addressInquiry.city);
+    form.setValue("fullAddress", addressInquiry.fullAddress);
+  }, [addressInquiry]);
+  //#endregion // * ------------ Form Data ------------
   return (
     <ModalLayout>
       <ModalLayoutHeading>
@@ -74,79 +94,121 @@ function KycL2Form() {
       </ModalLayoutHeading>
 
       <ModalLayoutBody>
-        <FormLayout onSubmit={form.handleSubmit(submitHandler)}>
-          <FormLayoutFieldGroup>
-            <FormLayoutField>
-              <FormLayoutLable>{"حوزه فعالیت"}</FormLayoutLable>
-
-              <InputSelect placeholder="حوزه فعالیت را انتخاب کنید">
-                <InputSelectMenu>
-                  {activityFields.map((item) => (
-                    <InputSelectItem key={item.value} value={item.value}>
-                      {item.label}
-                    </InputSelectItem>
-                  ))}
-                </InputSelectMenu>
-              </InputSelect>
-            </FormLayoutField>
-
-            <FormLayoutField>
-              <FormLayoutLable>{"شغل"}</FormLayoutLable>
-              <InputText placeholder="شغل خود را وارد کنید" />
-            </FormLayoutField>
-          </FormLayoutFieldGroup>
-
-          <FormLayoutField>
-            <FormLayoutLable>{"کد پستی"}</FormLayoutLable>
-            <InputText placeholder="کد پستی خود را وارد کنید" />
-          </FormLayoutField>
-
-          <FormLayoutField>
-            <FormLayoutLable>{"آدرس"}</FormLayoutLable>
-            <InputText
-              placeholder="آدرس محل سکونت را وارد کنید"
-              //@ts-ignore
-              textarea
-            />
-          </FormLayoutField>
-
-          <FormLayoutField>
-            <FormLayoutLable>{kycContent.kycL2PostalCodeLabel}</FormLayoutLable>
-
-            <Controller
-              name="file"
-              control={form.control}
-              render={({
-                field: { onChange, value, ...field },
-                fieldState,
-                formState,
-              }) => (
-                <>
-                  <InputFile
-                    {...field}
-                    multiple
-                    error={!!fieldState.error?.message}
-                    disabled={formState.isSubmitting}
-                    onChange={(e) => {
-                      const files = Array.from(e.target.files ?? []);
-                      onChange(files);
-                    }}
-                  />
-
-                  {!!fieldState.error?.message && (
-                    <Typography variant="body3" sx={{ color: "text.error" }}>
-                      {fieldState.error?.message}
-                    </Typography>
+        <FormControl disabled={form.formState.isSubmitting}>
+          <FormLayout onSubmit={form.handleSubmit(submitHandler)}>
+            <FormLayoutFieldGroup>
+              <FormLayoutField>
+                <FormLayoutLable>{"حوزه فعالیت"}</FormLayoutLable>
+                <Controller
+                  control={form.control}
+                  name="activityField"
+                  render={({ field, fieldState }) => (
+                    <InputSelect
+                      placeholder="حوزه فعالیت را انتخاب کنید"
+                      error={!!fieldState.error?.message}
+                      value={field.value}
+                      onChange={field.onChange}
+                    >
+                      <InputSelectMenu>
+                        {activityFields.map((item) => (
+                          <InputSelectItem key={item.value} value={item.value}>
+                            {item.label}
+                          </InputSelectItem>
+                        ))}
+                      </InputSelectMenu>
+                    </InputSelect>
                   )}
-                </>
-              )}
-            />
-          </FormLayoutField>
+                />
+              </FormLayoutField>
 
-          <FormLayoutSubmit disabled={form.formState.isSubmitting}>
-            {kycContent.upgradeKycSubmitButton}
-          </FormLayoutSubmit>
-        </FormLayout>
+              <FormLayoutField>
+                <FormLayoutLable>{"شغل"}</FormLayoutLable>
+                <InputText
+                  placeholder="شغل خود را وارد کنید"
+                  {...form.register("jobTitle")}
+                  error={!!form.formState.errors.jobTitle}
+                />
+              </FormLayoutField>
+            </FormLayoutFieldGroup>
+
+            <FormLayoutFieldGroup>
+              <FormLayoutField>
+                <FormLayoutLable>{"کد پستی"}</FormLayoutLable>
+                <InputText
+                  placeholder="کد پستی خود را وارد کنید"
+                  {...form.register("postalCode")}
+                  error={!!form.formState.errors.postalCode}
+                />
+              </FormLayoutField>
+
+              <FormLayoutField>
+                <FormLayoutLable>{"منطقه"}</FormLayoutLable>
+                <InputText
+                  placeholder="عدد منطقه را وارد کنید"
+                  {...form.register("state")}
+                  error={!!form.formState.errors.state}
+                />
+              </FormLayoutField>
+            </FormLayoutFieldGroup>
+
+            <FormLayoutField>
+              <FormLayoutLable>{"شهر"}</FormLayoutLable>
+              <InputText
+                placeholder="نام شهر را وارد کنید"
+                {...form.register("city")}
+                error={!!form.formState.errors.city}
+              />
+            </FormLayoutField>
+
+            <FormLayoutField>
+              <FormLayoutLable>{"آدرس"}</FormLayoutLable>
+              <InputText
+                placeholder="آدرس محل سکونت را وارد کنید"
+                //@ts-ignore
+                textarea
+                {...form.register("fullAddress")}
+                error={!!form.formState.errors.fullAddress}
+              />
+            </FormLayoutField>
+
+            <FormLayoutField>
+              <FormLayoutLable>
+                {kycContent.kycL2PostalCodeLabel}
+              </FormLayoutLable>
+
+              <Controller
+                name="file"
+                control={form.control}
+                render={({
+                  field: { onChange, value, ...field },
+                  fieldState,
+                }) => (
+                  <>
+                    <InputFile
+                      {...field}
+                      multiple
+                      error={!!fieldState.error?.message}
+                      onChange={(e) => {
+                        const files = Array.from(e.target.files ?? []);
+                        onChange(files);
+                      }}
+                    />
+
+                    {!!fieldState.error?.message && (
+                      <Typography variant="body3" sx={{ color: "text.error" }}>
+                        {fieldState.error?.message}
+                      </Typography>
+                    )}
+                  </>
+                )}
+              />
+            </FormLayoutField>
+
+            <FormLayoutSubmit>
+              {kycContent.upgradeKycSubmitButton}
+            </FormLayoutSubmit>
+          </FormLayout>
+        </FormControl>
       </ModalLayoutBody>
     </ModalLayout>
   );
