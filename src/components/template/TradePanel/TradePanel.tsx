aabcb364ff1734c3dc0f6c2a0b36227e698ca41b
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-14 22:36:48 +0330

    refactor(trading)
    Consumed `SettlementModeContext` and removed settlement field from the form

--- src/components/template/TradePanel/TradePanel.tsx
+++ src/components/template/TradePanel/TradePanel.tsx
@@ -2,6 +2,26 @@
 import PanelPaper from "@/components/ui/Paper/PanelPaper";
 import Tabs from "@/components/ui/Tabs/Tabs";
 import { TabsProvider } from "@/context/app/TabsContext";
+import Button from "@/components/ui/Button/Button";
+import ToggleButtonGroup from "@/components/ui/ButtonGroup/ToggleButtonGroup";
+import tradeTogglebuttonSell_sx from "@/packages/mui/theme/shared-style/features/trading/tradeTogglebuttonSell_sx";
+import { useMutation } from "@tanstack/react-query";
+import { placeOrderConfig } from "@/packages/react-query";
+import safeAsync from "@/utils/app/safeAsync";
+import BouncCircleLoader from "@/components/ui/Fallback/BounceCircleLoader";
+import { useController, useFormState, useWatch } from "react-hook-form";
+import LimitedPriceForm from "./LimitedPriceForm";
+import MarketPriceForm from "./MarketPriceForm";
+import { TradeFormSubscriber } from "./types";
+import useKycGuard from "@/hooks/features/kyc/useKycGuard";
+import KYC_REQUIRED_LEVELS from "@/constant/features/kyc/kycAccess";
+import { ReplaceSxWithSxOnlyObject } from "@/packages/mui/theme/types";
+import { formatFaPrice } from "@/utils";
+import { PRICE_UNITS } from "@/constant/features/priceConfig";
+import { useFinalTradeSunmmary } from "./hooks";
+import { WEIGHT_UNITS } from "@/constant/features/product/weightUnits";
+import { useTradeMode } from "@/context/feature/trade/TradeMode";
+import { useTradeForm } from "./TradeFormContext";
 import {
   Box,
   BoxProps,
@@ -15,43 +35,14 @@ import {
   Typography,
   TypographyProps,
 } from "@mui/material";
-import Button from "@/components/ui/Button/Button";
-import { zodResolver } from "@hookform/resolvers/zod";
-import tradeFormSchema from "@/validations/trade/tradeFormSchema";
-import ToggleButtonGroup from "@/components/ui/ButtonGroup/ToggleButtonGroup";
-import tradeTogglebuttonSell_sx from "@/packages/mui/theme/shared-style/features/trading/tradeTogglebuttonSell_sx";
-import { useMutation } from "@tanstack/react-query";
-import { placeOrderConfig } from "@/packages/react-query";
-import safeAsync from "@/utils/app/safeAsync";
-import { promiseAlert } from "@/packages/react-hot-toast";
-import alertMessages from "@/constant/app/alertMessages";
-import BouncCircleLoader from "@/components/ui/Fallback/BounceCircleLoader";
-import {
-  FormProvider,
-  useController,
-  useForm,
-  useFormState,
-  useWatch,
-} from "react-hook-form";
 import {
   TradeFormSchemaInputType,
   TradeFormSchemaOutputType,
 } from "@/validations/types/trade.types";
-import LimitedPriceForm from "./LimitedPriceForm";
-import MarketPriceForm from "./MarketPriceForm";
-import { TradeFormSubscriber } from "./types";
-import useSymbolParams from "@/hooks/features/trading/useSymbolParams";
-import useKycGuard from "@/hooks/features/kyc/useKycGuard";
-import KYC_REQUIRED_LEVELS from "@/constant/features/kyc/kycAccess";
-import { ReplaceSxWithSxOnlyObject } from "@/packages/mui/theme/types";
-import { formatFaPrice } from "@/utils";
-import { PRICE_UNITS } from "@/constant/features/priceConfig";
-import { useFinalTradeSunmmary } from "./hooks";
-import { getTradePrecent } from "@/v2-architecture/src/features/trading";
-import { WEIGHT_UNITS } from "@/constant/features/product/weightUnits";
-import { show } from "@ebay/nice-modal-react";
-import { GenericConfirmDialog } from "@/packages/nice-modal-react";
-import { useTradeForm } from "./TradeFormContext";
+import {
+  getTradePrecent,
+  useSettlementMode,
+} from "@/v2-architecture/src/features/trading";
 
 type OrderTypes = TradeFormSchemaInputType["orderType"];
 type OrderSide = TradeFormSchemaInputType["orderSide"];
@@ -81,73 +72,58 @@ function TradePanel() {
   const { checkAccess } = useKycGuard();
 
   //  * ------- Submit handler ---------
+  const { toggleMode, settlementMode } = useSettlementMode()!;
+  const { isDemo } = useTradeMode()!;
   const onSubmit = async (fields: TradeFormSchemaOutputType) => {
     const hasAccess = checkAccess(KYC_REQUIRED_LEVELS.trade);
     if (!hasAccess) return;
-
-    if (form.watch("settlementMode")) {
-      const confirm = await show(GenericConfirmDialog, {
-        color: "primary",
-        title: "توجه به نحوه تسویه معامله",
-        description:
-          "شما در حال ثبت معامله با حالت تسویه ۱۰٪ هستید. در این حالت تنها ۱۰٪ مبلغ معامله در ابتدا پرداخت می‌شود و ۹۰٪ باقی‌مانده به‌عنوان بدهی شما ثبت خواهد شد. شما حداکثر ۳ روز فرصت دارید بدهی ایجادشده را تسویه کنید.",
-      });
-      if (!confirm) return;
-    }
-    await safeAsync(() => placeOrderApi.mutateAsync(fields));
+    return safeAsync(() =>
+      placeOrderApi.mutateAsync({ ...fields, settlementMode, isDemo }),
+    );
   };
 
   return (
-    <FormProvider {...form}>
-      <PanelPaper sx={{ p: "20px 12px", height: "100%" }}>
-        <Box component={"form"} onSubmit={form.handleSubmit(onSubmit)}>
-          <Stack sx={{ width: "100%" }}>
-            {/* //* Trade side selection (Buy / Sell) */}
-            <TradeSideSelector control={form.control} />
+    <PanelPaper sx={{ p: "20px 12px", height: "100%" }}>
+      <Box component={"form"} onSubmit={form.handleSubmit(onSubmit)}>
+        <Stack sx={{ width: "100%" }}>
+          {/* //* Trade side selection (Buy / Sell) */}
+          <TradeSideSelector control={form.control} />
 
-            <Box sx={{ mt: "calc(32px - 14px)" }}>
-              {/* //* ---------- Order type selector ---------- */}
-              <OrderTypeSelector control={form.control} />
-              <FormControlLabel
-                sx={{ my: 4 }}
-                control={
-                  <Switch
-                    {...form.register("settlementMode")}
-                    defaultChecked={
-                      form.formState.defaultValues?.settlementMode
-                    }
-                  />
-                }
-                label={
-                  <Typography
-                    component="span"
-                    variant="caption"
-                    sx={{ color: "text.secondary" }}
-                  >
-                    {form.watch("orderSide") === "sell"
-                      ? `دریافت ${tradePrecent.number} درصد مبلغ`
-                      : `پرداخت ${tradePrecent.number} درصد مبلغ`}
-                  </Typography>
-                }
-              />
-              {/* // * ---------- Limited Price Tab ---------- */}
+          <Box sx={{ mt: "calc(32px - 14px)" }}>
+            {/* //* ---------- Order type selector ---------- */}
+            <OrderTypeSelector control={form.control} />
+            <FormControlLabel
+              sx={{ my: 4 }}
+              control={<Switch checked={settlementMode} onClick={toggleMode} />}
+              label={
+                <Typography
+                  component="span"
+                  variant="caption"
+                  sx={{ color: "text.secondary" }}
+                >
+                  {form.watch("orderSide") === "sell"
+                    ? `دریافت ${tradePrecent.number} درصد مبلغ`
+                    : `پرداخت ${tradePrecent.number} درصد مبلغ`}
+                </Typography>
+              }
+            />
+            {/* // * ---------- Limited Price Tab ---------- */}
 
-              <Box hidden={!isLimitedType}>
-                <LimitedPriceForm />
-              </Box>
-              {/* // * ---------- Market Price Tab ---------- */}
-              <Box hidden={!isMarketType}>
-                <MarketPriceForm />
-              </Box>
-
-              <TradeSummary />
-              {/* //* Submit order */}
-              <SubmitOrderButton control={form.control} />
+            <Box hidden={!isLimitedType}>
+              <LimitedPriceForm />
             </Box>
-          </Stack>
-        </Box>
-      </PanelPaper>
-    </FormProvider>
+            {/* // * ---------- Market Price Tab ---------- */}
+            <Box hidden={!isMarketType}>
+              <MarketPriceForm />
+            </Box>
+
+            <TradeSummary />
+            {/* //* Submit order */}
+            <SubmitOrderButton control={form.control} />
+          </Box>
+        </Stack>
+      </Box>
+    </PanelPaper>
   );
 }
 
@@ -163,7 +139,7 @@ function TradeSummary() {
   } = useFinalTradeSunmmary();
 
   const isSell = form.watch("orderSide") === "sell";
-  const settlementMode = form.watch("settlementMode");
+  const { settlementMode } = useSettlementMode()!;
   return (
     <Stack spacing={2.5} sx={{ mt: "8px" }}>
       <Summary>
