4ca9889156b949fa7f16352ee11aa14010673b70
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-14 20:44:46 +0330

    feat(type)
    Added
    - query and variable types of `tradeMode`, `settlementMode`

--- src/v2-architecture/src/features/trading/api/index.ts
+++ src/v2-architecture/src/features/trading/api/index.ts
@@ -3,3 +3,9 @@ export {
   setDemoSetting,
   type SetDemoSettingVariables,
 } from "./setDemoSettings";
+export type {
+  TradeModeQueries,
+  TradeModeVariables,
+  SettlementModeQueries,
+  SettlementModeVariables,
+} from "./types";
---
+++ src/v2-architecture/src/features/trading/api/types.ts
@@ -0,0 +1,12 @@
+export type TradeModeQueries = {
+  isdemo?: boolean;
+};
+export type TradeModeVariables = {
+  isDemo: boolean;
+};
+export interface SettlementModeQueries {
+  settlementMode?: boolean;
+}
+export interface SettlementModeVariables {
+  settlementMode: boolean;
+}
