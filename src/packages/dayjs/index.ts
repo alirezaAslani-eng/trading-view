51f7e395a72f66983f28f24587e36e95ef35bb48
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-15 22:26:28 +0330

    refactor(dayjs)
    Removed unused hook

--- src/packages/dayjs/index.ts
+++ src/packages/dayjs/index.ts
@@ -1,5 +1,4 @@
 export { default as convertToGregory } from "./helpers/convertToGregory";
 export { default as convertToJalali } from "./helpers/convertToJalali";
-export { default as useDate } from "./hooks/useDate";
 export * from "./core";
-export type * from "./hooks/types";
+
