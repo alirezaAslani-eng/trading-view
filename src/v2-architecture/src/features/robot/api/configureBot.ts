384f7bd9d15d54906139d3ebb7f4fb053ed01073
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-16 10:00:13 +0330

    refactor(robot API)
    Changed the `endpoint` & body data

--- src/v2-architecture/src/features/robot/api/configureBot.ts
+++ src/v2-architecture/src/features/robot/api/configureBot.ts
@@ -1,16 +1,21 @@
 // --- configureBot ---
 import { apiClient, ApiConfig, apiError } from "@/v2-architecture/src/api";
+import { booleanToNumber } from "@/v2-architecture/src/shared/utils";
 import { ConfigureBotSchemaOutput } from "@/validations/robot/configureBotSchema";
 
-const url = apiClient.authBaseURL("/api/v1/admin/market-maker/configs");
+const url = apiClient.authBaseURL("/api/v1/admin/market-maker/bots");
 
 export const configureBot = async ({
   signal,
   body,
+  params,
 }: Config): Promise<ConfigureBotData> => {
-  const res = await apiClient.put(url, {
+  const res = await apiClient.put(`${url}/${params.botId}`, {
     signal,
-    body: JSON.stringify([body]),
+    body: JSON.stringify({
+      ...body,
+      settlementMode: booleanToNumber(body.settlementMode),
+    }),
   });
   return apiError.jsonHandler(res);
 };
@@ -20,6 +25,13 @@ export type ConfigureBotData = void; // * the api doesn't return anything
 export type ConfigureBotVariables = ConfigureBotSchemaOutput;
 //#endregion // * ------------ Shared types ------------
 
+export interface ConfigureBotParams {
+  botId: string;
+}
+
 //#region // * ------------ Internal types ------------
-type Config = ApiConfig<{ body: ConfigureBotVariables }>;
+type Config = ApiConfig<{
+  body: ConfigureBotVariables;
+  params: ConfigureBotParams;
+}>;
 //#endregion // * ------------ Internal types ------------
