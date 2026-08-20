a84ce694ae85f5e91da0fdfdf01bdcf3906cde17
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-20 16:36:05 +0330

    feat(signalR)
    Added `signalRLog`

---
+++ src/packages/signalr/helpers.ts
@@ -0,0 +1,26 @@
+type SignalRLogAction =
+  | "CONNECTION_CREATED"
+  | "CONNECTION_STARTED"
+  | "CONNECTION_FAILED"
+  | "CONNECTION_CLOSED"
+  | "LISTENER_REGISTERED"
+  | "LISTENER_REMOVED"
+  | "EVENT_RECEIVED"
+  | "INVOKE_STARTED"
+  | "INVOKE_SUCCESS"
+  | "INVOKE_FAILED"
+  | "CACHE_UPDATED"
+  | "CACHE_INVALIDATED"
+  | "RECONNECT_STARTED"
+  | "RECONNECT_SUCCESS"
+  | "RECONNECT_FAILED";
+
+type Hubs = "wallet" | "market" | "orders";
+
+export function signalRLog(
+  hub: Hubs,
+  action: SignalRLogAction,
+  data?: unknown,
+) {
+  console.log(`[SIGNALR] ${action}`, data ?? "");
+}
