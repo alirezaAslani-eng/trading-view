dbebf8c5a8faa7418030c032c2d0122eb22010b8
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-20 18:50:46 +0330

    feat(trade)
    ### add market subscriber and order book listener
    
    - Add JoinMarketProvider to subscribe to the current symbol.
    - Add OrderBookSyncProvider to listen for order book updates.
    - Pass the current symbol to both market providers.

--- src/app/(panel)/panel/trade/[symbol]/page.tsx
+++ src/app/(panel)/panel/trade/[symbol]/page.tsx
@@ -15,51 +15,62 @@ import {
   SectionContent,
 } from "@/components/ui/Layout/PageLayout";
 import { OrderBookSyncProvider } from "@/context/feature/market/OrderBookSyncProvider";
+import { JoinMarketProvider } from "@/context/feature/market/JoinMarketProvider";
 
-function page() {
+async function page(props: PageProps<`/panel/trade/[symbol]`>) {
+  const symbol = (await props.params).symbol;
+
   return (
-    <Page>
-      <Header>
-        <PageHeader
-          title="معامله"
-          subtitle="نمایی کامل از تراکنش‌ها، معاملات و گردش حساب"
-        />
-      </Header>
-      <Main>
-        <Section>
-          <PriceOverview />
-        </Section>
-        <Section sx={tradePageSpacing_sx.section_sx}>
-          <SectionContent
-            sx={{
-              ...tradePageSpacing_sx.sectionContent_sx,
-              alignItems: "stretch",
-              maxHeight: "617px", // * depends on the height of the largest section
-            }}
-          >
-            {/* // * -------- Trade Panel -------- */}
-            <OrderBookSyncProvider />
-            <TradeFormProvider>
-              <Box sx={{ width: "270px", maxWidth: "270px" }}>
-                <TradePanel />
-              </Box>
-              {/* // * -------- Order Book -------- */}
-              <Box sx={{ width: "270px" }}>
-                <OrderBook />
-              </Box>
-            </TradeFormProvider>
+    <>
+      {/* // * One Symbol Subscriber */}
+      <JoinMarketProvider symbol={symbol} />
+      {/* // * One Symbol Subscriber */}
 
-            {/* // * ---------- Trading Chart ---------- */}
-            <Box sx={{ flex: 1, minWidth: "0px" }}>
-              <TradeChart />
-            </Box>
-          </SectionContent>
-        </Section>
-        <Section sx={tradePageSpacing_sx.section_sx}>
-          <TradingActivity />
-        </Section>
-      </Main>
-    </Page>
+      {/* // *Order Book Listener */}
+      <OrderBookSyncProvider symbol={symbol} />
+      {/* // *Order Book Listener */}
+      <Page>
+        <Header>
+          <PageHeader
+            title="معامله"
+            subtitle="نمایی کامل از تراکنش‌ها، معاملات و گردش حساب"
+          />
+        </Header>
+        <Main>
+          <Section>
+            <PriceOverview />
+          </Section>
+          <Section sx={tradePageSpacing_sx.section_sx}>
+            <SectionContent
+              sx={{
+                ...tradePageSpacing_sx.sectionContent_sx,
+                alignItems: "stretch",
+                maxHeight: "617px", // * depends on the height of the largest section
+              }}
+            >
+              {/* // * -------- Trade Panel -------- */}
+              <TradeFormProvider>
+                <Box sx={{ width: "270px", maxWidth: "270px" }}>
+                  <TradePanel />
+                </Box>
+                {/* // * -------- Order Book -------- */}
+                <Box sx={{ width: "270px" }}>
+                  <OrderBook />
+                </Box>
+              </TradeFormProvider>
+
+              {/* // * ---------- Trading Chart ---------- */}
+              <Box sx={{ flex: 1, minWidth: "0px" }}>
+                <TradeChart />
+              </Box>
+            </SectionContent>
+          </Section>
+          <Section sx={tradePageSpacing_sx.section_sx}>
+            <TradingActivity />
+          </Section>
+        </Main>
+      </Page>
+    </>
   );
 }
 
