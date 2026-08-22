72e7189bc1020635e43fb2f5ebf5042ccb0d3958
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-22 13:16:19 +0330

    fix(kyc validation)
    Replaced `FileList` with `File`

--- src/v2-architecture/src/features/kyc/validations/kycL3Schema.ts
+++ src/v2-architecture/src/features/kyc/validations/kycL3Schema.ts
@@ -1,28 +1,23 @@
 import { z } from "zod";
-import { type infer as Infer, input, output, object, custom, file } from "zod";
+import { type infer as Infer, input, output, object } from "zod";
 
 const MAX_VIDEO_SIZE = 100 * 1024 * 1024; // 100 MB
 
 const ACCEPTED_VIDEO_TYPES = [
   "video/mp4",
-  //   "video/webm",
-  //   "video/ogg",
-  //   "video/quicktime", // .mov
-  //   "video/x-msvideo", // .avi
+  "video/webm",
+  // "video/ogg",
+  // "video/quicktime", // .mov
+  // "video/x-msvideo", // .avi
 ] as const;
 
 const videoValidation = z
-  .instanceof(FileList)
-  .transform((files) => files.item(0))
-  .pipe(
-    z
-      .instanceof(File)
-      .refine((file) => file.size <= MAX_VIDEO_SIZE)
-      .refine((file) =>
-        ACCEPTED_VIDEO_TYPES.includes(
-          file.type as (typeof ACCEPTED_VIDEO_TYPES)[number]
-        )
-      )
+  .instanceof(File)
+  .refine((file) => file.size <= MAX_VIDEO_SIZE)
+  .refine((file) =>
+    ACCEPTED_VIDEO_TYPES.includes(
+      file.type as (typeof ACCEPTED_VIDEO_TYPES)[number],
+    ),
   );
 
 export const kycL3Schema = object({
