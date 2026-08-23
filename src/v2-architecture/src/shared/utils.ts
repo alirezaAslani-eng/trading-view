3245e50ee127004e02de2969697fe8865e49ac75
Alireza <alirezaaslane.mob7@gmail.com>
2026-08-23 11:53:49 +0330

    feat(utility)
    Created `createAudio` utility function

--- src/v2-architecture/src/shared/utils.ts
+++ src/v2-architecture/src/shared/utils.ts
@@ -1,1 +1,9 @@
 export const booleanToNumber = (value: boolean): 0 | 1 => (value ? 1 : 0);
+
+//#region // * ------------ Wrapped (new Audio) API ------------
+export function createAudio(src: string): HTMLAudioElement | null {
+  if (typeof window === "undefined") return null;
+
+  return new Audio(src);
+}
+//#endregion // * ------------ Wrapped (new Audio) API ------------
