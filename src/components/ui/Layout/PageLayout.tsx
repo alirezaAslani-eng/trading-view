123127548f41b458dcafe7836480abb47571e743
Alireza <alirezaaslane.mob7@gmail.com>
2026-09-01 11:13:26 +0330

    feat(page layout -> header)
    Responsived `Header` component for mobile

---
+++ src/components/ui/Layout/PageLayout.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { Box, styled, Typography, type BoxProps } from "@mui/material";
+import Alert from "../Alert/Alert";
+import { useTradeMode } from "@/context/feature/trade/TradeMode";
+import { ReplaceSxWithSxOnlyObject } from "@/packages/mui/theme/types";
+
+// * Container
+const Page = styled(Box)({
+  minHeight: "100svh",
+});
+
+// * Container -> Header
+const _Header = (props: BoxProps<"header">) => {
+  return <Box component="header" {...props} />;
+};
+
+const Header = styled(_Header)(({ theme }) => {
+  const { breakpoints } = theme;
+  return {
+    [breakpoints.up("xs")]: {
+      paddingTop: "3px",
+    },
+    // Desktop : paddingTop: "48px",
+    // Desktop : paddingBottom: "42px",
+  };
+});
+
+// * Container -> Main
+const _Main = ({ children, ...props }: BoxProps<"main">) => {
+  const { isDemo } = useTradeMode();
+
+  return (
+    <Box component="main" {...props}>
+      {/* // * TradeMode Warning section */}
+      {isDemo && (
+        <Alert
+          color="warning"
+          variant="standard"
+          sx={{
+            height: "40px",
+            width: "100%",
+            mb: "32px",
+          }}
+        >
+          حالت آزمایشی — معاملات شما واقعی نیستند و سود یا زیانی ثبت نمی‌شود.
+        </Alert>
+      )}
+
+      {/* // * Content */}
+      {children}
+    </Box>
+  );
+};
+
+const Main = styled(_Main)({
+  paddingBottom: "42px",
+});
+
+// * Container -> Main -> Section
+const _Section = (props: BoxProps<"section">) => {
+  return <Box component="section" {...props} />;
+};
+
+const Section = styled(_Section)({
+  marginTop: "50px",
+
+  ":first-of-type": {
+    marginTop: "0px",
+  },
+});
+
+// * Container -> Main -> Section -> SectionContent
+const SectionContent = styled(Box)({
+  display: "flex",
+  gap: "24px",
+});
+
+// * Container -> Main -> Section -> SectionContent -> SectionHeading
+const SectionHeading = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  marginBottom: "12px",
+});
+
+// * Container -> Main -> Section -> SectionContent -> SectionHeading -> Title
+const SectionTitle = styled(Typography)(({ theme }) => ({
+  fontSize: theme.typography.h6.fontSize,
+  fontFamily: theme.typography.h6.fontFamily,
+  lineHeight: theme.typography.h6.lineHeight,
+  color: theme.palette.text.heading,
+}));
+
+export {
+  Page,
+  Header,
+  Main,
+  Section,
+  SectionContent,
+  SectionHeading,
+  SectionTitle,
+};
