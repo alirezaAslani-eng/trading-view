763a437e85cb9ce5a006576f7adaf50df2da5241
Alireza <alirezaasalne.mob7@gmail.com>
2026-09-10 12:41:36 +0330

    feat(button)
    make button sizes responsive for mobile

--- src/packages/mui/theme/variants/button/buttonSize.ts
+++ src/packages/mui/theme/variants/button/buttonSize.ts
@@ -1,46 +1,73 @@
 import { ButtonSizeProps, ButtonSizeReturn } from "../types";
 
 function buttonSize({ size, theme }: ButtonSizeProps) {
-  const { typography } = theme;
+  const { typography, breakpoints } = theme;
+
   const sizes = {
     small: {
       rootSize: {
-        height: "36px",
-        minWidth: "36px",
+        // Shared between breakpoints
         borderRadius: "8px",
-        padding: "0px 12px",
-        fontSize: typography.button4.fontSize,
-        fontFamily: typography.button4.fontFamily,
-        lineHeight: typography.button4?.lineHeight,
         gap: "2px",
+
+        // xs
+        ...typography.button4,
+        height: "32px",
+        minWidth: "32px",
+        padding: "0px 10px",
+
+        [breakpoints.up("sm")]: {
+          height: "36px",
+          minWidth: "36px",
+          padding: "0px 12px",
+        },
       },
     } satisfies ButtonSizeReturn,
+
     medium: {
       rootSize: {
-        height: "45px",
-        minWidth: "45px",
+        // Shared between breakpoints
+        ...typography.button2,
         borderRadius: "10px",
-        padding: "0px 16px",
-        fontSize: typography.button2.fontSize,
-        fontFamily: typography.button2.fontFamily,
         gap: "4px",
+
+        // Mobile
+        height: "40px",
+        minWidth: "40px",
+        padding: "0px 14px",
+
+        [breakpoints.up("sm")]: {
+          height: "45px",
+          minWidth: "45px",
+          padding: "0px 16px",
+        },
       },
     } satisfies ButtonSizeReturn,
+
     large: {
       rootSize: {
-        height: "56px",
-        minWidth: "56px",
-        borderRadius: "28px",
-        padding: "0px 20px",
-        fontSize: typography.button2.fontSize,
-        fontFamily: typography.button2.fontFamily,
-        lineHeight: typography.button2?.lineHeight,
-        gap: "6px",
+        // Shared between breakpoints
+        ...typography.button2,
+
+        // Mobile
+        height: "48px",
+        minWidth: "48px",
+        borderRadius: "24px",
+        padding: "0px 16px",
+        gap: "4px",
+
+        [breakpoints.up("sm")]: {
+          height: "56px",
+          minWidth: "56px",
+          borderRadius: "28px",
+          padding: "0px 20px",
+          gap: "6px",
+        },
       },
     } satisfies ButtonSizeReturn,
   };
 
-  return sizes?.[size] || sizes.medium;
+  return sizes[size] || sizes.medium;
 }
 
 export default buttonSize;
